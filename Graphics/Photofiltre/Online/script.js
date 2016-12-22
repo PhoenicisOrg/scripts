f688aa38aafc9f8e761d74bd348908030148f44e
Quentin PÂRIS <git@quentin.paris>
2016-12-22 16:39:18 +0100

    Fixed javascript mistakes

--- Graphics/Photofiltre/Online/script.js
+++ Graphics/Photofiltre/Online/script.js
@@ -7,21 +7,21 @@ var setupWizard = SetupWizard("Photofiltre");
 setupWizard.presentation("Photofiltre", "Antonio Da Cruz", "http://photofiltre.free.fr", "Quentin PÂRIS");
 
 var tempFile = createTempFile("exe");
-print(tempFile);
-Downloader
+
+new Downloader()
     .wizard(setupWizard)
     .url("http://photofiltre.free.fr/utils/pf-setup-fr-652.exe")
     .checksum("dc965875d698cd3f528423846f837d0dcf39616d")
     .to(tempFile)
     .get();
 
-Wine
+new Wine()
     .wizard(setupWizard)
     .prefix("photofiltre")
     .version("1.7.12")
     .run(tempFile);
 
-WineShortcut
+new WineShortcut()
     .name("Test")
     .create();
 
