2d67f2a8e7cec2d63bbf2502ad72db6032df36f5
Quentin PÂRIS <git@quentin.paris>
2016-12-22 16:46:24 +0100

    Wine shortcut fix

--- Functions/Shortcuts/Wine/script.js
+++ Functions/Shortcuts/Wine/script.js
@@ -1,19 +1,21 @@
 var WineShortcut = function () {
     var that = this;
     that._shortcutCreator = Bean("shortcutCreator");
+    that._description = "";
+
     that.name = function (name) {
         that._name = name;
         return that;
     };
     that.description = function (description) {
-        that._name = description;
+        that._description = description;
         return that;
     };
     that.create = function () {
         that._shortcutCreator.createShortcut(
             new com.phoenicis.library.dto.ShortcutDTO.Builder()
                 .withName(that._name)
-                .withDescription(that_description)
+                .withDescription(that._description)
                 .build()
         );
     }
