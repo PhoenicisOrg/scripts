e84d186de81ec3a7ce4a02861b47e80d34fdda95
Quentin PÂRIS <git@quentin.paris>
2016-12-23 19:00:23 +0100

    IE6 script

--- Functions/Engines/Wine/script.js
+++ Functions/Engines/Wine/script.js
@@ -295,11 +295,15 @@ var Wine = function () {
 
 Wine.prototype.regsvr32 = function() {
     var _wine = this;
+
     this.install = function(dll) {
         _wine.run("regsvr32", ["/i", dll]);
         return _wine;
-    }
+    };
+
+    return this;
 };
+
 /**
  * Regedit support
  * @param args
