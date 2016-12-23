427fabb86ff2c63dd0daffc0501e6ad6af6b4d54
Quentin PÂRIS <git@quentin.paris>
2016-12-23 18:29:22 +0100

    Cabextract fix

--- Functions/Filesystem/Extract/script.js
+++ Functions/Filesystem/Extract/script.js
@@ -37,6 +37,7 @@ var CabExtract = function() {
             processArguments = ["cabextract", that._archive];
         }
 
+        print("Extracting to: " + that._destination);
         mkdir(that._destination);
         var processBuilder = new java.lang.ProcessBuilder(Java.to(processArguments, "java.lang.String[]"));
         processBuilder.directory(new java.io.File(that._destination));
