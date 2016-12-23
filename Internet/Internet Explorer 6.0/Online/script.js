29e7401c14f2cd2fec82fdd8db2cdb828e4edbc8
Quentin PÂRIS <git@quentin.paris>
2016-12-23 15:07:09 +0100

    Internet Explorer 6

--- Internet/Internet Explorer 6.0/Online/script.js
+++ Internet/Internet Explorer 6.0/Online/script.js
@@ -7,13 +7,11 @@ var setupWizard = SetupWizard("Internet Explorer 6.0");
 
 setupWizard.presentation("Internet Explorer 6.0", "Microsoft", "http://www.microsoft.com", "Quentin PÂRIS");
 
-var tempFile = createTempFile("exe");
-
-new Resource()
+var setupFile = new Resource()
     .wizard(setupWizard)
     .url("http://files.playonlinux.com/ie/6.0/ie60.exe")
     .checksum("8e483db28ff01a7cabd39147ab6c59753ea1f533")
-    .to(tempFile)
+    .to("ie60.exe")
     .get();
 
 new Wine()
@@ -21,7 +19,7 @@ new Wine()
     .version(LATEST_STABLE_VERSION)
     .architecture("i386")
     .prefix("InternetExplorer6")
-    .run(tempFile)
+    .run(setupFile)
     .wait()
     .run("iexplore", ["-unregserver"])
     .overrideDLL()
