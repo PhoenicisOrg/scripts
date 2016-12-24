47f32089799ede3512a61aac6bd7b02249009c46
Quentin PÂRIS <git@quentin.paris>
2016-12-24 18:08:46 +0100

    Luna style

--- Functions/Verbs/luna/script.js
+++ Functions/Verbs/luna/script.js
@@ -4,14 +4,14 @@ include(["Functions", "Net", "Resource"]);
 
 Wine.prototype.luna = function() {
     var lunaStyle = new Resource()
-        .wizard(setupWizard)
+        .wizard(this._wizard)
         .url("http://repository.playonlinux.com/divers/luna.msstyles")
         .checksum("50a71767f90c1d3d86ca188a84393f2d39664311")
         .name("luna.msstyles")
         .get();
 
     var lunaReg = new Resource()
-        .wizard(setupWizard)
+        .wizard(this._wizard)
         .url("http://repository.playonlinux.com/divers/luna.reg")
         .checksum("074e655d391ae87527f4cc50ba822a8aad83a09f")
         .name("luna.reg")
