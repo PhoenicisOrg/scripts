0ac8fbf1965929d55896e85301dc4c3a50bad636
Philipp Hoppermann <plata@mailbox.org>
2016-12-30 18:08:47 +0100

    added Hexcells

---
+++ Games/Hexcells/Online/script.js
@@ -0,0 +1,10 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Hexcells")
+    .editor("Matthew Brown")
+    .editorUrl("http://store.steampowered.com/app/265890")
+    .author("Plata")
+    .appId(265890)
+    .category("Games")
+    .go();
