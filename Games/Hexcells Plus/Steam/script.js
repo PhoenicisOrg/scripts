4bcd376518793edaa455ec7c71a5ddca8fbda2b3
Philipp Hoppermann <plata@mailbox.org>
2016-12-30 18:57:38 +0100

    added Hexcells Plus

---
+++ Games/Hexcells Plus/Steam/script.js
@@ -0,0 +1,10 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Hexcells Plus")
+    .editor("Matthew Brown")
+    .editorUrl("http://store.steampowered.com/app/271900")
+    .author("Plata")
+    .appId(271900)
+    .category("Games")
+    .go();
