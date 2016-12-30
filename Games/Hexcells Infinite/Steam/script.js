82622fda77814357c4aee117a1f5cc0fdaaca551
Philipp Hoppermann <plata@mailbox.org>
2016-12-30 19:12:50 +0100

    added Hexcells Infinite

---
+++ Games/Hexcells Infinite/Steam/script.js
@@ -0,0 +1,10 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Hexcells Infinite")
+    .editor("Matthew Brown")
+    .editorUrl("http://store.steampowered.com/app/304410")
+    .author("Plata")
+    .appId(304410)
+    .category("Games")
+    .go();
