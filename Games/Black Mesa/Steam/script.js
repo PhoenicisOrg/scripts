4490eb8db08205baf5414538648b2579d1759d22
Quentin PÂRIS <git@quentin.paris>
2016-12-31 00:33:53 +0100

    Black Mesa

---
+++ Games/Black Mesa/Steam/script.js
@@ -0,0 +1,9 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Black Mesa")
+    .editor("Crowbar Collective")
+    .author("Quentin PARIS")
+    .appId(362890)
+    .category("Games")
+    .go();
