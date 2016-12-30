96a46485b0bb90a57483bf42bc74b99dedcf4c5d
Quentin PÂRIS <git@quentin.paris>
2016-12-31 00:28:36 +0100

    Age Of Empires

--- Games/Age Of Empires II HD/Steam/script.js
+++
@@ -1,9 +0,0 @@
-include(["Functions", "QuickScript", "SteamScript"]);
-
-new SteamScript()
-    .name("Age Of Empires II HD")
-    .editor("Microsoft Studios")
-    .author("Quentin PARIS")
-    .appId(221380)
-    .category("Games")
-    .go();
