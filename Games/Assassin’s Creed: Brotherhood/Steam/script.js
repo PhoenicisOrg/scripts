0485557ec4054bec690c9b46ca54e226b3e6b0cc
Quentin PÂRIS <git@quentin.paris>
2016-12-31 22:39:31 +0100

    AC: Brotherhood

--- Games/Assassin’s Creed: Brotherhood/Steam/script.js
+++ Games/Assassin’s Creed: Brotherhood/Steam/script.js
@@ -1,4 +1,6 @@
 include(["Functions", "QuickScript", "SteamScript"]);
+include(["Functions", "Verbs", "d3dx9"]);
+include(["Functions", "Verbs", "crypt32"]);
 
 new SteamScript()
     .name("Assassin’s Creed® Brotherhood")
@@ -7,5 +9,6 @@ new SteamScript()
     .appId(48190)
     .preInstall(function(wine) {
         wine.crypt32();
+        wine.d3dx9();
     })
     .go();
