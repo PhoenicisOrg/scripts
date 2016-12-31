8a9f80b004fd3394e94699655a55143efa848072
Quentin PÂRIS <git@quentin.paris>
2016-12-31 20:19:45 +0100

    Crypt32 + SP3 verbs

--- Functions/Verbs/sp3extract/script.js
+++ Functions/Verbs/sp3extract/script.js
@@ -13,7 +13,7 @@ Wine.prototype.sp3extract = function(fileToExtract) {
     var setupFile = new Resource()
         .wizard(this._wizard)
         .url("http://download.windowsupdate.com/msdownload/update/software/svpk/2008/04/windowsxp-kb936929-sp3-x86-enu_c81472f7eeea2eca421e116cd4c03e2300ebfde4.exe")
-        .checksum("4fc3bf0dc96b5cf5ab26430fac1c33c5c50bd142")
+        .checksum("ba5cc6058e65a67e9d7f75375e94cf552a9c56bc")
         .name("windowsxp-kb936929-sp3-x86-enu_c81472f7eeea2eca421e116cd4c03e2300ebfde4.exe")
         .get();
 
