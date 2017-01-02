5d10101d9b42c1dc1eed8d57ee726a1535b17d33
Quentin PÂRIS <git@quentin.paris>
2017-01-02 22:07:18 +0100

    MFC42 verb

---
+++ Functions/Verbs/mfc42/script.js
@@ -0,0 +1,31 @@
+include(["Functions", "Engines", "Wine"]);
+include(["Functions", "Net", "Resource"]);
+
+Wine.prototype.mfc42 = function() {
+    var setupFile = new Resource()
+        .wizard(this._wizard)
+        .url("http://download.microsoft.com/download/vc60pro/Update/2/W9XNT4/EN-US/VC6RedistSetup_deu.exe")
+        .checksum("a8c4dd33e281c166488846a10edf97ff0ce37044")
+        .name("VC6RedistSetup_deu.exe")
+        .get();
+
+    remove(this.system32directory() + "/mfc42.dll");
+    remove(this.system32directory() + "/mfc42u.dll");
+
+    new CabExtract()
+        .archive(setupFile)
+        .wizard(this._wizard)
+        .to(this.system32directory())
+        .extract();
+
+    new CabExtract()
+        .archive(this.system32directory() + "/vcredist.exe")
+        .wizard(this._wizard)
+        .to(this.system32directory())
+        .extract(['-F', 'mfc42*.dll']);
+
+    this.overrideDLL()
+        .set("native, builtin", ["mfc42.dll", "mfc42u.dll"])
+        .do();
+    return this;
+};
