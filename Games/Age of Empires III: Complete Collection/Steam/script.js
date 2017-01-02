025c07172f03cecc28690415a88af9f2b0bf1da8
Quentin PÂRIS <git@quentin.paris>
2017-01-02 21:47:04 +0100

    Ages Of Empires III

---
+++ Games/Age of Empires III: Complete Collection/Steam/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Age of Empires® III: Complete Collection")
+    .editor("Microsoft Studios")
+    .author("Quentin PARIS")
+    .appId(105450)
+    .go();
