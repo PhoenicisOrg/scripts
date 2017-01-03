3ec2abfba924b94b165a7de16fdf7de6cf8a0143
Philipp Hoppermann <plata@mailbox.org>
2017-01-03 21:19:35 +0100

    check architecture

--- Functions/Verbs/vcrun2013/script.js
+++ Functions/Verbs/vcrun2013/script.js
@@ -10,18 +10,20 @@ Wine.prototype.vcrun2013 = function() {
         .name("vcredist_x86.exe")
         .get();
 
-    var setupFile64 = new Resource()
-        .wizard(this._wizard)
-        .url("http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe")
-        .checksum("8bf41ba9eef02d30635a10433817dbb6886da5a2")
-        .name("vcredist_x64.exe")
-        .get();
-
     this.run(setupFile32, "/q")
         .wait("Please wait while {0} is installed ...".format("Microsoft Visual C++ 2013 Redistributable (x86)"));
 
-    this.run(setupFile64, "/q")
-        .wait("Please wait while {0} is installed ...".format("Microsoft Visual C++ 2013 Redistributable (x86)"));
+    if (this.architecture() == "amd64") {
+        var setupFile64 = new Resource()
+            .wizard(this._wizard)
+            .url("http://download.microsoft.com/download/2/E/6/2E61CFA4-993B-4DD4-91DA-3737CD5CD6E3/vcredist_x64.exe")
+            .checksum("8bf41ba9eef02d30635a10433817dbb6886da5a2")
+            .name("vcredist_x64.exe")
+            .get();
+
+        this.run(setupFile64, "/q")
+            .wait("Please wait while {0} is installed ...".format("Microsoft Visual C++ 2013 Redistributable (x86)"));
+    }
 
     this.overrideDLL()
         .set("native, builtin", ["atl120.dll", "msvcp120.dll", "msvcr120.dll", "vcomp120.dll"])
