0549388ad09715b0809709e14c384170dbd9088c
Philipp Hoppermann <plata@mailbox.org>
2017-01-04 21:25:19 +0100

    check architecture

--- Functions/Verbs/vcrun2015/script.js
+++ Functions/Verbs/vcrun2015/script.js
@@ -10,18 +10,20 @@ Wine.prototype.vcrun2015 = function() {
         .name("vc_redist.x86.exe")
         .get();
 
-    var setupFile64 = new Resource()
-        .wizard(this._wizard)
-        .url("https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe")
-        .checksum("3155cb0f146b927fcc30647c1a904cd162548c8c")
-        .name("vc_redist.x64.exe")
-        .get();
-
     this.run(setupFile32, "/q")
         .wait("Please wait while {0} is installed ...".format("Microsoft Visual C++ 2015 Redistributable (x86)"));
 
-    this.run(setupFile64, "/q")
-        .wait("Please wait while {0} is installed ...".format("Microsoft Visual C++ 2015 Redistributable (x86)"));
+    if (this.architecture() == "amd64") {
+        var setupFile64 = new Resource()
+            .wizard(this._wizard)
+            .url("https://download.microsoft.com/download/9/3/F/93FCF1E7-E6A4-478B-96E7-D4B285925B00/vc_redist.x64.exe")
+            .checksum("3155cb0f146b927fcc30647c1a904cd162548c8c")
+            .name("vc_redist.x64.exe")
+            .get();
+
+        this.run(setupFile64, "/q")
+            .wait("Please wait while {0} is installed ...".format("Microsoft Visual C++ 2015 Redistributable (x64)"));
+    }
 
     var dlls = [
         "api-ms-win-crt-conio-l1-1-0.dll",
