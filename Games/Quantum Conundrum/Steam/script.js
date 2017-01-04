e4b8af74f793f755d5747466d71105273e958282
Philipp Hoppermann <plata@mailbox.org>
2017-01-04 21:52:04 +0100

    newer wine + vcrun2008

--- Games/Quantum Conundrum/Steam/script.js
+++ Games/Quantum Conundrum/Steam/script.js
@@ -1,12 +1,13 @@
 include(["Functions", "QuickScript", "SteamScript"]);
-include(["Functions", "Verbs", "msvc90"]);
+include(["Functions", "Verbs", "vcrun2008"]);
 
 new SteamScript()
     .name("Quantum Conundrum")
     .editor("Square Enix")
     .author("Plata")
     .appId(200010)
+    .wineVersion("1.9.23")
     .preInstall(function(wine, wizard) {
-        wine.msvc90();
+        wine.vcrun2008();
     })
     .go();
