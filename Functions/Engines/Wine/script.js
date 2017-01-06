6f9d24a8d0a75d4149d7889c6c44aaae95d1a834
Philipp Hoppermann <plata@mailbox.org>
2017-01-06 15:15:08 +0100

    use native application for certain file extension
    fixes: Implement wine.useNativeExtension() #39

--- Functions/Engines/Wine/script.js
+++ Functions/Engines/Wine/script.js
@@ -580,6 +580,44 @@ Wine.prototype.windowsVersion = function (version) {
     return this;
 };
 
+/**
+ * use native application for a certain file extension
+ * @param {string} [file extension (pdf, txt, rtf)]
+ * @returns {string|Wine}
+ */
+Wine.prototype.nativeApplication = function (extension) {
+    // FIXME: get
+    if (arguments.length == 0) {
+        return this.regedit().fetchValue(["HKEY_CLASSES_ROOT", "." + extension]);
+    }
+
+    // set
+    var mimetype = null;
+    switch (extension) {
+        case "pdf":
+            mimetype = "application/pdf";
+            break;
+        case "txt":
+            mimetype = "application/plain";
+            break;
+        case "rtf":
+            mimetype = "application/rtf";
+            break;
+        default:
+            throw "Could not determine mimetype for file extension \"" + extension +"\"";
+    }
+    var regeditFileContent =
+        "REGEDIT4\n" +
+        "\n" +
+        "[HKEY_CLASSES_ROOT\\." + extension + "]\n" +
+        "@=\"" + extension + "file\"\n" +
+        "\"Content Type\"=\"" + mimetype + "\"\n" +
+        "[HKEY_CLASSES_ROOT\\" + extension + "file\\Shell\\Open\\command]\n" +
+        "@=\"winebrowser \"%1\"\"";
+    this.regedit().patch(regeditFileContent);
+    return this;
+};
+
 var SetOsForApplication = function () {
     var that = this;
     that._regeditFileContent =
