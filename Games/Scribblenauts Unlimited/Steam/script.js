2eebaa0c615a36f8803e985697ade7df323963a0
Brainzyy <acescopezz@gmail.com>
2017-01-06 18:23:30 +0100

    Add Scribblenauts Unlimited

---
+++ Games/Scribblenauts Unlimited/Steam/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Scribblenauts Unlimited")
+    .editor("5th Cell Media")
+    .author("Brainzyy")
+    .appId(218680)
+    .go();
