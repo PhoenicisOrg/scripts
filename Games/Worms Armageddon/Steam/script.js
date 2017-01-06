56148637276241812b902358aed8d7860c43da01
Brainzyy <acescopezz@gmail.com>
2017-01-06 23:57:32 +0100

    Add 'Worms Armageddon'

---
+++ Games/Worms Armageddon/Steam/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Worms Armageddon")
+    .editor("Team17 Digital Ltd")
+    .author("Brainzyy")
+    .appId(217200)
+    .go();
