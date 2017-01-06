014d0db0779d8b6843a675d66de01fa47764ee7a
Philipp Hoppermann <plata@mailbox.org>
2017-01-06 15:54:19 +0100

    set managed only for NIKO.exe

--- Games/Niko: Through The Dream/Steam/script.js
+++ Games/Niko: Through The Dream/Steam/script.js
@@ -1,12 +1,13 @@
 include(["Functions", "QuickScript", "SteamScript"]);
+include(["Functions", "Verbs", "dotnet40"]);
 
 new SteamScript()
     .name("Niko: Through The Dream")
     .editor("Studio Paint")
     .author("Plata")
     .appId(296550)
-    .postInstall(function(wine) {
+    .postInstall(function(wine, wizard) {
         wine.dotnet40();
-        wine.setManaged(false);
+        wine.setManagedForApplication().set("NIKO.exe", false).do();
     })
     .go();
