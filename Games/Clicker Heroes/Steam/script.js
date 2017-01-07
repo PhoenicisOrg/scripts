82ce0aafb55b2875bdbf161cff5984154875f367
Brainzyy <acescopezz@gmail.com>
2017-01-08 00:49:11 +0100

    Add 'Clicker Heroes'

---
+++ Games/Clicker Heroes/Steam/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Clicker Heroes")
+    .editor("Playsaurus")
+    .author("Brainzyy")
+    .appId(363970)
+    .go();
