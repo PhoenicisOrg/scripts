ac634a100b24dea2f0cd04cbf29e96510f7ac3b5
Gijs Vermeulen <acescopezz@gmail.com>
2017-01-10 22:26:30 +0100

    Add Payday The Heist

---
+++ Games/PAYDAY™ The Heist/Steam/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("PAYDAY™ The Heist")
+    .editor("OVERKILL Software")
+    .author("Brainzyy")
+    .appId(24240)
+    .go();
