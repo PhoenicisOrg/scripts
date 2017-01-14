ca9797b60460eccf6e2de69e7ca961be655a5fc5
Philipp Hoppermann <plata@mailbox.org>
2017-01-14 12:00:07 +0100

    quicktime76
    fixes: implement quicktime76 #118

---
+++ Functions/Verbs/quicktime76/script.js
@@ -0,0 +1,17 @@
+include(["Functions", "Engines", "Wine"]);
+include(["Functions", "Net", "Resource"]);
+include(["Functions", "Verbs", "luna"]);
+
+Wine.prototype.quicktime76 = function() {
+    var setupFile = new Resource()
+        .wizard(this._wizard)
+        .url("http://appldnld.apple.com/QuickTime/041-0025.20101207.Ptrqt/QuickTimeInstaller.exe")
+        .checksum("1eec8904f041d9e0ad3459788bdb690e45dbc38e")
+        .name("QuickTimeInstaller.exe")
+        .get();
+
+    this.run(setupFile, ["ALLUSERS=1", "DESKTOP_SHORTCUTS=0", "QTTaskRunFlags=0", "QTINFO.BISQTPRO=1", "SCHEDULE_ASUW=0", "REBOOT_REQUIRED=No", "/qn"])
+        .wait("Please wait while {0} is installed ...".format("QuickTime"));
+
+    return this;
+};
