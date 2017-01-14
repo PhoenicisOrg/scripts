3867209a00e9d0ed1a814aa8b1f497a640927523
Philipp Hoppermann <plata@mailbox.org>
2017-01-14 16:33:02 +0100

    Assassin’s Creed II

---
+++ Games/Assassin’s Creed II/Steam/script.js
@@ -0,0 +1,9 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Assassin’s Creed II")
+    .editor("Ubisoft Montreal")
+    .author("Plata")
+    .appId(33230)
+    .wineVersion("1.9.23")
+    .go();
