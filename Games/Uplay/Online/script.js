a3322f02d66727e8b3b8e48d372515e42ba11871
Philipp Hoppermann <plata@mailbox.org>
2017-01-28 16:01:21 +0100

    use wine 2.0 staging

--- Games/Uplay/Online/script.js
+++ Games/Uplay/Online/script.js
@@ -8,7 +8,7 @@ new OnlineInstallerScript()
     .url("http://static3.cdn.ubi.com/orbit/launcher_installer/UplayInstaller.exe")
     .category("Games")
     .executable("UbisoftGameLauncher.exe")
-    .wineVersion("2.0-rc3")
+    .wineVersion("2.0")
     .wineDistribution("staging")
     .preInstall(function(wine, wizard) {
         wine.windowsVersion("vista");
