c2cda01ba4f7fe990afddcd495e5f6a6a9553e34
Philipp Hoppermann <plata@mailbox.org>
2017-01-28 11:50:13 +0100

    Ether One Redux: use wine stable

--- Games/Ether One Redux/Steam/script.js
+++ Games/Ether One Redux/Steam/script.js
@@ -5,7 +5,6 @@ new SteamScript()
     .editor("White Paper Games")
     .author("Plata")
     .appId(391920)
-    .wineVersion("1.9.23")
     .wineArchitecture("amd64")
     .executable("Steam.exe", ["-silent", "-applaunch", 391920, "-nosplash", "-opengl4"])
     .postInstall(function(wine, wizard) {
