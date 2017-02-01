33bd659ab587b4cc6ef3a6874d66df2b78d3a3ce
Plata <plata@mailbox.org>
2017-02-01 18:42:37 +0100

    implemented custom local installer

---
+++ Custom/LocalInstaller/Local/script.js
@@ -0,0 +1,9 @@
+include(["Functions", "QuickScript", "LocalInstallerScript"]);
+
+new LocalInstallerScript()
+    .editor("PlayOnLinux")
+    .applicationHomepage("https://www.playonlinux.com/en/")
+    .author("Plata")
+    .category("Custom")
+    .wineUserSettings(true)
+    .go();
