bb847a67fe5093ce03734c80f7109878a818ce23
Michael Pusterhofer <pusterhofer@student.tugraz.at>
2017-03-01 10:27:04 +0100

    update script.js

--- Games/League of Legends/BETA Client/script.js
+++ Games/League of Legends/BETA Client/script.js
@@ -1,21 +1,21 @@
 include(["Functions", "QuickScript", "CustomInstallerScript"]);
+include(["Functions", "Shortcuts","Wine"]);
 include(["Functions", "Verbs", "sandbox"]);
+include(["Functions", "Verbs", "d3dx9"]);
 include(["Functions", "Verbs", "vcrun2015"]);
 
 new CustomInstallerScript()
     .name("League of Legends BETA Client")
     .editor("Riot Games")
     .applicationHomepage("http://euw.leagueoflegends.com/")
-    .author("Plata")
+    .author("Plata,feanor12")
     .installationCommand(function(wizard) {
-        return {command: "msiexec", args: ["/i", "C://LoL/Lol.EUW.msi","APPDIR=C:\\LoL","/q"]};
+        return {command: "msiexec", args: ["/i", "C://LoL_tmp/Lol.EUW.msi","APPDIR=C:\\LoL","/q"]};
     })
     .category("Games")
     .wineDistribution("staging")
     .wineVersion("2.0")
     .preInstall(function(wine, wizard) {
-        wine.sandbox();
-
         var setupFile = new Resource()
             .wizard(wizard)
             .url("https://riotgamespatcher-a.akamaihd.net/ShellInstaller/EUW/LeagueofLegends_EUW_Installer_2016_11_10.exe")
@@ -23,10 +23,25 @@ new CustomInstallerScript()
             .name("LeagueofLegends_EUW_Installer_2016_11_10.exe")
             .get();
 
+        mkdir(wine.prefixDirectory+"drive_c/LoL_tmp");
         mkdir(wine.prefixDirectory+"drive_c/LoL");
-        wine.run(setupFile,["/extract:C:\\LoL","/exenoui"]).wait();
+        var launcher = wine.prefixDirectory+"drive_c/LoL/lol.launcher.admin.exe"
+        var client = wine.prefixDirectory+"drive_c/LoL/LeagueClient.exe"
+        lns(launcher,client)           
+        new WineShortcut()
+            .name("Beta Client")
+            .prefix("League of Legends BETA Client")
+            .search("LeagueClient.exe")
+            .create();
 
+        wine.run(setupFile,["/extract:C:\\LoL_tmp","/exenoui"]).wait();
+
+        wine.d3dx9();
         wine.vcrun2015();
+    })
+    .postInstall(function(wine,wizard) {
+        remove(wine.prefixDirectory+"drive_c/LoL_tmp")
+        remove(wine.prefixDirectory+"drive_c/LoL/LeagueClient.exe")
     })
     .executable("lol.launcher.admin.exe")
     .go();
