b8fa773987ba73e153da73d76e6860a7f91ffefc
odziom91 <odziomek91@gmail.com>
2017-04-19 18:36:26 +0200

    New games - 18.04.2017 - update
    Changelog:
    - comments removed

--- Games/18 Wheels of Steel Across America/18WoSAA/script.js
+++ Games/18 Wheels of Steel Across America/18WoSAA/script.js
@@ -1,10 +1,10 @@
 include(["Functions", "QuickScript", "LocalInstallerScript"]);
 
 new LocalInstallerScript()
-    .name("18 Wheels of Steel: Across America")     // name of the application
-    .editor("SCS Software")                         // editor of the application
-    .applicationHomepage("http://www.scssoft.com/") // application homepage
-    .author("odziom91")                             // author of this script (you)
-    .category("Games")                              // category
-    .executable("aa.exe")                           // exe name (for the shortcut)
+    .name("18 Wheels of Steel: Across America")     
+    .editor("SCS Software")                         
+    .applicationHomepage("http://www.scssoft.com/")
+    .author("odziom91")                             
+    .category("Games")                             
+    .executable("aa.exe")                          
     .go();
--- Games/Civilization V/Civ5/script.js
+++ Games/Civilization V/Civ5/script.js
@@ -1,8 +1,8 @@
 include(["Functions", "QuickScript", "SteamScript"]);
 
 new SteamScript()
-    .name("Civilization V")	// name of the game
-    .editor("Firaxis Games")	// editor of the game (see Steam store: developer)
-    .author("odziom91")		// author of this script (you)
-    .appId(8930)		// Steam application ID
+    .name("Civilization V")	
+    .editor("Firaxis Games")	
+    .author("odziom91")		
+    .appId(8930)		
     .go();
