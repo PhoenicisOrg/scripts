a81d4ef72722e180f7a0257b9db8be9591ff40ec
Plata <plata@mailbox.org>
2017-04-20 11:13:50 +0200

    18 Wheels of Steel Across America

---
+++ Games/18 Wheels of Steel Across America/18WoSAA/script.js
@@ -0,0 +1,10 @@
+include(["Functions", "QuickScript", "LocalInstallerScript"]);
+
+new LocalInstallerScript()
+    .name("18 Wheels of Steel: Across America")
+    .editor("SCS Software")
+    .applicationHomepage("http://www.scssoft.com/")
+    .author("odziom91")
+    .category("Games")
+    .executable("aa.exe")
+    .go();
