f45899a4a448912e2f1f24ca78ec3c99d78a8ae3
Plata <plata@mailbox.org>
2017-04-20 11:18:17 +0200

    Civilization V

---
+++ Games/Civilization V/Civ5/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Civilization V")
+    .editor("Firaxis Games")
+    .author("odziom91")
+    .appId(8930)
+    .go();
