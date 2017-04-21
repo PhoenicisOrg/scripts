ae352854eb072e07f66629ecea4723f860cd6a9a
odziom91 <odziomek91@gmail.com>
2017-04-21 15:50:11 +0200

    Add files via upload

---
+++ Games/Stronghold/Stronghold/script.js
@@ -0,0 +1,11 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+include(["Functions", "Engines", "Wine"]);
+
+
+new SteamScript()
+    .name("Stronghold")
+    .editor("FireFly Studios")
+    .author("odziom91")
+    .appId(40950)
+    .setVirtualDesktop = (1366, 768)
+    .go();
