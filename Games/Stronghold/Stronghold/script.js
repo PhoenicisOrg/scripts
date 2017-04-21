65bf1c793dbf5564f537793bddcad226c40fcd1e
odziom91 <odziomek91@gmail.com>
2017-04-21 16:07:40 +0200

    Add files via upload

--- Games/Stronghold/Stronghold/script.js
+++ Games/Stronghold/Stronghold/script.js
@@ -1,11 +1,17 @@
 include(["Functions", "QuickScript", "SteamScript"]);
 include(["Functions", "Engines", "Wine"]);
 
+var setupWizard = SetupWizard("Stronghold HD");
+
+setupWizard.presentation("Stronghold HD", "FireFly Studios", "http://#", "odziom91");
+
 var wine = new Wine()
+    .wizard(setupWizard)
     .prefix("Stronghold")
     .create()
     .setVirtualDesktop("1366", "768")
 .do();
+
 
 new SteamScript()
     .name("Stronghold")
