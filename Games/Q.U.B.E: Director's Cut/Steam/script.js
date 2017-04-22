32539d0027d0fb7bbdd68e2bff66c9192636d3ca
Plata <plata@mailbox.org>
2017-04-22 15:30:06 +0200

    Q.U.B.E: Director's Cut

---
+++ Games/Q.U.B.E: Director's Cut/Steam/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("QUBE: Director's Cut")
+    .editor("Toxic Games")
+    .author("Plata")
+    .appId(239430)
+    .go();
