bde9e036052554e1979ab71eece40ecead32f6f7
Plata <plata@mailbox.org>
2017-04-22 12:57:43 +0200

    fixed Assassin’s Creed Brotherhood

--- Games/Assassin’s Creed: Brotherhood/Steam/script.js
+++ Games/Assassin’s Creed: Brotherhood/Steam/script.js
@@ -1,14 +1,24 @@
 include(["Functions", "QuickScript", "SteamScript"]);
-include(["Functions", "Verbs", "d3dx9"]);
-include(["Functions", "Verbs", "crypt32"]);
 
 new SteamScript()
     .name("Assassin’s Creed® Brotherhood")
     .editor("Ubisoft")
-    .author("Quentin PARIS")
+    .author("Plata")
     .appId(48190)
-    .preInstall(function(wine, wizard) {
-        wine.crypt32();
-        wine.d3dx9();
+    .wineVersion("2.5")
+    .wineDistribution("staging")
+    .postInstall(function(wine, wizard) {
+        var tempFile = createTempFile("exe");
+
+        new Downloader()
+            .wizard(wizard)
+            .url("https://ubistatic3-a.akamaihd.net/orbit/launcher_installer/UplayInstaller.exe")
+            .to(tempFile)
+            .get();
+
+        wine.run(tempFile)
+            .wait("Please follow the steps of the Uplay setup.\n\nUncheck \"Run Uplay\" or close Uplay completely after the setup so that the installation of \"" + this._name + "\" can continue.");
+
+        wine.setOsForApplication().set("upc.exe", "winxp").do();
     })
     .go();
