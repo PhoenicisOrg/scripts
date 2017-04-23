505c4d9803d97403fcc33a4c68f02a5574fecb96
odziom91 <odziomek91@gmail.com>
2017-04-24 00:13:05 +0200

    Stronghold HD - version 2
    Changelog:
    - remove switch/case section,
    - split selected text from menu into two variables (x and y)

--- Games/Stronghold/Stronghold/script.js
+++ Games/Stronghold/Stronghold/script.js
@@ -16,27 +16,11 @@ new SteamScript()
 		];
 		var setupWizard = SetupWizard("Stronghold HD");
 		var selectedResolution = setupWizard.menu('Choose Virtual Desktop resolution:', resolutionModes);
-		var setupResolution = resolutionModes[selectedResolution.index];
-		switch (setupResolution) {
-		case "800x600":
-			wine.setVirtualDesktop(800, 600);
-			break;
-		case "1024x768":
-			wine.setVirtualDesktop(1024, 768);
-			break;
-		case "1280x720":
-			wine.setVirtualDesktop(1280, 720);
-			break;
-		case "1280x1024":
-			wine.setVirtualDesktop(1280, 1024);
-			break;
-		case "1366x768":
-			wine.setVirtualDesktop(1366, 768);
-			break;
-		case "1920x1080":
-			wine.setVirtualDesktop(1920, 1080);
-			break;
-		}
+		var setupResolution = selectedResolution.text;
+		var resolutionParts = setupResolution.split("x");
+		var x = resolutionParts[0];
+		var y = resolutionParts[1];
+		wine.setVirtualDesktop(x, y);
 		setupWizard.close();
      	})	
 	.go();
