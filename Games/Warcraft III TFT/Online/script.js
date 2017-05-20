c4d442aed2c1e3c8dd19a77ac2b4b6ebb304d7cf
FalseCAM <FalseCAM@users.noreply.github.com>
2017-05-20 15:53:30 +0200

    Warcraft III and Warcraft III: The Frozen Throne (#237)

---
+++ Games/Warcraft III TFT/Online/script.js
@@ -0,0 +1,11 @@
+include(["Functions", "QuickScript", "OnlineInstallerScript"]);
+
+new OnlineInstallerScript()
+    .name("Warcraft III")
+    .editor("Blizzard")
+    .applicationHomepage("https://eu.battle.net/shop/en/product/warcraft-iii-the-frozen-throne")
+    .author("FalseCAM")
+    .url("https://www.battle.net/download/getLegacy?product=W3XP&locale=en-US&os=WIN")
+    .category("Games")
+    .executable("Frozen Throne.exe")
+    .go();
