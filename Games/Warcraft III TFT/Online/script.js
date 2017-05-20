474a34760d18dc13b1613aa712e2728dfda8f2c0
FalseCAM <falsecam@gmail.com>
2017-05-20 14:10:23 +0200

    create shortcut by executable

--- Games/Warcraft III TFT/Online/script.js
+++ Games/Warcraft III TFT/Online/script.js
@@ -8,15 +8,5 @@ new OnlineInstallerScript()
     .author("FalseCAM")
     .url("https://www.battle.net/download/getLegacy?product=W3XP&locale=en-US&os=WIN")
     .category("Games")
-    .preInstall(function (wine, wizard) {
-        wine.prefix("Warcraft III")
-    })
-    .postInstall(function (wine, wizard){
-        new WineShortcut()
-        .name("Warcraft III TFT")
-        .prefix("Warcraft III")
-        .search("Frozen Throne.exe")
-        .miniature(["Games", "Warcraft III TFT"])
-        .create();
-    })
+    .executable("Frozen Throne.exe")
     .go();
