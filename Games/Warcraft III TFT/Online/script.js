7bcaae0d56c5bca9e71a7aae5615e13b15ef5702
FalseCAM <falsecam@gmail.com>
2017-05-20 13:35:16 +0200

    fixes for merge request

--- Games/Warcraft III TFT/Online/script.js
+++ Games/Warcraft III TFT/Online/script.js
@@ -1,11 +1,9 @@
 include(["Functions", "QuickScript", "OnlineInstallerScript"]);
-include(["Functions", "Verbs", "corefonts"]);
-include(["Functions", "Verbs", "vcrun2015"]);
 
 new OnlineInstallerScript()
     .name("Warcraft III")
     .editor("Blizzard")
-    .applicationHomepage("http://eu.battle.net/en/")
+    .applicationHomepage("http://eu.blizzard.com/en-gb/games/war3/")
     .author("FalseCAM")
     .url("https://www.battle.net/download/getLegacy?product=W3XP&locale=en-US&os=WIN")
     .category("Games")
