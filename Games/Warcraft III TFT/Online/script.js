46fd660cbaf6a78bbcb7f88c71d33ab2af1cb30d
FalseCAM <falsecam@gmail.com>
2017-05-20 12:19:33 +0200

    ...

--- Games/Warcraft III TFT/Online/script.js
+++ Games/Warcraft III TFT/Online/script.js
@@ -7,33 +7,7 @@ new OnlineInstallerScript()
     .editor("Blizzard")
     .applicationHomepage("http://eu.battle.net/en/")
     .author("FalseCAM")
-    .url("https://www.battle.net/download/getLegacy?product=WAR3&locale=en-US&os=WIN")
-    .category("Games")
-    .wineUserSettings(true)
-    .executable("Warcraft III.exe")
-    .wineVersion("2.7")
-    .wineDistribution("staging")
-    .preInstall(function (wine, wizard) {
-        wine.windowsVersion("winxp");
-        wine.corefonts();
-        wine.vcrun2015();
-    })
-    .go();
-
-new OnlineInstallerScript()
-    .name("Warcraft III")
-    .editor("Blizzard")
-    .applicationHomepage("http://eu.battle.net/en/")
-    .author("FalseCAM")
     .url("https://www.battle.net/download/getLegacy?product=W3XP&locale=en-US&os=WIN")
     .category("Games")
-    .wineUserSettings(true)
     .executable("Frozen Throne.exe")
-    .wineVersion("2.7")
-    .wineDistribution("staging")
-    .preInstall(function (wine, wizard) {
-        wine.windowsVersion("winxp");
-        wine.corefonts();
-        wine.vcrun2015();
-    })
     .go();
