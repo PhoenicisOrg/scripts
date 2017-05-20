494b52b65dec6d782ebed6b19b82acf6cb5ad560
FalseCAM <falsecam@gmail.com>
2017-05-20 13:46:14 +0200

    Warcraft III TFT uses Warcraft III wine prefix

--- Games/Warcraft III TFT/Online/script.js
+++ Games/Warcraft III TFT/Online/script.js
@@ -1,11 +1,14 @@
 include(["Functions", "QuickScript", "OnlineInstallerScript"]);
 
 new OnlineInstallerScript()
-    .name("Warcraft III")
+    .name("Warcraft III TFT")
     .editor("Blizzard")
     .applicationHomepage("http://eu.blizzard.com/en-gb/games/war3/")
     .author("FalseCAM")
     .url("https://www.battle.net/download/getLegacy?product=W3XP&locale=en-US&os=WIN")
     .category("Games")
     .executable("Frozen Throne.exe")
+    .preInstall(function (wine, wizard) {
+        wine.prefix("Warcraft III")
+    })
     .go();
