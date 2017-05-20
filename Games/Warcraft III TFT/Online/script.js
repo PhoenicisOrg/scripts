527f1b580af643ccc28187df6e90f7b51a8350ee
FalseCAM <falsecam@gmail.com>
2017-05-20 12:16:13 +0200

    added wc3 to wc3 tft

--- Games/Warcraft III TFT/Online/script.js
+++ Games/Warcraft III TFT/Online/script.js
@@ -3,7 +3,25 @@ include(["Functions", "Verbs", "corefonts"]);
 include(["Functions", "Verbs", "vcrun2015"]);
 
 new OnlineInstallerScript()
-    .name("Warcraft III TFT")
+    .name("Warcraft III")
+    .editor("Blizzard")
+    .applicationHomepage("http://eu.battle.net/en/")
+    .author("FalseCAM")
+    .url("https://www.battle.net/download/getLegacy?product=WAR3&locale=en-US&os=WIN")
+    .category("Games")
+    .wineUserSettings(true)
+    .executable("Warcraft III.exe")
+    .wineVersion("2.7")
+    .wineDistribution("staging")
+    .preInstall(function (wine, wizard) {
+        wine.windowsVersion("winxp");
+        wine.corefonts();
+        wine.vcrun2015();
+    })
+    .go();
+
+new OnlineInstallerScript()
+    .name("Warcraft III")
     .editor("Blizzard")
     .applicationHomepage("http://eu.battle.net/en/")
     .author("FalseCAM")
