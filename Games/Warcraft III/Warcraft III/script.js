476e36a672373fc93b91ffbfd0a74ec9387b3fe1
FalseCAM <falsecam@gmail.com>
2017-05-20 11:35:10 +0200

    added Warcraft III

---
+++ Games/Warcraft III/Warcraft III/script.js
@@ -0,0 +1,21 @@
+include(["Functions", "QuickScript", "OnlineInstallerScript"]);
+include(["Functions", "Verbs", "corefonts"]);
+include(["Functions", "Verbs", "vcrun2015"]);
+
+new OnlineInstallerScript()
+    .name("Warcraft III")
+    .editor("Blizzard")
+    .applicationHomepage("http://eu.battle.net/en/")
+    .author("FalseCAM")
+    .url("https://www.battle.net/download/getLegacy?product=WAR3&locale=en-US&os=WIN")
+    .category("Games")
+    .wineUserSettings(true)
+    .executable("Warcraft III.exe")
+    .wineVersion("2.7")
+    .wineDistribution("staging")
+    .preInstall(function (wine, wizard) {
+        wine.windowsVersion("winxp");
+        wine.corefonts();
+        wine.vcrun2015();
+    })
+    .go();
