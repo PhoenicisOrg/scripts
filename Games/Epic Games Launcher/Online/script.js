e3dc60948e823c22ebb06945dbe50397551b061c
plata <plata@users.noreply.github.com>
2017-06-04 12:11:14 +0200

    Epic Games Launcher (#231)
    * Epic Games Launcher

---
+++ Games/Epic Games Launcher/Online/script.js
@@ -0,0 +1,16 @@
+include(["Functions", "QuickScript", "OnlineInstallerScript"]);
+
+new OnlineInstallerScript()
+    .name("Epic Games Launcher")
+    .editor("Epic Games")
+    .applicationHomepage("https://www.unrealengine.com/download")
+    .author("Plata")
+    .url("https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi")
+    .checksum("efd4fddbf14f1c449f9868f3f47608794ed6acd4")
+    .installationArgs(["/q"])
+    .category("Games")
+    .executable("EpicGamesLauncher.exe", ["-SkipBuildPatchPrereq"])
+    .wineVersion("2.9")
+    .wineDistribution("staging")
+    .wineArchitecture("amd64")
+    .go();
