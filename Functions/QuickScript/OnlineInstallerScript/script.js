2379e7f369355f2f4f1230f2a9e606f09532b8a5
plata <plata@users.noreply.github.com>
2017-06-04 11:46:44 +0200

    OnlineInstallerScript: use correct file extension (#246)

--- Functions/QuickScript/OnlineInstallerScript/script.js
+++ Functions/QuickScript/OnlineInstallerScript/script.js
@@ -27,12 +27,14 @@ OnlineInstallerScript.prototype.installationArgs = function(installationArgs) {
 };
 
 OnlineInstallerScript.prototype._installationCommand = function(wizard) {
-    var installationFile = createTempFile("exe");
-
     // if no URL given, ask user
     if (!this._url) {
         this._url = wizard.textbox("Please select the download URL.");
     }
+
+    // get correct extension depending on URL
+    var extension = this._url.split('.').pop();
+    var installationFile = createTempFile(extension);
 
     new Downloader()
         .wizard(wizard)
