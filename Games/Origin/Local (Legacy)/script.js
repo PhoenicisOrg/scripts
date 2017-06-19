f317628c8a63b080c050e2dbf791832d41d1070e
plata <plata@users.noreply.github.com>
2017-06-19 18:13:08 +0200

    EA Origin (#260)

---
+++ Games/Origin/Local (Legacy)/script.js
@@ -0,0 +1,32 @@
+include(["Functions", "QuickScript", "LocalInstallerScript"]);
+include(["Functions", "Net", "Download"]);
+include(["Functions", "Filesystem", "Extract"]);
+
+new LocalInstallerScript()
+    .name("Origin")
+    .editor("Electronic Arts")
+    .applicationHomepage("https://www.origin.com/deu/en-us/store")
+    .author("Plata")
+    .category("Games")
+    .executable("Origin.exe")
+    .wineVersion("2.10")
+    .preInstall(function(wine, wizard) {
+        wizard.message(tr("When Origin launches, you will get an error message (\"Your update could not be completed.\"). This is ok. Just close the popup."));
+    })
+    .postInstall(function(wine, wizard) {
+        var originDir = wine.prefixDirectory + "drive_c/" + wine.programFiles() + "/Origin/";
+
+        new Downloader()
+            .wizard(wizard)
+            .url("https://origin-a.akamaihd.net/Origin-Client-Download/origin/live/OriginUpdate_9_12_0_34172.zip")
+            .checksum("c4a2a742f966efa0114bf8025699007ebbda4d8f")
+            .to(originDir + "OriginUpdate_9_12_0_34172.zip")
+            .get();
+
+        new Extractor()
+            .wizard(wizard)
+            .archive(originDir + "OriginUpdate_9_12_0_34172.zip")
+            .to(originDir)
+            .extract();
+    })
+    .go();
