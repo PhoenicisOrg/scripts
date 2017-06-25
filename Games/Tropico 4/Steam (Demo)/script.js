95ae362e8db96a8ecf637c05f8a6ba7e04c594b9
plata <plata@users.noreply.github.com>
2017-06-25 16:37:15 +0200

    Tropico 4 (#273)

---
+++ Games/Tropico 4/Steam (Demo)/script.js
@@ -0,0 +1,9 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Tropico 4 (Demo)")
+    .editor("Haemimont Games, Feral Interactive (Mac)")
+    .author("Plata")
+    .appId(57750)
+    .executable("Tropico4-demo.exe")
+    .go();
