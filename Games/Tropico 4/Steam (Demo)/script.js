929a6353aeab51a6f79ae0a08cf2495f751f38e0
Plata <plata@mailbox.org>
2017-06-25 16:15:25 +0200

    Tropico 4

---
+++ Games/Tropico 4/Steam (Demo)/script.js
@@ -0,0 +1,9 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Tropico 4 (Demo)")
+    .editor("2K Czech, Feral Interactive (Mac)")
+    .author("Plata")
+    .appId(57750)
+    .executable("Tropico4-demo.exe")
+    .go();
---
+++ Games/Tropico 4/Steam/script.js
@@ -0,0 +1,9 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Tropico 4")
+    .editor("2K Czech, Feral Interactive (Mac)")
+    .author("Plata")
+    .appId(57690)
+    .executable("Tropico4.exe")
+    .go();
