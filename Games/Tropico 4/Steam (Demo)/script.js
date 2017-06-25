090dca805048218ba748e43116b73c5d101436eb
Plata <plata@mailbox.org>
2017-06-25 16:21:37 +0200

    fixed editor

--- Games/Tropico 4/Steam (Demo)/script.js
+++ Games/Tropico 4/Steam (Demo)/script.js
@@ -2,7 +2,7 @@ include(["Functions", "QuickScript", "SteamScript"]);
 
 new SteamScript()
     .name("Tropico 4 (Demo)")
-    .editor("2K Czech, Feral Interactive (Mac)")
+    .editor("Haemimont Games, Feral Interactive (Mac)")
     .author("Plata")
     .appId(57750)
     .executable("Tropico4-demo.exe")
--- Games/Tropico 4/Steam/script.js
+++ Games/Tropico 4/Steam/script.js
@@ -2,7 +2,7 @@ include(["Functions", "QuickScript", "SteamScript"]);
 
 new SteamScript()
     .name("Tropico 4")
-    .editor("2K Czech, Feral Interactive (Mac)")
+    .editor("Haemimont Games, Feral Interactive (Mac)")
     .author("Plata")
     .appId(57690)
     .executable("Tropico4.exe")
