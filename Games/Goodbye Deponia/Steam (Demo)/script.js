324b043bab71177c88379170e382a81bc8def23b
plata <plata@users.noreply.github.com>
2017-07-01 16:22:47 +0200

    Goodbye Deponia (#281)

---
+++ Games/Goodbye Deponia/Steam (Demo)/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Goodbye Deponia (Demo)")
+    .editor("Daedalic Entertainment")
+    .author("Plata")
+    .appId(262880)
+    .go();
