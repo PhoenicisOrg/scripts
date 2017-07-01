ede1bdece3dd4f22898331990fca48d35155f73d
plata <plata@users.noreply.github.com>
2017-07-01 17:41:19 +0200

    Tomb Raider: Legend (#286)

---
+++ Games/Tomb Raider Legend/Steam/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Tomb Raider: Legend")
+    .editor("Crystal Dynamics")
+    .author("Plata")
+    .appId(7000)
+    .go();
