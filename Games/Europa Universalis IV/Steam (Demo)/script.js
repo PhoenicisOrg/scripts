52e7d0a892724e8322f07bc994aa99861a4d12d8
Plata <plata@mailbox.org>
2017-07-01 14:01:18 +0200

    Europa Universalis IV

---
+++ Games/Europa Universalis IV/Steam (Demo)/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Europa Universalis IV (Demo)")
+    .editor("Paradox Development Studio")
+    .author("Plata")
+    .appId(247890)
+    .go();
