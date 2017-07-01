416c946fe385c0915a36f3bb542bc9a89097eb2d
Plata <plata@mailbox.org>
2017-07-01 18:16:15 +0200

    Tomb Raider: Anniversary

---
+++ Games/Tomb Raider: Anniversary/Steam (Demo)/script.js
@@ -0,0 +1,9 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Tomb Raider: Anniversary (Demo)")
+    .editor("Crystal Dynamics, Feral Interactive (Mac)")
+    .author("Plata")
+    .appId(8030)
+    .executable("tra.exe")
+    .go();
---
+++ Games/Tomb Raider: Anniversary/Steam/script.js
@@ -0,0 +1,9 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Tomb Raider: Anniversary")
+    .editor("Crystal Dynamics, Feral Interactive (Mac)")
+    .author("Plata")
+    .appId(8000)
+    .executable("tra.exe")
+    .go();
