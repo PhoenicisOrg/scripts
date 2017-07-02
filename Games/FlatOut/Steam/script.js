41b4ab4cd39138ef8a61ba73feda35169cd8ad74
Plata <plata@mailbox.org>
2017-07-02 12:21:16 +0200

    FlatOut

---
+++ Games/FlatOut/Steam/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("FlatOut")
+    .editor("Bugbear Entertainment")
+    .author("Plata")
+    .appId(6220)
+    .go();
