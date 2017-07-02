d590f8c15ce875decf69d1f91e78b18662638f79
plata <plata@users.noreply.github.com>
2017-07-02 13:43:58 +0200

    FlatOut (#290)

---
+++ Games/FlatOut/Steam (Demo)/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("FlatOut (Demo)")
+    .editor("Bugbear Entertainment")
+    .author("Plata")
+    .appId(6230)
+    .go();
