61b80eb14d603a793b6cc035b6349cd6cbf2349e
plata <plata@users.noreply.github.com>
2017-07-02 11:36:50 +0200

    Tomb Raider: Underworld (#288)

---
+++ Games/Tomb Raider Underworld/Steam (Demo)/script.js
@@ -0,0 +1,9 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Tomb Raider: Underworld (Demo)")
+    .editor("Crystal Dynamics")
+    .author("Plata")
+    .appId(8150)
+    .executable("tru.exe")
+    .go();
---
+++ Games/Tomb Raider Underworld/Steam/script.js
@@ -0,0 +1,9 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Tomb Raider: Underworld")
+    .editor("Crystal Dynamics")
+    .author("Plata")
+    .appId(8140)
+    .executable("tru.exe")
+    .go();
