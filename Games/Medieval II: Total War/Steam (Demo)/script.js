584b55cb645689c0d9ad644c47795a082a90673e
Plata <plata@mailbox.org>
2017-07-02 13:39:30 +0200

    Medieval II: Total War

---
+++ Games/Medieval II: Total War/Steam (Demo)/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Medieval II: Total War™ (Demo)")
+    .editor("The Creative Assembly, Feral Interactive (Mac), Feral Interactive (Linux)")
+    .author("Plata")
+    .appId(4710)
+    .go();
---
+++ Games/Medieval II: Total War/Steam/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Medieval II: Total War™")
+    .editor("The Creative Assembly, Feral Interactive (Mac), Feral Interactive (Linux)")
+    .author("Plata")
+    .appId(4700)
+    .go();
