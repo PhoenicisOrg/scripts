bd2c600fdcd1b8f5f824676fcb1b9061948fb7b6
Plata <plata@mailbox.org>
2017-07-02 13:03:44 +0200

    BioShock

---
+++ Games/BioShock/Steam (Demo)/script.js
@@ -0,0 +1,8 @@
+include(["Functions", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("BioShock™ (Demo)")
+    .editor("2K Boston , 2K Australia")
+    .author("Plata")
+    .appId(7710)
+    .go();
