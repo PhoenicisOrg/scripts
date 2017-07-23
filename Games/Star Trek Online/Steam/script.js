66ba15a1d2510d15bd3623382506be8c95ba277d
ImperatorS79 <fevrier.simon@gmail.com>
2017-07-23 14:10:40 +0200

    New game Star Trek Online

---
+++ Games/Star Trek Online/Steam/script.js
@@ -0,0 +1,12 @@
+ 
+include(["Wine", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Star Trek Online")
+    .editor("Cryptic Studios")
+    .author("ImperatorS79")
+    .appId(9900)
+    .wineVersion("2.12")
+    .wineDistribution("staging")
+    
+.go();
