36fcbbc09a805fb0f618075b14834a7c20e53b32
Christian M <chriamue@users.noreply.github.com>
2017-07-24 19:06:43 +0200

    Update Steam July 2017
    Steam is crashing since last Steam update.
    Some crashes in net.cpp as described on this website: https://forum.level1techs.com/t/wine-crashes/117565
    I did test wine staging 2.12 and it is working.
    Tested only the Game AOE 2.

--- Games/Steam/Online/script.js
+++ Games/Steam/Online/script.js
@@ -8,5 +8,7 @@ new OnlineInstallerScript()
     .url("http://media.steampowered.com/client/installer/SteamSetup.exe")
     .checksum("e930dbdb3bc638f772a8fcd92dbcd0919c924318")
     .category("Games")
+    .wineDistribution("staging")
+    .wineVersion("2.12")
     .executable("Steam.exe")
     .go();
