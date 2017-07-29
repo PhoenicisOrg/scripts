6c3ee29de8d0f9031ba19994cf8e4af713422795
ImperatorS79 <fevrier.simon@gmail.com>
2017-07-29 12:28:39 +0200

    4 level JS namespace + end of script
    Still need some test from me ;)

--- Games/STAR WARS™ Empire at War - Gold Pack/Local/script.js
+++ Games/STAR WARS™ Empire at War - Gold Pack/Local/script.js
@@ -1,5 +1,7 @@
-include(["Wine", "QuickScript", "LocalInstallerScript"]);
-include(["Wine", "Verbs", "d3dx9"]);
+include(["Engines", "Wine", "QuickScript", "LocalInstallerScript"]);
+include(["Engines"," Wine", "Verbs", "d3dx9"]);
+include(["Engines", "Wine", "Engine", "Object"]);
+include(["Utils", "Functions", "Filesystem", "Files"]);
 
 new LocalInstallerScript()
     .name("STAR WARS™ Empire at War: Gold Pack")                       
@@ -10,4 +12,15 @@ new LocalInstallerScript()
     .preInstall(function (wine, wizard) {
         wine.d3dx9();
     })
+    .postInstall(function (wine, wizard) {
+        new Downloader()
+            .wizard(wizard)
+            .url("http://static.dolimg.com/mh_netstorage/lucasfilm/patches/pc/EAW_RAM_MPLobby_update.exe")
+            .checksum("9fdecb9609a17f7358f2984a3278b484")
+            .to(wine.prefixDirectory + "drive_c/users/Public/Documents/EAW_RAM_MPLobby_update.exe")
+            .get();
+        
+        wine.runInsidePrefix(wine.prefixDirectory + "drive_c/users/Public/Documents/EAW_RAM_MPLobby_update.exe","");
+    })
     .go(); 
+
