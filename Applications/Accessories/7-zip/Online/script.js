96e284338a3771fdc30669f1e61d2b2f6a7611bb
ImperatorS79 <fevrier.simon@gmail.com>
2017-08-02 19:38:08 +0200

    7-zip

---
+++ Applications/Accessories/7-zip/Online/script.js
@@ -0,0 +1,12 @@
+include(["Engines", "Wine", "QuickScript", "OnlineInstallerScript"]);
+
+new OnlineInstallerScript()
+    .name("7-zip")                       
+    .editor("Igor Pavlov")                               
+    .applicationHomepage("http://www.7-zip.org/")  
+    .author("ImperatorS79")                     
+    .url("http://www.7-zip.org/a/7z1604.exe")                       
+    .checksum("dd1cb1163c5572951c9cd27f5a8dd550b33c58a4")                       
+    .category("Accesories")                           
+    .executable("7zFM.exe")                  
+    .go(); 
