9bb21d8c68b533b31a5d655e177189223b1136cb
ImperatorS79 <fevrier.simon@gmail.com>
2017-08-02 21:02:49 +0200

    Add Caesar 3

---
+++ Applications/Games/Caesar III/Local/script.js
@@ -0,0 +1,13 @@
+include(["Engines", "Wine", "QuickScript", "LocalInstallerScript"])
+include(["Engines", "Wine", "Engine", "Object"]);;
+
+new LocalInstallerScript()
+    .name("Caesar III")                       
+    .editor("Impressions Games")                               
+    .author("ImperatorS79")                    
+    .category("Games")                          
+    .executable("c3.exe")
+    .postInstall(function(wine, wizard) {
+        wine.setVirtualDesktop(1280, 1024);
+    })
+    .go(); 
