318f8217f825ab980f43bf191ca81a9ddae4b7ef
ImperatorS79 <fevrier.simon@gmail.com>
2017-08-03 19:48:36 +0200

    Added Star Wars BF2 local installer (and patch 1.1) (#315)
    * Add Star Wars BF2 Local installer (with 1.1 patch)

---
+++ Applications/Games/STAR WARS Battlefront II/Local/script.js
@@ -0,0 +1,23 @@
+include(["Engines", "Wine", "QuickScript", "LocalInstallerScript"]);
+include(["Engines", "Wine", "Engine", "Object"]);
+include(["Utils", "Functions", "Filesystem", "Files"]);
+
+new LocalInstallerScript()
+    .name("STAR WARS™ Battlefront™ II")                       
+    .editor("Pandemic Studio")                               
+    .author("ImperatorS79")                     
+    .category("Games")                           
+    .executable("LaunchBFII.exe")                  
+    .postInstall(function(wine, wizard) {
+    // Install the 1.1 update of the game
+        new Downloader()
+            .wizard(wizard)
+            .url("http://static.dolimg.com/mh_netstorage/lucasfilm/patches/pc/BFIIUpdateInt1_1.exe")
+            .checksum("60eaddfaba1bc71fe8bbbb560f8da229748cfaa8")
+            .to(wine.prefixDirectory + "drive_c/Program Files/LucasArts/Star Wars Battlefront II/BFIIUpdateInt1_1.exe")
+            .get();
+            
+        wine.runInsidePrefix("/Program Files/LucasArts/Star Wars Battlefront II/BFIIUpdateInt1_1.exe");
+        
+    })
+    .go(); 
