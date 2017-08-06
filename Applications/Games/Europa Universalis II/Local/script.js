040e92ae868185c99fbab81918937a5da02a3f80
ImperatorS79 <fevrier.simon@gmail.com>
2017-08-06 12:23:32 +0200

    added Europa Universalis II (#318)

---
+++ Applications/Games/Europa Universalis II/Local/script.js
@@ -0,0 +1,10 @@
+include(["Engines", "Wine", "QuickScript", "LocalInstallerScript"]);
+
+new LocalInstallerScript()
+    .name("Europa Universalis II")                       
+    .editor("Ubisoft")                               
+    .author("ImperatorS79") 
+    .category("Games")                           
+    .executable("eu2.exe")
+    //TO DO : find static link to 1.07 and 1.09 update, and add a .postinstall. 
+    .go();
