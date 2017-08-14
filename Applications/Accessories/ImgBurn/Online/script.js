41085afb2d6e957fc48a8bf4395713527c9a4602
ImperatorS79 <fevrier.simon@gmail.com>
2017-08-14 18:32:29 +0200

    Added ImgBurn

---
+++ Applications/Accessories/ImgBurn/Online/script.js
@@ -0,0 +1,12 @@
+ include(["Engines", "Wine", "QuickScript", "OnlineInstallerScript"]);
+
+new OnlineInstallerScript()
+    .name("ImgBurn")                       
+    .editor("Lightning UK!")                               
+    .applicationHomepage("http://www.imgburn.com/")  
+    .author("ImperatorS79")                     
+    .url("http://download.imgburn.com/SetupImgBurn_2.5.8.0.exe")                       
+    .checksum("6fc013ed5944b13efc54648699ea80f304e37ad0")                      
+    .category("Accessories")                           
+    .executable("ImgBurn.exe")                  
+    .go(); 
