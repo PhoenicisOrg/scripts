88fb5baaf3c0c4b132a6354f591de5e7631ed486
plata <plata@users.noreply.github.com>
2017-08-18 13:25:57 +0200

    added Assassin's Creed III (#334)

---
+++ Applications/Games/Assassin's Creed III/Steam/script.js
@@ -0,0 +1,10 @@
+include(["Engines", "Wine", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Assassin’s Creed® III")
+    .editor("Ubisoft Montreal")
+    .author("Plata")
+    .appId(208480)
+    .wineVersion("2.14")
+    .wineDistribution("staging")
+    .go();
