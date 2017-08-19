cd5c34d84a010fe527248e449d0b05c8d8a97449
ImperatorS79 <fevrier.simon@gmail.com>
2017-08-19 17:50:42 +0200

    Update script.js

--- Applications/Games/Tom Clancy's Rainbow Six 3 : Raven Shield/Steam (Gold)/script.js
+++ Applications/Games/Tom Clancy's Rainbow Six 3 : Raven Shield/Steam (Gold)/script.js
@@ -4,5 +4,8 @@ new SteamScript()
     .name("Tom Clancy's Rainbow Six® 3 Gold")              
     .editor("Red Storm Entertainment")     
     .author("ImperatorS79")  
-    .appId(19830)               
+    .appId(19830)
+    .postInstall(function(wine, wizard) {
+        wine.setVirtualDesktop(1280, 1024);
+    })
     .go(); 
