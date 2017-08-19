35bb6974e7c57b9460baab428e7449351a9f1fdc
ImperatorS79 <fevrier.simon@gmail.com>
2017-08-19 16:53:33 +0200

    Added Tom Clancy's Rainbow Six 3

---
+++ Applications/Games/Tom Clancy's Rainbow Six 3 : Raven Shield/Steam (Gold)/script.js
@@ -0,0 +1,8 @@
+include(["Engines", "Wine", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Tom Clancy's Rainbow Six® 3 Gold")              
+    .editor("Red Storm Entertainment")     
+    .author("ImperatorS79")  
+    .appId(19830)               
+    .go(); 
