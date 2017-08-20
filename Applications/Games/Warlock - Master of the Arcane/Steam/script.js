3c2490b616fd9d54f77302f287cc9b1d3cbc4a3e
madoar <madoar@users.noreply.github.com>
2017-08-20 15:15:21 +0200

    updated Warlock - Master of the Arcane: set wine version to 2.14 (#357)
    Steam does not work with the previous version anymore.

--- Applications/Games/Warlock - Master of the Arcane/Steam/script.js
+++ Applications/Games/Warlock - Master of the Arcane/Steam/script.js
@@ -11,7 +11,7 @@ new SteamScript()
     .editor("Paradox Interactive")
     .author("madoar")
     .appId(203630)
-    .wineVersion("2.10")
+    .wineVersion("2.14")
     .wineDistribution("staging")
     .preInstall(function (wine, wizard) {
         wine.corefonts();
