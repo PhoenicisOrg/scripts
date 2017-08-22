69f703f630c9c7a78530903d438701d309b4be1f
ImperatorS79 <fevrier.simon@gmail.com>
2017-08-22 15:19:00 +0200

    added MS Office 2013 (#363)

---
+++ Applications/Office/Microsoft Office 2013/Local/script.js
@@ -0,0 +1,52 @@
+include(["Engines", "Wine", "QuickScript", "LocalInstallerScript"]);
+
+new LocalInstallerScript()
+    .name("Microsoft Office 2013")                       
+    .editor("Microsoft")                              
+    .applicationHomepage("https://products.office.com/fr-be/microsoft-office-2013")
+    .wineVersion("2.14")
+    .wineDistribution("staging")
+    .author("ImperatorS79")                     
+    .category("Office")                           
+    // exe set with WineShorcut
+    .postInstall(function(wine, wizard) {
+         wine.overrideDLL()
+            .set("native, builtin", ["riched20"])
+            .do();
+            
+        new WineShortcut()
+            .name("Microsoft Word 2013")
+            .prefix("Microsoft Office 2013")
+            .search("WINWORD.EXE")
+            .miniature(["Office", "Microsoft Office 2013"])
+            .create();
+            
+        new WineShortcut()
+            .name("Microsoft Excel 2013")
+            .prefix("Microsoft Excel 2013")
+            .search("EXCEL.EXE")
+            .miniature(["Office", "Microsoft Office 2013"])
+            .create();
+            
+        new WineShortcut()
+            .name("Microsoft PowePoint 2013")
+            .prefix("Microsoft Office 2013")
+            .search("POWERPNT.EXE")
+            .miniature(["Office", "Microsoft Office 2013"])
+            .create();
+            
+        new WineShortcut()
+            .name("Microsoft OneNote 2013")
+            .prefix("Microsoft Office 2013")
+            .search("ONENOTE.EXE")
+            .miniature(["Office", "Microsoft Office 2013"])
+            .create();
+            
+        new WineShortcut()
+            .name("Microsoft Outlook 2013")
+            .prefix("Microsoft Office 2013")
+            .search("OUTLOOK.EXE")
+            .miniature(["Office", "Microsoft Office 2013"])
+            .create();
+    })
+    .go();
