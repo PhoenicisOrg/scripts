0e6424db85db3effbc81c4395d67244a7697ecb7
plata <plata@users.noreply.github.com>
2017-08-23 12:16:38 +0200

    added BRINK (#367)

---
+++ Applications/Games/BRINK/Steam/script.js
@@ -0,0 +1,8 @@
+include(["Engines", "Wine", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("BRINK")
+    .editor("Splash Damage")
+    .author("Plata")
+    .appId(22350)
+    .go();
