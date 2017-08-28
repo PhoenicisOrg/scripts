48f05c2b80fb6c0509d249ab3f5e6d1605bde670
ImperatorS79 <fevrier.simon@gmail.com>
2017-08-28 18:10:06 +0200

    Added Assassin's Creed

---
+++ Applications/Games/Assassin's Creed/Steam/script.js
@@ -0,0 +1,10 @@
+include(["Engines", "Wine", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Assassin's Creed")              
+    .editor("Ubisoft")     
+    .author("ImperatorS79")
+    .wineVersion("2.15")
+    .wineDistribution("staging")
+    .appId(15100)               
+    .go(); 
