5c9d07c4b89d45fac599340f6550046fa463cd28
ImperatorS79 <fevrier.simon@gmail.com>
2017-08-28 18:41:55 +0200

    Update script.js

--- Applications/Games/Assassin's Creed/Steam/script.js
+++ Applications/Games/Assassin's Creed/Steam/script.js
@@ -2,7 +2,7 @@ include(["Engines", "Wine", "QuickScript", "SteamScript"]);
 
 new SteamScript()
     .name("Assassin's Creed™")              
-    .editor("Ubisoft")     
+    .editor("Ubisoft Montréal")     
     .author("ImperatorS79")
     .wineVersion("2.15")
     .wineDistribution("staging")
