f68aada0cf45182c44918a5f2b79e0fc3df27ced
ImperatorS79 <fevrier.simon@gmail.com>
2017-09-03 13:12:50 +0200

    Added Far Cry

---
+++ Applications/Games/Far Cry/Steam/script.js
@@ -0,0 +1,8 @@
+include(["Engines", "Wine", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Far Cry")              
+    .editor("Crytek GmbH")    
+    .author("ImperatorS79")  
+    .appId(13520)               
+    .go(); 
