98651c1fac8fa7e66e4d77bdaaf207cc2a8951d4
plata <plata@users.noreply.github.com>
2017-09-04 17:10:11 +0200

    added Tomb Raider: The Dagger Of Xian (#399)

---
+++ Applications/Games/Tomb Raider: The Dagger Of Xian/Online (Demo)/script.js
@@ -0,0 +1,15 @@
+include(["Engines", "Wine", "QuickScript", "ZipScript"]);
+
+new ZipScript()
+    .name("Tomb Raider: The Dagger Of Xian (Demo)")
+    .editor("Nicobass")
+    .applicationHomepage("http://tombraider-dox.com/")
+    .author("Plata")
+    .url("http://dl.cdn.chip.de/downloads/71953335/TombRaiderDOX.zip?cid=&platform=chip&1504439392-1504446892-310ba5-B-bc9c7f2bac140a936b39c49ca10f99cc.zip")
+    .checksum("f321a0db456227ba3f1e36bbead2b8564bfb26f5")
+    .category("Games")
+    .executable("TombRaiderDOX.exe")
+    .wineVersion("2.14")
+    .wineDistribution("staging")
+    .wineArchitecture("amd64")
+    .go();
