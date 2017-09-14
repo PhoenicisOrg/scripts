217945eaada2f8ddac7b03698975a36807819246
ImperatorS79 <fevrier.simon@gmail.com>
2017-09-14 13:38:31 +0200

    Added Star Craft 2

---
+++ Applications/Games/Star Craft II/Online/script.js
@@ -0,0 +1,19 @@
+include(["Engines", "Wine", "QuickScript", "OnlineInstallerScript"]);
+include(["Engines", "Wine", "Verbs", "vcrun2015"]);
+include(["Engines", "Wine", "Verbs", "corefonts"]);
+
+new OnlineInstallerScript()
+    .name("Star Craft II")                       
+    .editor("Blizzard")                               
+    .applicationHomepage("http://eu.battle.net/sc2/")  
+    .author("ImperatorS79")                    
+    .url("https://eu.battle.net/download/getInstaller?os=win&installer=StarCraft-II-Setup.exe")                       
+    // The checksum change each time you download                       
+    .category("Games")                           
+    .executable("StarCraft II.exe")
+	.preInstall(function(wine, wizard) {
+        wine.windowsVersion("winxp");
+        wine.vcrun2015();
+        wine.corefonts();
+	})	
+    .go();
