275a4e0f2a0fb57db65e5afffd238115f6dd1d09
ImperatorS79 <fevrier.simon@gmail.com>
2017-09-16 00:45:40 +0200

    Update script.js

--- Engines/Wine/Verbs/dotnet45/script.js
+++ Engines/Wine/Verbs/dotnet45/script.js
@@ -6,7 +6,7 @@ include(["Engines", "Wine", "Verbs", "dotnet40"]);
 
 /**
 * Inspired from dotnet40 verb, winetricks dotnet45 and POL4 POL_install_dotnet45
-* -> https://github.com/Winetricks/winetricks/blob/63bc6dbe612d017a0cb6bf6e4cde265162d75bca/src/winetricks#L8523
+* -> https://github.com/Winetricks/winetricks/blob/63bc6dbe612d017a0cb6bf6e4cde265162d75bca/src/winetricks#L6886
 * @returns {Wine}
 */
 Wine.prototype.dotnet45 = function() {
