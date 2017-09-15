f3ccea36b9cc7ab6c6b590e4a883ab9467e3a4c3
ImperatorS79 <fevrier.simon@gmail.com>
2017-09-15 20:06:24 +0200

    Added Overwatch

---
+++ Applications/Games/Overwatch/Online/script.js
@@ -0,0 +1,24 @@
+include(["Engines", "Wine", "QuickScript", "OnlineInstallerScript"]);
+include(["Engines", "Wine", "Verbs", "vcrun2015"]);
+include(["Engines", "Wine", "Verbs", "corefonts"]);
+
+new OnlineInstallerScript()
+    .name("Overwatch")                      
+    .editor("Blizzard")                              
+    .applicationHomepage("http://www.playoverwatch.com/")  
+    .author("ImperatorS79")                     
+    .url("https://eu.battle.net/download/getInstaller?os=win&installer=Overwatch-Setup.exe")                      
+    //The checksum is different each time you download
+    .wineVersion("LATEST_STAGING_VERSION")
+    .wineDistribution("staging")
+    .wineArchitecture("amd64")
+    .category("Games")                           
+    .executable("Overwatch.exe")
+    .preInstall(function(wine, wizard) {
+        wine.windowsVersion("winxp");
+        wine.vcrun2015();
+        wine.corefonts();
+        wine.enableCSMT();
+    })
+    
+    .go(); 
