e36d30528da4d63e7aed71ee8d04fea6db8b00f7
ImperatorS79 <fevrier.simon@gmail.com>
2017-09-17 21:28:10 +0200

    Added Elite:Dangerous

---
+++ Applications/Games/Elite:Dangerous/Steam/script.js
@@ -0,0 +1,21 @@
+include(["Engines", "Wine", "QuickScript", "SteamScript"]);
+include(["Engines", "Wine", "Verbs", "corefonts"]);
+include(["Engines", "Wine", "Verbs", "dotnet452"]);
+include(["Engines", "Wine", "Verbs", "vcrun2012"]);
+
+new SteamScript()
+    .name("Elite:Dangerous")             
+    .editor("Frontier Developments")     
+    .author("ImperatorS79")
+    .wineVersion(LATEST_STAGING_VERSION)
+    .wineDistribution("staging")
+    .wineArchitecture("amd64")
+    .preInstall(function(wine, wizard) {
+        wine.dotnet452();
+        wine.corefonts();
+        wine.vcrun2012();
+        wine.windowsVersion("win7");
+        wine.enableCSMT();
+    })
+    .appId(359320)               
+    .go(); 
