75162b45cd1e24dbaa73b90a6c38b3c47f356b14
Plata <plata@mailbox.org>
2017-09-23 11:52:25 +0200

    updated Tomb Raider: The Dagger Of Xian (installer available)

--- Applications/Games/Tomb Raider: The Dagger Of Xian/Local (Demo)/script.js
+++ Applications/Games/Tomb Raider: The Dagger Of Xian/Local (Demo)/script.js
@@ -1,12 +1,10 @@
-include(["Engines", "Wine", "QuickScript", "OnlineInstallerScript"]);
+include(["Engines", "Wine", "QuickScript", "LocalInstallerScript"]);
 
-new OnlineInstallerScript()
+new LocalInstallerScript()
     .name("Tomb Raider: The Dagger Of Xian (Demo)")
     .editor("Nicobass")
     .applicationHomepage("http://tombraider-dox.com/")
     .author("Plata")
-    .url("http://tr.soja.pw/setup-x86-TombRaider-DOX-Demo.exe")
-    .checksum("3d35b0a0ecd5cdf58ffb2f786158369befbd3f12")
     .category("Games")
     .executable("TombRaiderDOX.exe")
     .wineVersion(LATEST_STAGING_VERSION)
