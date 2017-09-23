db223aaff5667931c3b411d24596b73ca7d505cc
Plata <plata@mailbox.org>
2017-09-23 15:40:50 +0200

    added Guild Wars 2

---
+++ Applications/Games/Guild Wars 2/Local/script.js
@@ -0,0 +1,16 @@
+include(["Engines", "Wine", "QuickScript", "LocalInstallerScript"]);
+
+new LocalInstallerScript()
+    .name("Guild Wars 2")
+    .editor("ArenaNet")
+    .applicationHomepage("https://www.guildwars2.com")
+    .author("Plata")
+    .category("Games")
+    .executable("Gw2.exe")
+    .wineVersion(LATEST_STAGING_VERSION)
+    .wineDistribution("staging")
+    .preInstall(function(wine, wizard) {
+        // avoid that launcher freezes the complete system
+        wine.setVirtualDesktop(1280, 1024);
+    })
+    .go();
---
+++ Applications/Games/Guild Wars 2/Online/script.js
@@ -0,0 +1,18 @@
+include(["Engines", "Wine", "QuickScript", "OnlineInstallerScript"]);
+
+new OnlineInstallerScript()
+    .name("Guild Wars 2")
+    .editor("ArenaNet")
+    .applicationHomepage("https://www.guildwars2.com")
+    .author("Plata")
+    .url("https://s3.amazonaws.com/gw2cdn/client/branches/Gw2Setup.exe")
+    .checksum("febee41863ed7b844ea683f0931b9e7b0c9ee064")
+    .category("Games")
+    .executable("Gw2.exe")
+    .wineVersion(LATEST_STAGING_VERSION)
+    .wineDistribution("staging")
+    .preInstall(function(wine, wizard) {
+        // avoid that launcher freezes the complete system
+        wine.setVirtualDesktop(1280, 1024);
+    })
+    .go();
