f460d3f5826f581fed6aef6428792e97b1e86c12
ImperatorS79 <fevrier.simon@gmail.com>
2017-10-02 09:35:05 +0200

    added Warface (#443)

---
+++ Applications/Games/Warface/Steam/script.js
@@ -0,0 +1,13 @@
+include(["Engines", "Wine", "QuickScript", "SteamScript"]);
+
+new SteamScript()
+    .name("Warface")              
+    .editor("Crytek")     
+    .author("ImperatorS79")  
+    .appId(291480)
+    .wineVersion(LATEST_STAGING_VERSION)
+    .wineDistribution("staging")
+    .postInstall(function(wine, wizard) {
+        wine.enableCSMT();
+    })
+    .go(); 
