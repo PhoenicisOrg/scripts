c682671d0d75583ff6d9c108f840d8cb8e195b18
plata <plata@users.noreply.github.com>
2017-10-21 11:39:42 +0200

    updated Wine LATEST_STABLE_VERSION to 2.0.3 (#447)

--- Engines/Wine/Engine/Object/script.js
+++ Engines/Wine/Engine/Object/script.js
@@ -3,7 +3,7 @@ include(["Utils", "Functions", "Filesystem", "Extract"]);
 include(["Utils", "Functions", "Net", "Download"]);
 include(["Utils", "Functions", "Net", "Resource"]);
 
-LATEST_STABLE_VERSION = "2.0.2";
+LATEST_STABLE_VERSION = "2.0.3";
 LATEST_DEVELOPMENT_VERSION = "2.19";
 LATEST_STAGING_VERSION = "2.18";
 
