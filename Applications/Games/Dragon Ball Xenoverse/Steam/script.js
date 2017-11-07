04de87559c42b22099d514234af30c9e88ab9dd2
ImperatorS79 <fevrier.simon@gmail.com>
2017-11-07 22:09:47 +0100

    Update script.js

--- Applications/Games/Dragon Ball Xenoverse/Steam/script.js
+++ Applications/Games/Dragon Ball Xenoverse/Steam/script.js
@@ -4,11 +4,6 @@ new SteamScript()
     .name("Dragon Ball Xenoverse")
     .editor("DIMPS")
     .author("ImperatorS79")
-    .wineVersion(LATEST_STAGING_VERSION)
-    .wineDistribution("staging")
     .wineArchitecture("amd64")
     .appId(323470)
-    .preInstall(function(wine, wizard) {
-        wine.enableCSMT();
-    })
     .go();
