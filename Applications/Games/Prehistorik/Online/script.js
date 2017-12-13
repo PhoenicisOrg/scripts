9fd6a0f51acbc336fa69a6b3d7ff87f6ebdf24ae
Plata <plata@mailbox.org>
2017-12-13 19:48:59 +0100

    added Oldwares

---
+++ Applications/Games/Prehistorik/Online/script.js
@@ -0,0 +1,13 @@
+include(["Engines", "Wine", "QuickScript", "ZipScript"]);
+
+new ZipScript()
+    .name("Prehistorik")
+    .editor("Titus")
+    .applicationHomepage("")
+    .author("Quentin PÂRIS")
+    .url("http://repository.playonlinux.com/divers/oldware/historik.zip")
+    .checksum("62a21d0dfcd68ae61646e1bc5b1c4a03b3e1091f")
+    .category("Games")
+    .wineVersion("1.8.6-dos_support")
+    .executable("HISTORIK.EXE")
+    .go();
---
+++ Applications/Games/Prince Of Persia: Original/Online/script.js
@@ -0,0 +1,13 @@
+include(["Engines", "Wine", "QuickScript", "ZipScript"]);
+
+new ZipScript()
+    .name("Prince Of Persia: Original")
+    .editor("Broderbund Softwared")
+    .applicationHomepage("")
+    .author("Quentin PÂRIS")
+    .url("http://repository.playonlinux.com/divers/oldware/prince.zip")
+    .checksum("6c4148233f840011715c351c399d35b0fc716ae7")
+    .category("Games")
+    .wineVersion("1.8.6-dos_support")
+    .executable("PRINCE.EXE")
+    .go();
