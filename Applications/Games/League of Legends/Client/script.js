6d3b391539956add5b38e2de617b60290a893251
Thog <me@thog.eu>
2017-12-22 20:03:52 +0100

    Fix League of Legends
    - Rename "League of Legends BETA Client" to "League Of Legends" (It isn't in beta anymore)
    - Update installers checksums and installation method (That should fix
    any issues with the launcher)

--- Applications/Games/League of Legends/Client/script.js
+++ Applications/Games/League of Legends/Client/script.js
@@ -2,30 +2,15 @@ include(["Engines", "Wine", "QuickScript", "CustomInstallerScript"]);
 include(["Engines", "Wine", "Shortcuts","Wine"]);
 include(["Engines", "Wine", "Verbs", "sandbox"]);
 include(["Engines", "Wine", "Verbs", "d3dx9"]);
-include(["Engines", "Wine", "Verbs", "vcrun2015"]);
 
-// Installs League of Legends and creates two shortcuts.
-//
-// One shortcut called League of Legends which starts the launcher
-// and one shortcut called BETA Client which starts the BETA client
-//
-// Important: 
-// The BETA-Client has to be installed using the launcher.
-// After the upgrade the BETA Client will do the game patching.
-///////////////////////////////////////////////////////////////////
+// Installs League of Legends
 
 new CustomInstallerScript()
-    .name("League of Legends BETA Client")
+    .name("League of Legends")
     .editor("Riot Games")
     .applicationHomepage("http://leagueoflegends.com/")
-    .author("Plata, feanor12")
-    .installationCommand(function(/*wizard*/) {
-        return {command: "msiexec", args: ["/i", "C://LoL_tmp/LoL.msi", "APPDIR=C:\\LoL"]};
-    })
-    .category("Games")
-    .wineDistribution("staging")
-    .wineVersion(LATEST_STAGING_VERSION)
-    .preInstall(function(wine, wizard) {
+    .author("Plata, feanor12, Thog")
+    .installationCommand(function(wizard) {
         // Select the region and download the setup file
         ////////////////////////////////////////////////
         var regions = ["EU West",
@@ -45,52 +30,52 @@ new CustomInstallerScript()
             case "EU West":
                 regionID = "EUW";
                 url = baseUrl+"EUW/LeagueofLegends_EUW_Installer_2016_11_10.exe";
-                sha1 = "cbbf9883a59d2dada87e6aa8cc4484b07ea797da";
+                sha1 = "e8cb081395849f3753f8c00d558901b8c3df69e3";
                 break;
             case "Latin America North":
                 regionID = "LA1";
                 url = baseUrl+"LA1/LeagueofLegends_LA1_Installer_2016_05_26.exe";
-                sha1 = "3120d4a530bbc4c5978177ebf96260e87e3d31b8";
+                sha1 = "996ece64ba2ba9c8b9195c3519a6d7637d82b8d3";
                 break;
             case "Latin America South":
                 regionID = "LA2";
                 url = baseUrl+"LA2/LeagueofLegends_LA2_Installer_2016_05_27.exe";
-                sha1 = "f9c77681dbe07e7b05d5534234973302c878552b";
+                sha1 = "5c5e007ee266315b6433dd87e6692753170aab78";
                 break;
             case "Oceania":
                 regionID = "OC1";
                 url = baseUrl+"OC1/LeagueofLegends_OC1_Installer_2016_05_13.exe";
-                sha1 = "54d805e797a6ca9632cc83338cb67d5b904313f7";
+                sha1 = "74b9a327e66fc527edb86e9ea45c9798bdffec5f";
                 break;
             case "Japan":
                 regionID = "JP";
                 url = baseUrl+"JP/LeagueofLegends_JP_Installer_2016_05_31.exe";
-                sha1 = "5773f1e71aaaef2f9f908c3e3aa2a7eaa40387a7";
+                sha1 = "da25b973358837ef9abbb9cb3d55aae214a75de0";
                 break;
             case "Turkey":
                 regionID = "TR";
                 url = baseUrl+"TR/LeagueofLegends_TR_Installer_2016_11_08.exe";
-                sha1 = "1423ac48d1504b7c47d1acbf94e90f2c4a561d2d";
+                sha1 = "93fe9b3e581fa5cd41c504ef91ce55e4227b43e3";
                 break;
             case "Brasil":
                 regionID = "BR";
                 url = baseUrl+"BR/LeagueofLegends_BR_Installer_2016_05_13.exe";
-                sha1 = "6ee06e6fee6f44989f5cc78e97a143efdc3569c8";
+                sha1 = "fbef6186cb690b4259e63d53d8c73e556e1d5ddc";
                 break;
             case "EU Nordic & East":
                 regionID = "EUNE";
                 url = baseUrl+"EUNE/LeagueofLegends_EUNE_Installer_2016_11_10.exe";
-                sha1 = "bc1b68fe2007a82fbbe0a3d6231988795875f2ff";
+                sha1 = "17727c665ce59e3faf95c2c0db2fe5509383e750";
                 break;
             case "North America":
                 regionID = "NA";
                 url = baseUrl+"NA/LeagueofLegends_NA_Installer_2016_05_13.exe";
-                sha1 = "423a9760833c09f82b75d12ab7b73c7e8ae68cdb";
+                sha1 = "70a253f29351f1d6952fa1af39fb8b2b01bc6cde";
                 break;
             case "Russia":
                 regionID = "RU";
                 url = baseUrl+"RU/LeagueofLegends_RU_Installer_2016_05_13.exe";
-                sha1 = "c789127190217ee2dc922c105df74d9367e9a606";
+                sha1 = "2d462decf629cab880386407598f9c5ea6db2ef5";
                 break;
         }
         var setupFile = new Resource()
@@ -99,36 +84,25 @@ new CustomInstallerScript()
             .checksum(sha1)
             .name(fileName(url))
             .get();
-        
-        mkdir(wine.prefixDirectory+"drive_c/LoL_tmp");
-        mkdir(wine.prefixDirectory+"drive_c/LoL");
-        
-        // Create run script
-        /////////////////////////////////////////
-        var client = wine.prefixDirectory+"drive_c/LoL/run.bat";
-        var batContent = "IF EXIST \"C:\\LoL\\LeagueClient.exe\" (\n"+
-                         "start C:\\LoL\\LeagueClient.exe \n"+
-                         ") ELSE ( start C:\\LoL\\lol.launcher.admin.exe )";
-        writeToFile(client, batContent);  
 
-
-        // Extract the msi file and create a link to it
-        ///////////////////////////////////////////////
-        wine.run(setupFile, ["/extract:C:\\LoL_tmp", "/exenoui"]).wait();
-        var regionMSIPath = wine.prefixDirectory+"drive_c/LoL_tmp/LoL."+regionID+".msi";
-        var linkPath = wine.prefixDirectory+"drive_c/LoL_tmp/LoL.msi";
-        lns(regionMSIPath, linkPath);
-
-        wine.d3dx9();
-        wine.vcrun2015();
+        return {command: setupFile, args: ["--installdir", "C:\\LoL\\", "--mode", "unattended"]};
     })
-    .postInstall(function(wine/*, wizard*/) {
-        remove(wine.prefixDirectory+"drive_c/LoL_tmp");
+    .category("Games")
+    .wineDistribution("staging")
+    .wineVersion(LATEST_STAGING_VERSION)
+    .preInstall(function(win /*, wizard*/) {
+        wine.windowsVersion("winxp");
+        wine.d3dx9();
+        wine.overrideDLL().set("native, builtin", ["atl120", "msvcp120", "msvcr120", "vcomp120", "msvcp140"]).do();
+        wine.enableCSMT();
 
-        // Enable BETA
-        //////////////
-        var userConfPath = wine.prefixDirectory+"drive_c/LoL/RADS/system/user.cfg";
-        writeToFile(userConfPath,"leagueClientOptIn = yes\nreplayPopUpShown = yes");
+        mkdir(wine.prefixDirectory + "drive_c/LoL");
+
+        // Create run script to start the right exe
+        /////////////////////////////////////////
+        var client = wine.prefixDirectory + "drive_c/LoL/run.bat";
+        var batContent = "start C:\\LoL\\LeagueClient.exe";
+        writeToFile(client, batContent); 
     })
     .executable("run.bat")
     .go();
