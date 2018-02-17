b19ce8bb3798aed47644b519cb65714979650a5b
Zemogiter <zumi999@vp.pl>
2018-02-17 20:48:33 +0100

    Update script.js
    Removed unnecessary lines about dll overriding, added full name of the dev studio, removed line setting the OS version (wine 3.1 sets this to win7 by default), fixed indentation.

--- Applications/Games/Subnautica/Steam/script.js
+++ Applications/Games/Subnautica/Steam/script.js
@@ -5,28 +5,27 @@ include(["Engines", "Wine", "Verbs", "d3dx9_42"]);
 include(["Engines", "Wine", "Verbs", "xact"]);
 include(["Engines", "Wine", "Verbs", "corefonts"]);
 
+var screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+wine.setVirtualDesktop(screenSize.width, screenSize.height);
+
 new SteamScript()
-    .name("Subnautica")
-    .editor("Unknown Worlds")
-    .author("Zemogiter")
-    .applicationHomepage("https://unknownworlds.com/subnautica/")
-    .wineDistribution("upstream")
-    .wineVersion(3.1)
-    .wineArchitecture("amd64")
-    .category("Games")
-    .preInstall(function(wine, wizard) {
-        wine.windowsVersion("win7");
-        wine.vcrun2013();
-        wine.corefonts();
-	wine.vcrun2008();
-	wine.d3dx9_42();
-	wine.xact();
-})
-    this.overrideDLL()
-    .set("native,builtin", ["atl120", "d3dx9_42", "msvcr120", "msvcp120", "mvsvr90", "vcomp120"])
-    .set("disabled", ["dwrite", "gameoverlayrenderer"])
-    .do();
-    .appId(264710)
-    .executable("Steam.exe", ["-silent", "-applaunch", 264710, "-no-ces-sandbox"]) 
-.go();
-.print("Debug output");
+	.name("Subnautica")
+	.editor("Unknown Worlds Entertainment")
+	.author("Zemogiter")
+	.applicationHomepage("https://unknownworlds.com/subnautica/")
+	.wineDistribution("upstream")
+	.wineVersion(3.1)
+	.wineArchitecture("amd64")
+	.category("Games")
+	.preInstall(function(wine, wizard) {
+        	wine.vcrun2013();
+        	wine.corefonts();
+		wine.vcrun2008();
+		wine.d3dx9_42();
+		wine.xact();
+	})
+	.gameOverlay(false)
+	.appId(264710)
+	.executable("Steam.exe", ["-silent", "-applaunch", 264710, "-no-ces-sandbox"]) 
+	.go();
+	.print("Debug output");
