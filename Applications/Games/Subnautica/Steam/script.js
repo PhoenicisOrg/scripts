ed801a702bcf81008c641f600096f379ee338c9f
Zemogiter <zumi999@vp.pl>
2018-02-18 11:32:49 +0100

    Update script.js
    Removed the unnecesseary line about executable.

--- Applications/Games/Subnautica/Steam/script.js
+++ Applications/Games/Subnautica/Steam/script.js
@@ -1,12 +1,9 @@
 include(["Engines", "Wine", "QuickScript", "SteamScript"]);
 include(["Engines", "Wine", "Verbs", "vcrun2013"]);
 include(["Engines", "Wine", "Verbs", "vcrun2008"]);
-include(["Engines", "Wine", "Verbs", "d3dx9_42"]);
+include(["Engines", "Wine", "Verbs", "d3dx9"]);
 include(["Engines", "Wine", "Verbs", "xact"]);
 include(["Engines", "Wine", "Verbs", "corefonts"]);
-
-var screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-wine.setVirtualDesktop(screenSize.width, screenSize.height);
 
 new SteamScript()
 	.name("Subnautica")
@@ -16,16 +13,16 @@ new SteamScript()
 	.wineDistribution("upstream")
 	.wineVersion(3.1)
 	.wineArchitecture("amd64")
-	.category("Games")
+	.appId(264710)
 	.preInstall(function(wine, wizard) {
         	wine.vcrun2013();
         	wine.corefonts();
 		wine.vcrun2008();
-		wine.d3dx9_42();
+		wine.d3dx9();
 		wine.xact();
 	})
 	.gameOverlay(false)
-	.appId(264710)
-	.executable("Steam.exe", ["-silent", "-applaunch", 264710, "-no-ces-sandbox"]) 
 	.go();
-	.print("Debug output");
+
+var screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
+wine.setVirtualDesktop(screenSize.width, screenSize.height);
