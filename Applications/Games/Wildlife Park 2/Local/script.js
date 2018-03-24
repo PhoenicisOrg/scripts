2f7ec48eac34e4497470cb6342066bec33211744
Zemogiter <zumi999@vp.pl>
2018-03-24 11:44:51 +0100

    Update script.js

--- Applications/Games/Wildlife Park 2/Local/script.js
+++ Applications/Games/Wildlife Park 2/Local/script.js
@@ -9,7 +9,7 @@ new LocalInstallerScript()
     .category("Games")
     .executable("WLP2.exe")
     .preInstall(function(wine,wizzard) {
-	wizard.message(tr("On first run the game might not go into full screen. If that happens go to options and set the resolution to 1280x960. You will be asked to close the game in order to apply the new settings. Click Yes. Once you start the game again you should see a window when you can set your game resolution to match your screen."));
+	wizard.message(tr("On first run the game might not go into full screen. If that happens go to options and set the resolution to 1280x960. You will be asked to close the game in order to apply the new settings. Click Yes. Once you start the game again you should see a window where you can set your game resolution to match your screen."));
 	wine.quartz();
 	var screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
 	wine.setVirtualDesktop(screenSize.width, screenSize.height);
