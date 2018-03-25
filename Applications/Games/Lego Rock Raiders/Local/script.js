82b3f6d790d9799033c56c0814f861f29ac7650d
Zemogiter <zumi999@vp.pl>
2018-03-25 15:10:29 +0200

    Update script.js
    Fixed error when downloading .dll file, changed the md5 checksum because the previous one threw an non-fatal error (the new checksum comes from the log file)

--- Applications/Games/Lego Rock Raiders/Local/script.js
+++ Applications/Games/Lego Rock Raiders/Local/script.js
@@ -7,16 +7,16 @@ new LocalInstallerScript()
     .author("Zemogiter")
     .category("Games")
     .executable("LegoRR.exe")
-    .preInstall(function(wine, wizard) {
-    	wine.windowsVersion("nt40");
+    .preInstall(function(wine,wizard) {
+        wine.windowsVersion("nt40");
     })
-    .postInstall(function(wine, wizard) {
-        var GameDir = wine.prefixDirectory + "drive_c/" + wine.programFiles() + "/LEGO Media/Games/Rock Raiders/";
-        new Downloader()
-            .wizard(wizard)
-            .url("http://s2.pliki.info/5709/d3drm.dll")
-            .checksum("dde9e3b8c264957ae0a017d371293123")
-            .to(GameDir)
-            .get();
+    .postInstall(function(wine,wizard) {
+	    var GameDir = wine.prefixDirectory + "drive_c/" + wine.programFiles() + "/LEGO Media/Games/Rock Raiders/d3drm.dll";
+	    new Downloader()
+		    .wizard(wizard)
+		    .url("http://s2.pliki.info/5709/d3drm.dll")
+		    .checksum("af25767f425fa7877b996e5d29a54cab29b5ecfb")
+		    .to(GameDir)
+		    .get();
     })
     .go();
