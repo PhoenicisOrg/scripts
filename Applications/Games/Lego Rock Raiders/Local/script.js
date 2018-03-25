4281903bcf877d3218a5a5ec493315c1e3b25cc6
Zemogiter <zumi999@vp.pl>
2018-03-25 16:00:48 +0200

    Update script.js
    Fixed indentation so codacy should be fine. Added info about an WIP verb to (possibly) fix cutscenes not playing.

--- Applications/Games/Lego Rock Raiders/Local/script.js
+++ Applications/Games/Lego Rock Raiders/Local/script.js
@@ -1,5 +1,6 @@
 include(["Engines", "Wine", "QuickScript", "LocalInstallerScript"]);
 include(["Utils", "Functions", "Filesystem", "Files"]);
+//include(["Engines", "Wine", "Verbs", "iv50"]); this verb is WIP
 
 new LocalInstallerScript()
     .name("Lego Rock Raiders")
@@ -9,14 +10,15 @@ new LocalInstallerScript()
     .executable("LegoRR.exe")
     .preInstall(function(wine,wizard) {
         wine.windowsVersion("nt40");
+        //wine.iv50(); 
     })
     .postInstall(function(wine,wizard) {
-	    var GameDir = wine.prefixDirectory + "drive_c/" + wine.programFiles() + "/LEGO Media/Games/Rock Raiders/d3drm.dll";
-	    new Downloader()
-		    .wizard(wizard)
-		    .url("http://s2.pliki.info/5709/d3drm.dll")
-		    .checksum("af25767f425fa7877b996e5d29a54cab29b5ecfb")
-		    .to(GameDir)
-		    .get();
+        var GameDir = wine.prefixDirectory + "drive_c/" + wine.programFiles() + "/LEGO Media/Games/Rock Raiders/d3drm.dll";
+        new Downloader()
+            .wizard(wizard)
+            .url("http://s2.pliki.info/5709/d3drm.dll")
+            .checksum("af25767f425fa7877b996e5d29a54cab29b5ecfb")
+            .to(GameDir)
+            .get();
     })
     .go();
