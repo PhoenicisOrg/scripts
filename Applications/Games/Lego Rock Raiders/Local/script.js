1337ffd318a49b90c6087859feead4c898f3ce2d
Zemogiter <zumi999@vp.pl>
2018-03-26 18:48:56 +0200

    Update script.js
    This is a complete script with experimental `.preInstall` hook containing Indeo Codecs 5 installer. I was unable to get this to work and as such, the cutscenes are not working. Every other part of the game works though. Declared the Downloader function (shouldn't affect functionality, its just to fix codacy)

--- Applications/Games/Lego Rock Raiders/Local/script.js
+++ Applications/Games/Lego Rock Raiders/Local/script.js
@@ -1,6 +1,7 @@
 include(["Engines", "Wine", "QuickScript", "LocalInstallerScript"]);
 include(["Utils", "Functions", "Filesystem", "Files"]);
-//include(["Engines", "Wine", "Verbs", "iv50"]); this verb is WIP
+//include(["Utils", "Functions", "Apps", "Resources"]);
+//include(["Engines", "Wine", "Engine", "Object"]);
 
 new LocalInstallerScript()
     .name("Lego Rock Raiders")
@@ -8,13 +9,22 @@ new LocalInstallerScript()
     .author("Zemogiter")
     .category("Games")
     .executable("LegoRR.exe")
-    .preInstall(function(wine,wizard) {
+    /*.preInstall(function(wine,wizard) {
         wine.windowsVersion("nt40");
-        //wine.iv50(); 
-    })
+        var SetupFile = new Downloader()
+            .wizard(wizard)
+            .url("http://s3.amazonaws.com/moviecodec/files/iv5setup.exe")
+            .checksum("c5550ae11b1500e26c45dbbe87debfbbe4aa491a")
+            .to(wine.prefixDirectory + "drive_c/iv5setup.exe")
+            .get();
+        wine.run(wine.prefixDirectory + "drive_c/iv5setup.exe")
+        .wait(tr("Please wait while {0} is installed ...", "Indeo Codecs 5"));
+        var registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("fix.reg");
+        wine.regedit().patch(registrySettings);
+    })*/
     .postInstall(function(wine,wizard) {
         var GameDir = wine.prefixDirectory + "drive_c/" + wine.programFiles() + "/LEGO Media/Games/Rock Raiders/d3drm.dll";
-        new Downloader()
+        var DllFile = new Downloader()
             .wizard(wizard)
             .url("http://s2.pliki.info/5709/d3drm.dll")
             .checksum("af25767f425fa7877b996e5d29a54cab29b5ecfb")
