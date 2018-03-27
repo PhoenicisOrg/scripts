5ba9ee21aedd0ccb58fd452fb4dc594addb33dbf
Zemogiter <zumi999@vp.pl>
2018-03-27 22:24:12 +0200

    Update script.js
    Reverted the change in `.preInstall` line

--- Applications/Games/Wildlife Park 2/Steam/script.js
+++ Applications/Games/Wildlife Park 2/Steam/script.js
@@ -7,7 +7,7 @@ new SteamScript()
     .applicationHomepage("www.wildlifepark2.com")
     .author("Zemogiter")
     .appId(264710)
-    .preInstall(function(wine,wizard) {
+    .preInstall(function(wine, wizard) {
 	wizard.message(tr("On first run the game might not go into full screen. If that happens go to options and set the resolution to 1280x960. You will be asked to close the game in order to apply the new settings. Click Yes. Once you start the game again you should see a window where you can set your game resolution to match your screen."));
 	wine.quartz();
 	var screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
