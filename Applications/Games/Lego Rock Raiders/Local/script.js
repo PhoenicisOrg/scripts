abade9cb9fcc8606b771337f12c4a32740e5f043
Zemogiter <zumi999@vp.pl>
2018-03-28 21:01:04 +0200

    Update script.js
    Removed unused code.

--- Applications/Games/Lego Rock Raiders/Local/script.js
+++ Applications/Games/Lego Rock Raiders/Local/script.js
@@ -1,7 +1,5 @@
 include(["Engines", "Wine", "QuickScript", "LocalInstallerScript"]);
 include(["Utils", "Functions", "Filesystem", "Files"]);
-//include(["Utils", "Functions", "Apps", "Resources"]);
-//include(["Engines", "Wine", "Engine", "Object"]);
 
 new LocalInstallerScript()
     .name("Lego Rock Raiders")
@@ -9,19 +7,6 @@ new LocalInstallerScript()
     .author("Zemogiter")
     .category("Games")
     .executable("LegoRR.exe")
-    /*.preInstall(function(wine,wizard) {
-        wine.windowsVersion("nt40");
-        var SetupFile = new Downloader()
-            .wizard(wizard)
-            .url("http://s3.amazonaws.com/moviecodec/files/iv5setup.exe")
-            .checksum("c5550ae11b1500e26c45dbbe87debfbbe4aa491a")
-            .to(wine.prefixDirectory + "drive_c/iv5setup.exe")
-            .get();
-        wine.run(wine.prefixDirectory + "drive_c/iv5setup.exe")
-        .wait(tr("Please wait while {0} is installed ...", "Indeo Codecs 5"));
-        var registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("fix.reg");
-        wine.regedit().patch(registrySettings);
-    })*/
     .postInstall(function(wine,wizard) {
         var GameDir = wine.prefixDirectory + "drive_c/" + wine.programFiles() + "/LEGO Media/Games/Rock Raiders/d3drm.dll";
         new Downloader()
