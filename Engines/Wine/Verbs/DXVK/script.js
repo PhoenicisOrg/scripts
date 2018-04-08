239cac11c7fa4c854900f205ee695749f958c8bb
ImperatorS79 <fevrier.simon@gmail.com>
2018-04-08 12:33:33 +0200

    Update script.js

--- Engines/Wine/Verbs/DXVK/script.js
+++ Engines/Wine/Verbs/DXVK/script.js
@@ -10,12 +10,13 @@ include(["Utils", "Functions", "Filesystem", "Files"]);
 Wine.prototype.DXVK = function() {
     print("NOTE: you need a driver that support Vulkan enough to run DXVK");
     print("NOTE: wine version should be greater or equal to 3.5");
+	
     var setupFile = new Resource()
         .wizard(this.wizard())
         .url("https://github.com/doitsujin/dxvk/releases/download/v0.41/dxvk-0.41.tar.gz")
         .checksum("4bbcb3020ba12a5a0cb7c388264579068b307bc6")
         .name("dxvk-0.41.tar.gz")
-        .get();
+        .get();	
 	
     new Extractor()
         .wizard(this.wizard())
@@ -27,8 +28,7 @@ Wine.prototype.DXVK = function() {
         cp(this.prefixDirectory + "/TMP/dxvk-0.41/x32/d3d11.dll", this.system32directory());
         cp(this.prefixDirectory + "/TMP/dxvk-0.41/x32/dxgi.dll", this.system32directory());
     }
-        
-        
+          
     if (this.architecture() == "amd64") {
         cp(this.prefixDirectory + "/TMP/dxvk-0.41/x32/d3d11.dll", this.system64directory());
         cp(this.prefixDirectory + "/TMP/dxvk-0.41/x32/dxgi.dll", this.system64directory());
