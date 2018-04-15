99b2652bfdbe12661bb208550d7d9a22b5c9ee0d
ImperatorS79 <fevrier.simon@gmail.com>
2018-04-15 13:07:43 +0200

    Update DXVK to version 0.42

--- Engines/Wine/Verbs/DXVK/script.js
+++ Engines/Wine/Verbs/DXVK/script.js
@@ -12,9 +12,9 @@ Wine.prototype.DXVK = function() {
 
     var setupFile = new Resource()
         .wizard(this.wizard())
-        .url("https://github.com/doitsujin/dxvk/releases/download/v0.41/dxvk-0.41.tar.gz")
+        .url("https://github.com/doitsujin/dxvk/releases/download/v0.42/dxvk-0.42.tar.gz")
         .checksum("4bbcb3020ba12a5a0cb7c388264579068b307bc6")
-        .name("dxvk-0.41.tar.gz")
+        .name("dxvk-0.42.tar.gz")
         .get();
 
     new Extractor()
@@ -24,16 +24,16 @@ Wine.prototype.DXVK = function() {
         .extract();
 
     if (this.architecture() == "x86") {
-        cp(this.prefixDirectory + "/TMP/dxvk-0.41/x32/d3d11.dll", this.system32directory());
-        cp(this.prefixDirectory + "/TMP/dxvk-0.41/x32/dxgi.dll", this.system32directory());
+        cp(this.prefixDirectory + "/TMP/dxvk-0.42/x32/d3d11.dll", this.system32directory());
+        cp(this.prefixDirectory + "/TMP/dxvk-0.42/x32/dxgi.dll", this.system32directory());
     }
 
     if (this.architecture() == "amd64") {
-        cp(this.prefixDirectory + "/TMP/dxvk-0.41/x32/d3d11.dll", this.system64directory());
-        cp(this.prefixDirectory + "/TMP/dxvk-0.41/x32/dxgi.dll", this.system64directory());
+        cp(this.prefixDirectory + "/TMP/dxvk-0.42/x32/d3d11.dll", this.system64directory());
+        cp(this.prefixDirectory + "/TMP/dxvk-0.42/x32/dxgi.dll", this.system64directory());
 
-        cp(this.prefixDirectory + "/TMP/dxvk-0.41/x64/d3d11.dll", this.system32directory());
-        cp(this.prefixDirectory + "/TMP/dxvk-0.41/x64/dxgi.dll", this.system32directory());
+        cp(this.prefixDirectory + "/TMP/dxvk-0.42/x64/d3d11.dll", this.system32directory());
+        cp(this.prefixDirectory + "/TMP/dxvk-0.42/x64/dxgi.dll", this.system32directory());
     }
 
     this.overrideDLL()
