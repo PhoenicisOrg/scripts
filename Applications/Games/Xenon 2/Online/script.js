1e7c83bb0808031506e81b4173c31f805a82117b
plata <plata@users.noreply.github.com>
2018-04-22 11:59:54 +0200

    Update Xenon 2

--- Applications/Games/Xenon 2/Online/script.js
+++ Applications/Games/Xenon 2/Online/script.js
@@ -7,7 +7,7 @@ var installerImplementation = {
             .editor("")
             .applicationHomepage("")
             .author("Quentin PÂRIS")
-            .url("http://repository.playonlinux.com/divers/oldware/Xenon2.zip")
+            .url("https://repository.playonlinux.com/divers/oldware/Xenon2.zip")
             .checksum("9b61e88cad02f663e76fe40a379319a4956546c2")
             .category("Games")
             .wineVersion("1.8.6-dos_support")
