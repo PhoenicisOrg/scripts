a22b35fa2ea6a7d8101de83e2b9373b2a713e5c3
plata <plata@users.noreply.github.com>
2018-04-22 11:59:20 +0200

    Update Prehistorik

--- Applications/Games/Prehistorik/Online/script.js
+++ Applications/Games/Prehistorik/Online/script.js
@@ -7,7 +7,7 @@ var installerImplementation = {
             .editor("Titus")
             .applicationHomepage("")
             .author("Quentin PÂRIS")
-            .url("http://repository.playonlinux.com/divers/oldware/historik.zip")
+            .url("https://repository.playonlinux.com/divers/oldware/historik.zip")
             .checksum("62a21d0dfcd68ae61646e1bc5b1c4a03b3e1091f")
             .category("Games")
             .wineVersion("1.8.6-dos_support")
