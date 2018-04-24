806c1c516de40828c6e43f6947abf16f866a5a0e
Plata <plata@mailbox.org>
2018-04-24 18:46:19 +0200

    Remove repeated imports

--- i18n/update_translations.py
+++ i18n/update_translations.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 from __future__ import print_function
-from __future__ import print_function
-from __future__ import print_function
-from __future__ import print_function
-from __future__ import print_function
-from __future__ import print_function
-from __future__ import print_function
-from __future__ import print_function
 import errno
 import fnmatch
 import json
