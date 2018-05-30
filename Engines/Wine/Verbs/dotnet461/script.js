f39067cf187f05e64c1925c7612fcd0b3c6b839d
Zemogiter <zumi999@vp.pl>
2018-05-30 18:25:16 +0200

    Update script.js
    Changed the implementation so it matches other .NET scripts.

--- Engines/Wine/Verbs/dotnet461/script.js
+++ Engines/Wine/Verbs/dotnet461/script.js
@@ -9,7 +9,8 @@ include(["engines", "wine", "verbs", "dotnet452"]);
 * @returns {Wine} Wine object
 */
 Wine.prototype.dotnet461 = function () {
-
+    var OSVersion = this.windowsVersion();
+    
     var setupFile = new Resource()
         .wizard(this._wizard)
         .url("https://download.microsoft.com/download/E/4/1/E4173890-A24A-4936-9FC9-AF930FE3FA40/NDP461-KB3102436-x86-x64-AllOS-ENU.exe")
@@ -19,23 +20,30 @@ Wine.prototype.dotnet461 = function () {
 
     this.uninstall("Mono");
 
-    this.run("reg", ["delete", "HKLM\Software\Microsoft\NET Framework Setup\NDP\v4", "/f"])
-        .wait(tr("Please wait ..."));
+    this.wizard().wait(tr("Please wait ..."));
+    this.run("reg", ["delete", "HKLM\Software\Microsoft\NET Framework Setup\NDP\v4", "/f"], null, false, true);
 
     remove(this.system32directory() + "/mscoree.dll");
    
     this.dotnet452()
+    this.windowsVersion("win7");
 
     this.overrideDLL()
         .set("builtin", ["fusion"])
         .do();
 
-    this.run(setupFile, [setupFile, "/q", "/c:\"install.exe /q\""])
-        .wait(tr("Please wait while {0} is installed ...", ".NET Framework 4.6.1"));
+    this.wizard().wait(tr("Please wait while {0} is installed ...", ".NET Framework 4.5.2"));
+    this.run(setupFile, [setupFile, "/q", "/c:\"install.exe /q\""], null, false, true);
 
     this.overrideDLL()
         .set("native", ["mscoree"])
         .do();
+    
+    this.windowsVersion(OSVersion);
+
+    if (OSVersion != "win2003") {
+        print(tr("{0} applications can have issues when windows version is not set to \"win2003\"", ".NET 4.5.2"));
+}
 
     return this;
 };
