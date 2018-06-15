592626fb8356c2a71d6bab9c45b2d695744f3e40
ImperatorS79 <fevrier.simon@gmail.com>
2018-06-15 13:46:57 +0200

    Update script.js

--- Engines/Wine/Verbs/VK9/script.js
+++ Engines/Wine/Verbs/VK9/script.js
@@ -56,7 +56,8 @@ Wine.prototype.VK9 = function () {
         .set("native", ["d3d9"])
         .do();
     
-    var userFilePath = this.wizard().browse("Please select the .exe directory", wine.prefixDirectory());
+    var directory = this.prefixDirectory();
+    var userFilePath = this.wizard().browse("Please select the .exe directory", directory);
     print(userFilePath);
 
     // Need to copy VK9.conf and shader into executable directory
