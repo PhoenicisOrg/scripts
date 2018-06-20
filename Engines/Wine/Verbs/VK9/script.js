f73f4694ad493b95c24211afdc12c20df5fec44e
ImperatorS79 <fevrier.simon@gmail.com>
2018-06-20 15:56:39 +0200

    Use 0.25.0 since 0.26.0 needs debug libs

--- Engines/Wine/Verbs/VK9/script.js
+++ Engines/Wine/Verbs/VK9/script.js
@@ -8,24 +8,21 @@ include(["utils", "functions", "filesystem", "files"]);
 * @returns {Wine} Wine object
 */
 Wine.prototype.VK9 = function () {
+    if (this.architecture() == "amd64") {
+        throw "{0} cannot curreently be installed in a 64bit wine prefix.".format("VK9");
+    }
+
     print("NOTE: you need a driver that supports Vulkan enough to run VK9");
     print("NOTE: wine version should be greater or equal to 3.5");
     print("NOTE: this is a debug dll");
 
-    var vk9Version = "0.26.0";
+    var vk9Version = "0.25.0";
 
     var setupFile32 = new Resource()
         .wizard(this.wizard())
-        .url("https://github.com/disks86/VK9/releases/download/" + vk9Version +"/"+ vk9Version + "-bin-x86-Debug.zip")
-        .checksum("bc8970ae03e705b9871748a16ec6fa21dda5073e")
-        .name(vk9Version + "-bin-x86-Debug")
-        .get();
-
-    var setupFile64 = new Resource()
-        .wizard(this.wizard())
-        .url("https://github.com/disks86/VK9/releases/download/" + vk9Version +"/"+ vk9Version + "-bin-x86_64-Debug.zip")
-        .checksum("4a5bfbe0242a4ddc55702660a36902f68e98caef")
-        .name(vk9Version + "-bin-x86_64-Debug")
+        .url("https://github.com/disks86/VK9/releases/download/" + vk9Version +"/"+ vk9Version + "-bin-Release.zip")
+        .checksum("9ec007090000c4923e502b0360b9ebfd304fbf05")
+        .name(vk9Version + "-bin-Realease.zip")
         .get();
 
     new Extractor()
@@ -34,28 +31,12 @@ Wine.prototype.VK9 = function () {
         .to(this.prefixDirectory() + "/TMP32/")
         .extract();
 
-    if (this.architecture() == "x86") {
-        cp(this.prefixDirectory() + "/TMP32/D3d9.dll", this.system32directory());
-    }
-
-    if (this.architecture() == "amd64") {
-        new Extractor()
-            .wizard(this.wizard())
-            .archive(setupFile64)
-            .to(this.prefixDirectory() + "/TMP64/")
-            .extract();
-
-        cp(this.prefixDirectory() + "/TMP32/D3d9.dll", this.system64directory());
-
-        cp(this.prefixDirectory() + "/TMP64/D3d9.dll", this.system32directory());
-
-        remove(this.prefixDirectory() + "/TMP64/");
-    }
+    cp(this.prefixDirectory() + "/TMP32/D3d9.dll", this.system32directory());
 
     this.overrideDLL()
         .set("native", ["d3d9"])
         .do();
-    
+
     var userFilePath = this.wizard().browse("Please select the .exe directory", this.prefixDirectory(), ["exe"]);
     print(userFilePath);
 
