dffbcedeefe832a95d7c273c98401d1bcd5eafc7
Zemogiter <zumi999@vp.pl>
2018-07-08 22:19:21 +0200

    Add Space Colony

---
+++ Applications/Games/Space Colony/Local/script.js
@@ -0,0 +1,27 @@
+include(["engines", "wine", "quick_script", "local_installer_script"]);
+
+var installerImplementation = {
+    run: function () {
+        new LocalInstallerScript()
+            .name("Space Colony")                       
+            .editor("Firefly Studios")                            
+            .applicationHomepage("http://www.spacecolonyhd.com/") 
+            .author("Zemogiter")           
+            .category("Games")                        
+            .executable("SpaceColony.exe")
+            .postInstall(function(wine,wizard){
+                var patch = new Resource()
+                    .wizard(this._wizard)
+                    .url("https://d1ztm8591kdhlc.cloudfront.net/hdpatches/Space_Colony_HD_Update.exe")
+                    .checksum("C821E5C7035B9B517823466F4CEDADD3")
+                    .algorithm("MD5")
+                    .name("Space_Colony_HD_Update.exe")
+                    .get();
+                this.run(patch, [/q])              
+            .go();
+    }
+};
+
+
+/* exported Installer */
+var Installer = Java.extend(org.phoenicis.scripts.Installer, installerImplementation);
---
+++ Applications/Games/Space Colony/Steam/script.js
@@ -0,0 +1,19 @@
+include(["engines", "wine", "quick_script", "steam_script"]);
+include(["engines", "wine", "verbs", "dotnet40"]);
+include(["engines", "wine", "verbs", "vcrun2010"]);
+
+var installerImplementation = {
+    run: function () {
+        new SteamScript()
+        .name("Space Colony")
+        .editor("Firefly Studios")
+        .author("Zemogiter")
+        .appId(297920)
+        .go();
+
+    }
+};
+
+
+/* exported Installer */
+var Installer = Java.extend(org.phoenicis.scripts.Installer, installerImplementation);
