1c178d5a14fde81053092237d150308452718259
Zemogiter <zumi999@vp.pl>
2018-09-01 13:22:18 +0200

    Update script.js
    After some more testing it turns out the Steam version of this game also requires d3dx9 verb or else it will crash before showing the launcher.

--- Applications/Games/Space Colony/Steam/script.js
+++ Applications/Games/Space Colony/Steam/script.js
@@ -1,6 +1,7 @@
 include(["engines", "wine", "quick_script", "steam_script"]);
 include(["engines", "wine", "verbs", "vcrun2010"]);
 include(["engines", "wine", "verbs", "dotnet40"]);
+include(["engines", "wine", "verbs", "d3dx9"]);
 
 var installerImplementation = {
     run: function () {
@@ -9,12 +10,13 @@ var installerImplementation = {
             .editor("Firefly Studios")
             .author("Zemogiter")
             .wineDistribution("upstream")
-            .wineVersion(3.15)
+            .wineVersion("3.15")
             .appId(297920)
-            .preInstall(function(wine) {
+            .preInstall(function (wine) {
                 wine.vcrun2010();
                 wine.dotnet40();
-        })
+                wine.d3dx9();
+            })
             .go();
     }
 };
