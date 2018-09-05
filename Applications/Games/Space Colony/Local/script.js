38919c8155a19fe92878599e4ebb2e8471abdbcc
Zemogiter <zumi999@vp.pl>
2018-09-05 19:34:18 +0200

    Update script.js

--- Applications/Games/Space Colony/Local/script.js
+++ Applications/Games/Space Colony/Local/script.js
@@ -8,21 +8,21 @@ var installerImplementation = {
             .editor("Firefly Studios")                            
             .applicationHomepage("http://www.spacecolonyhd.com/") 
             .author("Zemogiter")           
-            .category("Games")                        
-            .executable("SpaceColony.exe")
-            .postInstall(function(wine,wizard){
+            .category("Games")
+            .executable("Space Colony.exe")
+            .postInstall(function (wine,wizard){
                 var patch = new Resource()
                     .wizard(this._wizard)
                     .url("https://d1ztm8591kdhlc.cloudfront.net/hdpatches/Space_Colony_HD_Update.exe")
-                    .checksum("C821E5C7035B9B517823466F4CEDADD3")
+                    .checksum("c821e5c7035b9b517823466f4cedadd3")
                     .algorithm("MD5")
                     .name("Space_Colony_HD_Update.exe")
                     .get();
-                this.run(patch, ["/q"])              
-            .go();
+                wine.run(patch) 
+            })             
+            .go(["/q"]);
     }
 };
-
 
 /* exported Installer */
 var Installer = Java.extend(org.phoenicis.scripts.Installer, installerImplementation);
