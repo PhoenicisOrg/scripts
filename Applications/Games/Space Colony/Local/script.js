9de29b07152e9bb88cfdb50f487afc5097510d2c
Zemogiter <zumi999@vp.pl>
2018-09-05 20:15:15 +0200

    Update script.js
    Fixing trailing spaces

--- Applications/Games/Space Colony/Local/script.js
+++ Applications/Games/Space Colony/Local/script.js
@@ -4,13 +4,13 @@ include(["utils", "functions", "net", "resource"]);
 var installerImplementation = {
     run: function () {
         new LocalInstallerScript()
-            .name("Space Colony")                       
-            .editor("Firefly Studios")                            
-            .applicationHomepage("http://www.spacecolonyhd.com/") 
-            .author("Zemogiter")           
+            .name("Space Colony")
+            .editor("Firefly Studios")
+            .applicationHomepage("http://www.spacecolonyhd.com/")
+            .author("Zemogiter")
             .category("Games")
             .executable("Space Colony.exe")
-            .postInstall(function (wine,wizard){
+            .postInstall(function (wine, /*wizard*/){
                 var patch = new Resource()
                     .wizard(this._wizard)
                     .url("https://d1ztm8591kdhlc.cloudfront.net/hdpatches/Space_Colony_HD_Update.exe")
@@ -18,8 +18,8 @@ var installerImplementation = {
                     .algorithm("MD5")
                     .name("Space_Colony_HD_Update.exe")
                     .get();
-                wine.run(patch) 
-            })             
+                wine.run(patch)
+            })
             .go(["/q"]);
     }
 };
