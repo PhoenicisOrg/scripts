3e506497779cf31441842558ebe6cb28251397bc
Zemogiter <zumi999@vp.pl>
2018-09-07 19:21:21 +0200

    Update script.js
    Changed how the installation function is defined to match other scripts.

--- Applications/Games/Lego Rock Raiders/Local/script.js
+++ Applications/Games/Lego Rock Raiders/Local/script.js
@@ -1,19 +1,27 @@
 include(["engines", "wine", "quick_script", "local_installer_script"]);
 include(["utils", "functions", "net", "download"]);
 
-new LocalInstallerScript()
-    .name("Lego Rock Raiders")
-    .editor("LEGO Media")
-    .author("Zemogiter")
-    .category("Games")
-    .executable("LegoRR.exe")
-    .postInstall(function(wine,wizard) {
-        var GameDir = wine.prefixDirectory() + "drive_c/" + wine.programFiles() + "/LEGO Media/Games/Rock Raiders/d3drm.dll";
-        new Downloader()
-            .wizard(wizard)
-            .url("http://s2.pliki.info/5709/d3drm.dll")
-            .checksum("af25767f425fa7877b996e5d29a54cab29b5ecfb")
-            .to(GameDir)
-            .get();
-    })
-    .go();
+var installerImplementation = {
+    run: function() {
+        new LocalInstallerScript()
+            .name("Lego Rock Raiders")
+            .editor("LEGO Media")
+            .author("Zemogiter")
+            .category("Games")
+            .executable("LegoRR.exe")
+            .postInstall(function(wine,wizard) {
+                var GameDir = wine.prefixDirectory() + "drive_c/" + wine.programFiles() + "/LEGO Media/Games/Rock Raiders/d3drm.dll";
+                new Downloader()
+                .wizard(wizard)
+                .url("http://s2.pliki.info/5709/d3drm.dll")
+                .checksum("dde9e3b8c264957ae0a017d371293123")
+                .algorithm("MD5")
+                .to(GameDir)
+                .get();
+            })
+            .go();
+    }
+};
+
+/* exported Installer */
+var Installer = Java.extend(org.phoenicis.scripts.Installer, installerImplementation);
