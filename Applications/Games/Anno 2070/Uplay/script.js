b440ea62db311f6b4256ddbbef81a2e7a09850c9
Zemogiter <zumi999@vp.pl>
2018-09-15 11:07:21 +0200

    Update script.js
    Changing the Wine version. Uplay now works.

--- Applications/Games/Anno 2070/Uplay/script.js
+++ Applications/Games/Anno 2070/Uplay/script.js
@@ -1,7 +1,6 @@
 include(["engines", "wine", "quick_script", "uplay_script"]);
 include(["engines", "wine", "plugins", "glsl"]);
 include(["engines", "wine", "plugins", "override_dll"]);
-include(["engines", "wine", "verbs", "crypt32"]);
 include(["engines", "wine", "verbs", "corefonts"]);
 include(["engines", "wine", "plugins", "windows_version"]);
 
@@ -12,13 +11,11 @@ var installerImplementation = {
             .editor("Ubisoft")
             .applicationHomepage("http://anno-game.ubi.com/anno-2070/en-US/")
             .author("Zemogiter")
-            .wineArchitecture("amd64")
-            .wineVersion(3.10)
-            .wineDistribution("staging")
+            .wineVersion("3.16")
+            .wineDistribution("upstream")
             .appId(22)
-            .preInstall(function (wine){
+            .preInstall(function (wine, wizard){
                 wine.windowsVersion("win7");
-                wine.crypt32();
                 wine.corefonts();
                 wine.UseGLSL("disabled");
                 wine.overrideDLL()
