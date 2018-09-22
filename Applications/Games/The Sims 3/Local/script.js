345b5a4ca1e1cbb589602a9012355e8dfcc46007
Zemogiter <zumi999@vp.pl>
2018-09-22 17:33:36 +0200

    Add The Sims 3

---
+++ Applications/Games/The Sims 3/Local/script.js
@@ -0,0 +1,29 @@
+include(["engines", "wine", "quick_script", "local_installer_script"]);
+include(["engines", "wine", "verbs", "dotnet40"]);
+include(["engines", "wine", "verbs", "vcrun2010"]);
+include(["engines", "wine", "verbs", "tahoma"]);
+include(["engines", "wine", "verbs", "mfc42"]);
+
+var installerImplementation = {
+    run: function () {
+        new LocalInstallerScript()
+            .name("The Sims 3")
+            .editor("Electronic Arts")
+            .applicationHomepage("http://www.thesims3.com/")
+            .author("Zemogiter")
+            .category("Games")
+            .executable("Sims3Launcher.exe", ["xgamma -gamma 1"])
+            .wineVersion("3.16")
+            .wineDistribution("upstream")
+            .preInstall(function (wine){
+                wine.dotnet40();
+                wine.mfc42();
+                wine.tahoma();
+                wine.vcrun2010
+            })
+            .go();
+    }
+};
+
+/* exported Installer */
+var Installer = Java.extend(org.phoenicis.scripts.Installer, installerImplementation);
