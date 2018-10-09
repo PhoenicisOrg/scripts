a4073731f9d20df8d6baf5c878a004c3c24c544c
renard119 <44000051+renard119@users.noreply.github.com>
2018-10-09 19:24:07 +0200

    Update script.js

--- Applications/Multimedia/Captvty/3.0.0.63402/script.js
+++ Applications/Multimedia/Captvty/3.0.0.63402/script.js
@@ -1,4 +1,22 @@
-include(["engines", "wine", "engines", "wine"]);
-include(["engines", "wine", "shortcuts", "wine"]);
+include(["engines", "wine", "quick_script", "online_installer_script"]);
+include(["engines", "wine", "verbs", "dotnet45"]);
 
-var application = "Captvty 3.0.0.63402"
+var installerImplementation = {
+    run: function () {
+        new OnlineInstallerScript()
+            .name("Captvty")                       // name of the application
+            .editor("Guillaume")                               // editor of the application
+            .applicationHomepage("http://v3.captvty.fr/")  // application homepage
+            .author("")                     // author of this script (you)
+            .url("http://releases.captvty.fr/0340f3631221f953f55170d90904318eb662b89b/captvty-3.0.0.63402.zip")                       // where the exe can be downloaded
+            .checksum("b0773711af580ed877c634509f0023164fb87c02")                       // sha1sum of the exe
+            .category("Multimedia")                           // category
+            .executable("Captvty.exe")                  // exe name (for the shortcut)
+      .preInstall(function(wine, wizard) {
+        wine.dotnet45();
+    })
+            .go();
+};
+
+/* exported Installer */
+var Installer = Java.extend(org.phoenicis.scripts.Installer, installerImplementation);
