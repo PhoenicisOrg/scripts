bb631b4f60faf0b95d84da096444a398e11b3409
renard119 <44000051+renard119@users.noreply.github.com>
2018-10-09 19:25:12 +0200

    Update script.js

--- Applications/Multimedia/Captvty/3.0.0.63402/script.js
+++ Applications/Multimedia/Captvty/3.0.0.63402/script.js
@@ -4,17 +4,17 @@ include(["engines", "wine", "verbs", "dotnet45"]);
 var installerImplementation = {
     run: function () {
         new OnlineInstallerScript()
-            .name("Captvty")                       // name of the application
-            .editor("Guillaume")                               // editor of the application
-            .applicationHomepage("http://v3.captvty.fr/")  // application homepage
-            .author("")                     // author of this script (you)
+            .name("Captvty")                                    // name of the application
+            .editor("Guillaume")                                // editor of the application
+            .applicationHomepage("http://v3.captvty.fr/")       // application homepage
+            .author("renard119")                                // author of this script (you)
             .url("http://releases.captvty.fr/0340f3631221f953f55170d90904318eb662b89b/captvty-3.0.0.63402.zip")                       // where the exe can be downloaded
             .checksum("b0773711af580ed877c634509f0023164fb87c02")                       // sha1sum of the exe
-            .category("Multimedia")                           // category
-            .executable("Captvty.exe")                  // exe name (for the shortcut)
-      .preInstall(function(wine, wizard) {
-        wine.dotnet45();
-    })
+            .category("Multimedia")                             // category
+            .executable("Captvty.exe")                          // exe name (for the shortcut)
+            .preInstall(function(wine, wizard) {
+                wine.dotnet45();
+            })
             .go();
 };
 
