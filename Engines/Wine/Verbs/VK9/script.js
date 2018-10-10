19e674dc85c876427f8340ed3270b836a226e758
ImperatorS79 <fevrier.simon@gmail.com>
2018-10-10 15:12:08 +0200

    Updated VK9 to 0.28.1 (no need of browseFile anymore !)

--- Engines/Wine/Verbs/VK9/script.js
+++ Engines/Wine/Verbs/VK9/script.js
@@ -8,21 +8,16 @@ include(["utils", "functions", "filesystem", "files"]);
 * @returns {Wine} Wine object
 */
 Wine.prototype.VK9 = function () {
-    if (this.architecture() == "amd64") {
-        throw "{0} cannot currently be installed in a 64bit wine prefix.".format("VK9");
-    }
-
     print("NOTE: you need a driver that supports Vulkan enough to run VK9");
     print("NOTE: wine version should be greater or equal to 3.5");
-    print("NOTE: this is a debug dll");
 
-    var vk9Version = "0.25.0";
+    var vk9Version = "0.28.1";
 
     var setupFile32 = new Resource()
         .wizard(this.wizard())
-        .url("https://github.com/disks86/VK9/releases/download/" + vk9Version +"/"+ vk9Version + "-bin-Release.zip")
-        .checksum("9ec007090000c4923e502b0360b9ebfd304fbf05")
-        .name(vk9Version + "-bin-Realease.zip")
+        .url("https://github.com/disks86/VK9/releases/download/" + vk9Version +"/"+ vk9Version + "-bin-x86-Release.zip")
+        .checksum("da39a3ee5e6b4b0d3255bfef95601890afd80709")
+        .name(vk9Version + "-bin-x86-Realease.zip")
         .get();
 
     new Extractor()
@@ -31,19 +26,38 @@ Wine.prototype.VK9 = function () {
         .to(this.prefixDirectory() + "/TMP32/")
         .extract();
 
-    cp(this.prefixDirectory() + "/TMP32/D3d9.dll", this.system32directory());
+    if (this.architecture() == "x86")
+    {
+        cp(this.prefixDirectory() + "/TMP32/d3d9.dll", this.system32directory());
+        
+        remove(this.prefixDirectory() + "/TMP32/");
+    }
+    
+    if (this.architecture() == "amd64")
+    {
+        var setupFile32 = new Resource()
+            .wizard(this.wizard())
+            .url("https://github.com/disks86/VK9/releases/download/" + vk9Version +"/"+ vk9Version + "-bin-x86_64-Release.zip")
+            .checksum("4f50beda158d50b9b0bb8d293838686e7225de18")
+            .name(vk9Version + "-bin-x86_64-Realease.zip")
+            .get();
+
+        new Extractor()
+            .wizard(this.wizard())
+            .archive(setupFile32)
+            .to(this.prefixDirectory() + "/TMP64/")
+            .extract();
+        
+        cp(this.prefixDirectory() + "/TMP32/d3d9.dll", this.system64directory());
+        cp(this.prefixDirectory() + "/TMP64/d3d9.dll", this.system32directory());
+        
+        remove(this.prefixDirectory() + "/TMP32/");
+        remove(this.prefixDirectory() + "/TMP64/");
+    }
 
     this.overrideDLL()
         .set("native", ["d3d9"])
         .do();
-
-    var userFilePath = this.wizard().browse("Please select the .exe directory", this.prefixDirectory(), ["exe"]);
-    print(userFilePath);
-
-    // Need to copy VK9.conf and shader into executable directory
-    // var executables = this._fileSearcher.search(_shortcutPrefixDirectory, this._search);
-
-    remove(this.prefixDirectory() + "/TMP32/");
-
+    
     return this;
 }
