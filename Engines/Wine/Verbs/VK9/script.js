58238e1b53631c589d21367def8d62d78440939f
ImperatorS79 <fevrier.simon@gmail.com>
2018-10-13 09:42:14 +0200

    Implement version choice

--- Engines/Wine/Verbs/VK9/script.js
+++ Engines/Wine/Verbs/VK9/script.js
@@ -7,16 +7,17 @@ include(["utils", "functions", "filesystem", "files"]);
 * Setup VK9-> https://github.com/disks86/VK9
 * @returns {Wine} Wine object
 */
-Wine.prototype.VK9 = function () {
+Wine.prototype.VK9 = function (vk9Version) {
     print("NOTE: you need a driver that supports Vulkan enough to run VK9");
     print("NOTE: wine version should be greater or equal to 3.5");
+    print("NOTE: work from 0.28.0");
 
-    var vk9Version = "0.28.1";
+    if (typeof vk9Version === 'undefined')
+        vk9Version = "0.28.1";
 
     var setupFile32 = new Resource()
         .wizard(this.wizard())
         .url("https://github.com/disks86/VK9/releases/download/" + vk9Version +"/"+ vk9Version + "-bin-x86-Release.zip")
-        .checksum("da39a3ee5e6b4b0d3255bfef95601890afd80709")
         .name(vk9Version + "-bin-x86-Realease.zip")
         .get();
 
@@ -26,7 +27,7 @@ Wine.prototype.VK9 = function () {
         .to(this.prefixDirectory() + "/TMP32/")
         .extract();
 
-    cp(this.prefixDirectory() + "/TMP32/d3d9.dll", this.system32directory());
+    cp(this.prefixDirectory() + "/TMP32/" + vk9Version + "-bin-x86-Release/" + "d3d9.dll", this.system32directory());
 
     remove(this.prefixDirectory() + "/TMP32/");
 
@@ -35,7 +36,6 @@ Wine.prototype.VK9 = function () {
         var setupFile64 = new Resource()
             .wizard(this.wizard())
             .url("https://github.com/disks86/VK9/releases/download/" + vk9Version +"/"+ vk9Version + "-bin-x86_64-Release.zip")
-            .checksum("4f50beda158d50b9b0bb8d293838686e7225de18")
             .name(vk9Version + "-bin-x86_64-Realease.zip")
             .get();
 
@@ -44,8 +44,8 @@ Wine.prototype.VK9 = function () {
             .archive(setupFile64)
             .to(this.prefixDirectory() + "/TMP64/")
             .extract();
-      
-        cp(this.prefixDirectory() + "/TMP64/d3d9.dll", this.system64directory());
+
+        cp(this.prefixDirectory() + "/TMP64/" + vk9Version + "-bin-x86_64-Release/" + "d3d9.dll", this.system64directory());
 
         remove(this.prefixDirectory() + "/TMP64/");
     }
@@ -53,6 +53,6 @@ Wine.prototype.VK9 = function () {
     this.overrideDLL()
         .set("native", ["d3d9"])
         .do();
-    
+
     return this;
 }
