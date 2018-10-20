379cb4bb8a0e137a21050dbc1c39ac95725a1bfb
Quentin Pâris <github@playonlinux.com>
2018-10-20 13:31:34 +0200

    Update script.js

--- Engines/Wine/Engine/Implementation/script.js
+++ Engines/Wine/Engine/Implementation/script.js
@@ -252,7 +252,7 @@ var engineImplementation = {
             ldPath = this.getLocalDirectory(subCategory, version) + "/lib/:" + ldPath
         }
         environment.put("LD_LIBRARY_PATH", ldPath);
-        
+
         if (this._operatingSystemFetcher.fetchCurrentOperationSystem().getWinePackage() === "darwin") {
             environment.put("DYLD_FALLBACK_LIBRARY_PATH", ldPath);
             environment.put("FREETYPE_PROPERTIES", "truetype:interpreter-version=35");
