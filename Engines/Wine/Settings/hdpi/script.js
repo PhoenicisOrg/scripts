36a6915523b9cddacf6dd527c705d4692f7443f7
Quentin PARIS <git@quentin.paris>
2018-12-02 11:38:20 +0100

    Retina support: Add settings entry

---
+++ Engines/Wine/Settings/hdpi/script.js
@@ -0,0 +1,31 @@
+include(["engines", "wine", "engine", "object"]);
+include(["engines", "wine", "plugins", "hdpi"]);
+
+/**
+ * setting to enable/disable GLSL
+*/
+var settingImplementation = {
+    _options: [tr("Disabled"), tr("Enabled")],
+
+    getText: function () {
+        return tr("Retina support");
+    },
+    getOptions: function () {
+        return this._options;
+    },
+    getCurrentOption: function (container) {
+        var currentValue = new Wine()
+            .prefix(container)
+            .hdpi();
+
+        return currentValue ? this._options[1] : this._options[0];
+    },
+    setOption: function (container, optionIndex) {
+        new Wine()
+          .prefix(container)
+          .hdpi(1 == optionIndex);
+    }
+};
+
+/* exported Setting */
+var Setting = Java.extend(org.phoenicis.engines.EngineSetting, settingImplementation);
