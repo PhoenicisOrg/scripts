f664e447f018e99cc9020ed7437c9b6214570765
Zemogiter <zumi999@vp.pl>
2018-12-10 17:47:04 +0100

    Add files via upload

---
+++ Applications/Games/Space Engineers/Steam/script.js
@@ -0,0 +1,27 @@
+include(["engines", "wine", "quick_script", "steam_script"]); 
+include(["engines", "wine", "verbs", "dotnet461"]);
+include(["engines", "wine", "verbs", "vcrun2015"]);
+include(["engines", "wine", "verbs", "dxvk"]);
+
+var installerImplementation = {
+    run: function () {
+        new SteamScript()
+            .name("Space Engineers")
+            .editor("Keen Software House")
+            .author("Zemogiter")
+            .appId(244850)
+            .wineVersion("3.20")
+            .wineDistribution("staging")
+            .wineArchitecture("amd64")
+            .preInstall(function (wine, wizard) {
+                wizzard.message(tr("Please ensure you have the latest drivers (415.22 minimum for NVIDIA and mesa 19 for AMD) or else this game will not work."));
+                wine.dotnet461();
+                wine.vcrun2015();
+                wine.dxvk();
+            })
+            .go();
+    }
+};
+
+/* exported Installer */
+var Installer = Java.extend(org.phoenicis.scripts.Installer, installerImplementation);
