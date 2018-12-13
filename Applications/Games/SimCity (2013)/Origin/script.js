fd50f1c129b70670fb774188fb45dcdec06a4993
Zemogiter <zumi999@vp.pl>
2018-12-13 10:31:45 +0100

    Update script.js

--- Applications/Games/SimCity (2013)/Origin/script.js
+++ Applications/Games/SimCity (2013)/Origin/script.js
@@ -1,17 +1,16 @@
-include(["engines", "wine", "quick_script", "online_installer_script"]);
+include(["engines", "wine", "quick_script", "origin_script"]);
 
 var installerImplementation = {
     run: function () {
-        new OnlineInstallerScript()
+        new OriginScript()
             .name("SimCity (2013)")
             .editor("Electronic Arts")
-            .applicationHomepage("https://www.ea.com/en-gb/games/simcity")
+            .applicationHomepage("http://www.origin.com/")
             .author("ZemoScripter")
-            .url("https://origin-a.akamaihd.net/Origin-Client-Download/origin/live/OriginThinSetup.exe")
             .category("Games")
-            .executable("Origin.exe")
-            .wineVersion("3.16")
+            .wineVersion("3.19")
             .wineDistribution("staging")
+            .appId("71654,71480,71630,71650,71573,71631,71652,71572,71632,1004769,1004768,1004771,1004770,1008749,1008760,1008761,1008762,1008763,1008764,1008776,1008777,1008778,1008779,1015233,1015232,1015226")
             .go();
     }
 };
