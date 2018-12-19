8fc88e79cff35f0726306690c8702e153b11f823
Zemogiter <zumi999@vp.pl>
2018-12-19 01:41:35 +0100

    Update script.js
    Switched from dotnet40 to dotnet20 and using the wine 4.0-rc2 since it comes with better steam browser support.

--- Applications/Games/The Sims 3/Steam/script.js
+++ Applications/Games/The Sims 3/Steam/script.js
@@ -1,5 +1,5 @@
 include(["engines", "wine", "quick_script", "steam_script"]);
-include(["engines", "wine", "verbs", "dotnet40"]);
+include(["engines", "wine", "verbs", "dotnet20"]);
 include(["engines", "wine", "verbs", "vcrun2010"]);
 include(["engines", "wine", "verbs", "tahoma"]);
 include(["engines", "wine", "verbs", "mfc42"]);
@@ -12,10 +12,10 @@ var installerImplementation = {
             .applicationHomepage("http://www.thesims3.com/")
             .author("Zemogiter")
             .wineDistribution("upstream")
-            .wineVersion(3.16)
+            .wineVersion("4.0-rc2")
             .appId(47890)
             .preInstall(function (wine/*, wizard*/) {
-                wine.dotnet40();
+                wine.dotnet20();
                 wine.mfc42();
                 wine.tahoma();
                 wine.vcrun2010();
