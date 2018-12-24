d407fe45e2f4b751e7e83317b4217a4f32040050
ovari <17465872+ovari@users.noreply.github.com>
2018-12-24 12:54:33 +1100

    Update script.js

--- Utils/Functions/Net/Download/script.js
+++ Utils/Functions/Net/Download/script.js
@@ -94,7 +94,7 @@ Downloader.prototype.onlyIfUpdateAvailable = function (onlyIfUpdateAvailable) {
 */
 Downloader.prototype.get = function () {
     if (!this._message) {
-        this._message = tr("Please wait while {0} is downloaded ...", this._fetchFileNameFromUrl(this._url));
+        this._message = tr("Please wait while {0} is downloaded…", this._fetchFileNameFromUrl(this._url));
     }
 
     if (this._wizard) {
