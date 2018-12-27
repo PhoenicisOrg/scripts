1206e5f8af4240f817ca8aba52ff2c35a817162f
ovari <17465872+ovari@users.noreply.github.com>
2018-12-27 21:22:57 +1100

    Update script.js

--- Engines/Wine/QuickScript/Steam Script/script.js
+++ Engines/Wine/QuickScript/Steam Script/script.js
@@ -135,16 +135,16 @@ SteamScript.prototype.go = function () {
     this.fixCertificateIssue(wine);
 
     // Steam installation has finished
-    setupWizard.wait(tr("Please wait…"));
+    setupWizard.wait(tr("Please wait..."));
 
     this._preInstall(wine, setupWizard);
 
     // back to generic wait (might have been changed in preInstall)
-    setupWizard.wait(tr("Please wait…"));
+    setupWizard.wait(tr("Please wait..."));
 
     wine.runInsidePrefix(wine.programFiles() + "/Steam/Steam.exe", ["steam://install/" + this._appId], false);
 
-    setupWizard.wait(tr("Please wait until Steam has finished the download…"));
+    setupWizard.wait(tr("Please wait until Steam has finished the download..."));
 
     // wait until download started
     while (!this.downloadStarted(wine)) {
