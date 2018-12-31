794dafb81d09250b8ff905bf237379114bf67ade
Quentin PARIS <git@quentin.paris>
2018-12-31 09:01:21 +0100

    Fix Codacy errors

--- Engines/Wine/Plugins/DOS support/script.js
+++ Engines/Wine/Plugins/DOS support/script.js
@@ -5,22 +5,22 @@ include(["utils", "functions", "filesystem", "files"]);
  * Fetch the prefix dosbox configuration file
  * @returns {string}
  */
-Wine.prototype.dosConfigFile = function() {
+Wine.prototype.dosConfigFile = function () {
     return this.prefixDirectory() + "/playonlinux_dos.cfg";
 };
 
 /**
  * Set a setting in the prefix dosbox configuration file
- * @param key The key (example: dosbox_memsize)
- * @param value The value (example: 64)
+ * @param {string} key The key (example: dosbox_memsize)
+ * @param {*} value The value (example: 64)
  */
 Wine.prototype.dosbox_setSetting = function (key, value) {
     var settingsToWrite = this.dosbox_getSettings();
     settingsToWrite[key] = value;
 
     var newFileString = "";
-    for (var keyToWrite in settingsToWrite){
-        if(keyToWrite !== "" && settingsToWrite[keyToWrite]) {
+    for (var keyToWrite in settingsToWrite) {
+        if (keyToWrite !== "" && settingsToWrite[keyToWrite]) {
             newFileString += keyToWrite + "=" + settingsToWrite[keyToWrite] + "\n"
         }
     }
@@ -32,11 +32,11 @@ Wine.prototype.dosbox_setSetting = function (key, value) {
  * @returns {{}}
  */
 Wine.prototype.dosbox_getSettings = function () {
-    if(fileExists(this.dosConfigFile())) {
+    if (fileExists(this.dosConfigFile())) {
         var dosboxSettingsContent = cat(this.dosConfigFile());
         var settingResults = {};
-        dosboxSettingsContent.split("\n").forEach(function(dosboxSettingLine) {
-            splitDosboxSettingLine = dosboxSettingLine.split("=");
+        dosboxSettingsContent.split("\n").forEach(function (dosboxSettingLine) {
+            var splitDosboxSettingLine = dosboxSettingLine.split("=");
             settingResults[splitDosboxSettingLine[0]] = splitDosboxSettingLine[1];
         });
         return settingResults;
@@ -47,7 +47,7 @@ Wine.prototype.dosbox_getSettings = function () {
 
 /**
  * Get one dosbox setting
- * @param key The key of the setting (example dosbox_memsize)
+ * @param {string} key The key of the setting (example dosbox_memsize)
  * @returns {*} The value (example: 64)
  */
 Wine.prototype.dosbox_getSetting = function (key) {
@@ -57,35 +57,35 @@ Wine.prototype.dosbox_getSetting = function (key) {
 /**
  * Sets dosbox_memsize parameter in the current prefix
  * https://www.dosbox.com/wiki/dosbox.conf#.5Bdosbox.5D
- * @param memSize
+ * @param {int} memSize The memory size
  */
-Wine.prototype.dosbox_setMemsize = function(memSize) {
+Wine.prototype.dosbox_setMemsize = function (memSize) {
     this.dosbox_setSetting("dosbox_memsize", memSize)
 };
 
 /**
  * Sets render_aspect parameter in the current prefix
  * https://www.dosbox.com/wiki/dosbox.conf#.5Brender.5D
- * @param renderAspect
+ * @param {boolean} renderAspect The render aspect
  */
-Wine.prototype.dosbox_setRenderAspect = function(renderAspect) {
+Wine.prototype.dosbox_setRenderAspect = function (renderAspect) {
     this.dosbox_setSetting("render_aspect", renderAspect)
 };
 
 /**
  * Sets render_frameskip parameter in the current prefix
  * https://www.dosbox.com/wiki/dosbox.conf#.5Brender.5D
- * @param renderFrameSkip
+ * @param {int} renderFrameSkip The render frameskip
  */
-Wine.prototype.dosbox_setRenderFrameskip = function(renderFrameSkip) {
+Wine.prototype.dosbox_setRenderFrameskip = function (renderFrameSkip) {
     this.dosbox_setSetting("render_frameskip", renderFrameSkip)
 };
 
 /**
  * Sets CPU cycles in the current prefix
  * https://www.dosbox.com/wiki/dosbox.conf#.5Bcpu.5D
- * @param cpu_cycles (example: max 95% limit 33000)
+ * @param {*} cpu_cycles (example: max 95% limit 33000)
  */
-Wine.prototype.dosbox_setCPUCycles = function(cpu_cycles) {
+Wine.prototype.dosbox_setCPUCycles = function (cpu_cycles) {
     this.dosbox_setSetting("cpu_cycles", cpu_cycles)
 };
