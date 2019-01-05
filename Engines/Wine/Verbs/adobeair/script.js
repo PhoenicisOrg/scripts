0d1aba2f5560b947922265985c76994673b09149
Kreyren <KostWarCZE@RiXotStudio.cz>
2019-01-05 11:30:16 +0000

    Update script.js
    Needs verification, my skill in javascript is not to be trusted.. did my best tho >.>

--- Engines/Wine/Verbs/adobeair/script.js
+++ Engines/Wine/Verbs/adobeair/script.js
@@ -1,4 +1,39 @@
+include(["engines", "wine", "engine", "object"]);
+include(["utils", "functions", "net", "resource"]);
+include(["utils", "functions", "filesystem", "files"]);
+include(["engines", "wine", "verbs", "luna"]);
+
 /**
-* Verb to install Uplay
-* @returns {Wine} Wine object
+* Verb to install adobeair
+* @returns {Wine} Wine object // Verify
 */
+Wine.prototype.adobeair = function () {
+    var setupFile = new Resource()
+        .wizard(this.wizard())
+        .url("https://airdownload.adobe.com/air/win/download/latest/AdobeAIRInstaller.exe")
+        .checksum("24532d41ef2588c0daac4b6f8b7f863ee3c1a1b1e90b2d8d8b3eb4faa657e5e3")
+        .name("AdobeAIRInstaller.exe")
+        .get();
+  
+        this.wizard().wait(tr("Please wait while {0} is installed...", "Adobe air"));
+        this.run(setupFile, "/S", null, false, true);
+
+        return this;
+    };
+
+/**
+ * Verb to install adobeair
+*/
+var verbImplementation = {
+    install: function (container) {
+        var wine = new Wine();
+        wine.prefix(container);
+        var wizard = SetupWizard(InstallationType.VERBS, "adobeair", java.util.Optional.empty());
+        wine.wizard(wizard);
+        wine.adobeair();
+        wizard.close();
+    }
+};
+
+/* exported Verb */
+var Verb = Java.extend(org.phoenicis.engines.Verb, verbImplementation);
