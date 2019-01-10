27ae01601598233df5f1a8de9416c909f2865d95
Quentin PARIS <git@quentin.paris>
2019-01-10 21:29:30 +0100

    Adobe air script

---
+++ Engines/Wine/Verbs/adobeair/script.js
@@ -0,0 +1,36 @@
+include(["engines", "wine", "engine", "object"]);
+include(["utils", "functions", "net", "resource"]);
+
+/**
+ * Verb to install adobeair
+ * @returns {Wine} Wine object
+ */
+Wine.prototype.adobeair = function () {
+    var adobeair = new Resource()
+        .wizard(this.wizard())
+        .url("https://airdownload.adobe.com/air/win/download/latest/AdobeAIRInstaller.exe")
+        .checksum("68d26cca4c6c8230d3f7aa850ee227d518288dfc")
+        .name("AdobeAIRInstaller.exe")
+        .get();
+
+    this.run(adobeair).wait();
+
+    return this;
+};
+
+/**
+ * Verb to install adobeair
+ */
+var verbImplementation = {
+    install: function (container) {
+        var wine = new Wine();
+        wine.prefix(container);
+        var wizard = SetupWizard(InstallationType.VERBS, "adobeair", java.util.Optional.empty());
+        wine.wizard(wizard);
+        wine.adobeair();
+        wizard.close();
+    }
+};
+
+/* exported Verb */
+var Verb = Java.extend(org.phoenicis.engines.Verb, verbImplementation);
