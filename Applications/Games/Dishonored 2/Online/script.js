ed16c2960a65cef415fbeb27732ffea5159330af
Jacob Hrbek <werifgx@gmail.com>
2019-01-12 12:22:50 +0000

    Games/Dishonored 2 : Translated on SteamScript
    script.js
    - Line 8  :  Needs sanity check
    - Line 18 : Added `.publishes` which is not supported, but seems sane to me
    -- probably include in phoenicis?
    - Added argument `+com_showLoadingScreen 0` which should disable loading screens.
    -- Implementation needs sanity check on line 24
    
    Signed-off-by: Jacob Hrbek <werifgx@gmail.com>

--- Applications/Games/Dishonored 2/Online/script.js
+++ Applications/Games/Dishonored 2/Online/script.js
@@ -1,23 +1,27 @@
-include(["engines", "wine", "plugins", "windows_version"]);
+include(["engines", "wine", "quick_script", "steam_script"]);
+
 include(["engines", "wine", "verbs", "xact"]);
 include(["engines", "wine", "verbs", "corefonts"]);
-include(["engines", "wine", "verbs", "steam"]); // Sane? Expected winesteam
+include(["engines", "wine", "verbs", "dxvk"]);
+include(["engines", "wine", "plugins", "windows_version"]);
+
 include(["engines", "wine", "shortcuts", "wine"]); // Sane? Dunno 
 
-// TODO : Executable on the game to prevent openning steam.
-// TODO : Add `+com_showLoadingScreen 0` in launch options to prevent annoying loading screens
-
-// Installs Dishonored 2
+// Expected to run winesteam on wine-staging (wine untested) on 3.21+ (untested) with xact,dxvk,coreconts dependencies
+// Has complicated shaders which are causing an issues (https://github.com/ValveSoftware/Proton/issues/823)
 
 var installerImplementation = {
     run: function () {
-        new CustomInstallerScript()
+        new SteamScript()
             .name("Dishonored 2")
+            .category("Games")
+            // .gameOverlay(false) // Disable game overlay
             .editor("Arkane Studios")
             .applicationHomepage("https://dishonored.bethesda.net/")
-            .author("kreyren")
+            .publisher("Bethesda Softworks") // Not mensioned, but seems sane 
+            .author("Kreyren")
             .appId(403640)
-            .category("Games")
+            .executable("Steam.exe", ["-silent", "-applaunch", 123456, "+com_showLoadingScreen 0"]) // Verify
             .wineDistribution("staging")
             .wineVersion(LATEST_STAGING_VERSION)
             .preInstall(function (wine /*, wizard*/) {
@@ -25,10 +29,7 @@ var installerImplementation = {
                 wine.xact();
                 wine.dxvk();
                 wine.coreconts();
-                wine.steam();
-            })
             .go();
-    }
 };
 
 /* exported Installer */
