ca8ef4051df515eabd508e747b73e881a70b9db6
Jacob Hrbek <werifgx@gmail.com>
2019-01-15 15:48:24 +0100

    Update

--- Applications/Games/Overwatch/Online/script.js
+++ Applications/Games/Overwatch/Online/script.js
@@ -1,9 +1,11 @@
 include(["engines", "wine", "quick_script", "online_installer_script"]);
-include(["engines", "wine", "plugins", "csmt"]);
+// include(["engines", "wine", "plugins", "csmt"]);
 include(["engines", "wine", "plugins", "windows_version"]);
 include(["engines", "wine", "verbs", "vcrun2015"]);
 include(["engines", "wine", "verbs", "corefonts"]);
 include(["engines", "wine", "verbs", "dxvk"]);
+
+BUG: https://github.com/PhoenicisOrg/scripts/issues/792
 
 var installerImplementation = {
     run: function () {
@@ -18,13 +20,13 @@ var installerImplementation = {
             .wineDistribution("staging")
             .wineArchitecture("amd64") // Multilib preffered? (without setting wine architecture)
             .category("Games")
-            .executable("Battle.net.exe")
+            .executable("Battle.net.exe") // affected by bug 
             .preInstall(function (wine/*, wizard*/) {
                 wine.windowsVersion("win7");
-                wine.vcrun2015();
-                wine.corefonts();
-                wine.enableCSMT();
-                wine.DXVK(); 
+                wine.vcrun2015(); // untested, should be mandatory
+                wine.corefonts(); // Untested, for fonts?
+                // wine.enableCSMT(); // Commented out since it's not tested
+                wine.DXVK();  // Mandatory for graphics
             })
             .go();
     }
