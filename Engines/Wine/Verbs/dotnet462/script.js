32c12d16aadd5ec60e7013442fbf91417306ba81
ImperatorS79 <fevrier.simon@gmail.com>
2019-01-23 13:01:56 +0100

    Update script.js

--- Engines/Wine/Verbs/dotnet462/script.js
+++ Engines/Wine/Verbs/dotnet462/script.js
@@ -13,9 +13,9 @@ Wine.prototype.dotnet462 = function () {
         print(tr("This package ({0}) may not fully work on a 64-bit installation. 32-bit prefixes may work better.", "dotnet462"));
     }
 
-    var OSVersion = this.windowsVersion();
-    if (OSVersion == null)
-        OSVersion = "win7";
+    var osVersion = this.windowsVersion();
+    if (osVersion == null)
+        osVersion = "win7";
 
     var setupFile = new Resource()
         .wizard(this._wizard)
@@ -36,14 +36,14 @@ Wine.prototype.dotnet462 = function () {
     this.wizard().wait(tr("Please wait while {0} is installed...", ".NET Framework 4.6.2"));
     this.run(setupFile, [setupFile, "/q", "/c:\"install.exe /q\""], null, false, true);
 
-    this.wizard().wait(tr("Please wait ..."));
+    this.wizard().wait(tr("Please wait..."));
     this.run("reg", ["delete", "HKCU\\Software\\Wine\\DllOverrides\\fusion", "/f"], null, false, true);
 
     this.overrideDLL()
         .set("native", ["mscoree"])
         .do();
 
-    this.windowsVersion(OSVersion);
+    this.windowsVersion(osVersion);
 
     return this;
 };
