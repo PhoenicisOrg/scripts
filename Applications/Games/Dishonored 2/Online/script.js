60cf3ae36a00a635ad36c63b924219fb486d8f23
Jacob Hrbek <werifgx@gmail.com>
2019-01-25 17:14:49 +0100

    update

--- Applications/Games/Dishonored 2/Online/script.js
+++ Applications/Games/Dishonored 2/Online/script.js
@@ -1,34 +1,30 @@
-include(["engines", "wine", "plugins", "windows_version"]);
+include(["engines", "wine", "quick_script", "steam_script"]);
 include(["engines", "wine", "verbs", "xact"]);
 include(["engines", "wine", "verbs", "corefonts"]);
-include(["engines", "wine", "verbs", "steam"]); // Sane? Expected winesteam
-include(["engines", "wine", "shortcuts", "wine"]); // Sane? Dunno 
+include(["engines", "wine", "verbs", "dxvk"]);
+include(["engines", "wine", "plugins", "windows_version"]);
 
-// TODO : Executable on the game to prevent openning steam.
-// TODO : Add `+com_showLoadingScreen 0` in launch options to prevent annoying loading screens
-
-// Installs Dishonored 2
-
+// Expected to run winesteam on wine-staging (wine untested) on 3.21+ (untested) with xact,dxvk,coreconts dependencies
+// Has complicated shaders which are causing an issues  (on AMDGPU?) (https://github.com/ValveSoftware/Proton/issues/823)
 var installerImplementation = {
     run: function () {
-        new CustomInstallerScript()
+        new SteamScript()
             .name("Dishonored 2")
+            .category("Games")
             .editor("Arkane Studios")
             .applicationHomepage("https://dishonored.bethesda.net/")
-            .author("kreyren")
+            .publisher("Bethesda Softworks") // Not mensioned, but seems sane 
+            .author("Kreyren")
             .appId(403640)
-            .category("Games")
+            .executable("Steam.exe", ["-silent", "-applaunch", 123456, "+com_showLoadingScreen 0"]) // Verify
             .wineDistribution("staging")
             .wineVersion(LATEST_STAGING_VERSION)
             .preInstall(function (wine /*, wizard*/) {
                 wine.windowsVersion("win7");
                 wine.xact();
                 wine.dxvk();
-                wine.coreconts();
-                wine.steam();
-            })
+                wine.corefonts();
             .go();
-    }
 };
 
 /* exported Installer */
