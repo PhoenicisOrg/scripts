e4dc8adacbba3b73165718c014d3d49c6420389f
Zemogiter <zumi999@vp.pl>
2019-02-01 17:08:45 +0100

    Update script.js

--- Applications/Games/Subnautica/Steam/script.js
+++ Applications/Games/Subnautica/Steam/script.js
@@ -15,20 +15,23 @@ var installerImplementation = {
             .author("Zemogiter")
             .applicationHomepage("https://unknownworlds.com/subnautica/")
             .wineDistribution("upstream")
-            .wineVersion("4.0-rc1")
+            .wineVersion("4.0")
             .wineArchitecture("amd64")
             .appId(264710)
             .preInstall(function (wine, wizard) {
+                wizard.message(tr("Please ensure you have the latest drivers (418.30 minimum for NVIDIA and mesa 19 for AMD) or else this game might not work correctly."));
+                wizard.message(tr("You can make the game smoother by using this: https://github.com/lutris/lutris/wiki/How-to:-Esync"));
                 wine.vcrun2013();
                 wine.corefonts();
                 wine.d3dx11();
                 wine.DXVK();
-                wizard.message(tr("Please ensure you have the latest drivers (415.22 minimum for NVIDIA and mesa 19 for AMD) or else this game might not work correctly."));
-                wizard.message(tr("You can make the game smoother by using this: https://github.com/lutris/lutris/wiki/How-to:-Esync"));
                 var screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
                 wine.setVirtualDesktop(screenSize.width, screenSize.height);
                 var registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("fix.reg");
                 wine.regedit().patch(registrySettings);
+            })
+            .postInstall(function (wine, wizard) {
+                wizard.message(tr("Due to potential confilct with Vulkan, shader mods break the game (the executable file works but no window is displayed)."));
             })
             .gameOverlay(false)
             .go();
