9e3501c985e5bef3544c071e86d77788d07eebea
Zemogiter <zumi999@vp.pl>
2019-02-02 18:40:22 +0100

    Update script.js
    Removal of the registry fix since it is no longer needed.

--- Applications/Games/Subnautica Below Zero/Steam/script.js
+++ Applications/Games/Subnautica Below Zero/Steam/script.js
@@ -1,5 +1,4 @@
 include(["engines", "wine", "quick_script", "steam_script"]);
-include(["engines", "wine", "plugins", "regedit"]);
 include(["engines", "wine", "plugins", "virtual_desktop"]);
 include(["engines", "wine", "verbs", "vcrun2013"]);
 include(["engines", "wine", "verbs", "corefonts"]);
@@ -25,8 +24,6 @@ var installerImplementation = {
                 wine.DXVK();
                 var screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
                 wine.setVirtualDesktop(screenSize.width, screenSize.height);
-                var registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("fix.reg");
-                wine.regedit().patch(registrySettings);
             })
             .gameOverlay(false)
             .go();
