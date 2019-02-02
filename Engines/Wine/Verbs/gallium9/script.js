5e8ea63bfb9a59eb791f987d0f21087de42289d5
ImperatorS79 <fevrier.simon@gmail.com>
2019-02-02 17:32:57 +0100

    Update script.js

--- Engines/Wine/Verbs/gallium9/script.js
+++ Engines/Wine/Verbs/gallium9/script.js
@@ -9,29 +9,29 @@ include(["utils", "functions", "filesystem", "files"]);
 * @returns {Wine} Wine object
 */
 Wine.prototype.gallium9 = function () {
-    var message = "Using Gallium 9 requires to have a driver supporting the Gallium 9 state tracker, as well as d3dapater9.so installed (ex: libd3d9adapter-mesa package). Please be sure there are installed.";
-
+    this.wizard().message(tr("Using Gallium 9 requires to have a driver supporting the Gallium 9 state tracker, as well as d3dapater9.so installed (ex: libd3d9adapter-mesa package). Please be sure there are installed."));
+	
     var setupFile = new Resource()
         .wizard(this.wizard())
-        .url()
-        .name("gallium9-0.1.tar.gz")
+        .url("https://github.com/iXit/wine-nine-standalone/releases/download/v0.2/gallium-nine-standalone-v0.2.tar.gz")
+        .name("gallium-nine-standalone-v0.2.tar.gz")
         .get();
 
     new Extractor()
         .wizard(this.wizard())
         .archive(setupFile)
-        .to(this.prefixDirectory() + "/TMP/")
+        .to(this.prefixDirectory())
         .extract();
 
-    cp(this.prefixDirectory() + "/TMP/nine/lib32/d3d9-nine.dll.so", this.system32directory());
-    cp(this.prefixDirectory() + "/TMP/nine/bin32/ninewinecfg.exe.so", this.system32directory());
+    cp(this.prefixDirectory() + "/gallium-nine-standalone/nine/lib32/d3d9-nine.dll.so", this.system32directory());
+    cp(this.prefixDirectory() + "/gallium-nine-standalone/nine/bin32/ninewinecfg.exe.so", this.system32directory());
 
     lns(this.system32directory() + "/d3d9-nine.dll.so", this.system32directory() + "/d3d9-nine.dll");
     lns(this.system32directory() + "/ninewinecfg.exe.so", this.system32directory() + "/ninewinecfg.exe");
 
     if (this.architecture() == "amd64") {
-        cp(this.prefixDirectory() + "/TMP/nine/lib64/d3d9-nine.dll.so", this.system64directory());
-	cp(this.prefixDirectory() + "/TMP/nine/bin64/ninewinecfg.exe.so", this.system64directory());
+        cp(this.prefixDirectory() + "/gallium-nine-standalone/nine/lib64/d3d9-nine.dll.so", this.system64directory());
+	cp(this.prefixDirectory() + "/gallium-nine-standalone/nine/bin64/ninewinecfg.exe.so", this.system64directory());
 	    
         lns(this.system64directory() + "/d3d9-nine.dll.so", this.system64directory() + "/d3d9-nine.dll");
         lns(this.system64directory() + "/ninewinecfg.exe.so", this.system64directory() + "/ninewinecfg.exe");
