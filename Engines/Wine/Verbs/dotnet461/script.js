dc1e4d2d2ad67e3f1110a5d268aac03ffb55d75e
ImperatorS79 <fevrier.simon@gmail.com>
2019-02-04 11:22:42 +0100

    Update script.js

--- Engines/Wine/Verbs/dotnet461/script.js
+++ Engines/Wine/Verbs/dotnet461/script.js
@@ -9,7 +9,7 @@ include(["engines", "wine", "verbs", "dotnet46"]);
 * @returns {Wine} Wine object
 */
 Wine.prototype.dotnet461 = function () {
-    if (this.architecture == "amd64") {
+    if (this.architecture() == "amd64") {
         print(tr("This package ({0}) may not fully work on a 64-bit installation. 32-bit prefixes may work better.", "dotnet461"));
     }
 
