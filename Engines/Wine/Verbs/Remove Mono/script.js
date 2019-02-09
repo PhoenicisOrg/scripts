12e3d687af1c1281e05e49031981b7d07b993ab4
test <fevrier.simon@gmail.com>
2019-02-09 15:40:00 +0100

    Run npm tools

--- Engines/Wine/Verbs/Remove Mono/script.js
+++ Engines/Wine/Verbs/Remove Mono/script.js
@@ -7,22 +7,22 @@ include(["utils", "functions", "filesystem", "files"]);
 */
 Wine.prototype.remove_mono = function () {
     this.uninstall("Mono");
-    
+
     this.wizard().wait(tr("Please wait..."));
     this.run("reg", ["delete", "HKLM\\Software\\Microsoft\\.NETFramework\\v2.0.50727\\SBSDisabled", "/f"], null, false, true);
 
     this.wizard().wait(tr("Please wait..."));
     this.run("reg", ["delete", "HKLM\\Software\\Microsoft\\NET Framework Setup\\NDP\\v3.5", "/f"], null, false, true);
-    
+
     this.wizard().wait(tr("Please wait..."));
     this.run("reg", ["delete", "HKLM\\Software\\Microsoft\\NET Framework Setup\\NDP\\v4", "/f"], null, false, true);
 
     remove(this.system32directory() + "/mscoree.dll");
-    if(this.architecture() == "amd64")
+    if (this.architecture() == "amd64")
     {
-        remove(this.system64directory() + "/mscoree.dll"); 
+        remove(this.system64directory() + "/mscoree.dll");
     }
-    
+
     return this;
 };
 
--- Engines/Wine/Verbs/dotnet20/script.js
+++ Engines/Wine/Verbs/dotnet20/script.js
@@ -11,24 +11,24 @@ include(["engines", "wine", "verbs", "remove_mono"]);
 */
 Wine.prototype.dotnet20 = function () {
     var osVersion = this.windowsVersion();
-    
+
     if (this.architecture() == "x86") {
         this.windowsVersion("win2k");
-        
+
         var setupFile = new Resource()
             .wizard(this.wizard())
             .url("https://download.lenovo.com/ibmdl/pub/pc/pccbbs/thinkvantage_en/dotnetfx.exe")
             .checksum("a3625c59d7a2995fb60877b5f5324892a1693b2a")
             .name("dotnetfx.exe")
             .get();
-            
+
         this.remove_mono();
-        
+
         this.wizard().wait(tr("Please wait while {0} is installed...", ".NET Framework 2.0"));
         this.run(setupFile, ["/q:a", "/c:install.exe /q"], null, false, true);
-        
+
         this.windowsVersion(osVersion);
-        
+
         remove(this.system32directory() + "/msvcr80.dll");
         remove(this.system32directory() + "/msvcm80.dll");
         remove(this.system32directory() + "/msvcp80.dll");
@@ -40,13 +40,13 @@ Wine.prototype.dotnet20 = function () {
             .checksum("e59cca309463a5d98daeaada83d1b05fed5126c5")
             .name("NetFx64.exe")
             .get();
-            
+
         this.remove_mono();
-        
+
         this.wizard().wait(tr("Please wait while {0} is installed...", ".NET Framework 2.0"));
         this.run(setupFile, ["/q:a", "/c:install.exe /q"], null, false, true)
     }
-    
+
     //This is in winetricks source, but does not seem to work
     //this.wizard().wait(tr("Please wait while executing ngen..."));
     //this.run(this.prefixDirectory() + "/drive_c/windows/Microsoft.NET/Framework/v2.0.50727/ngen.exe", "executequeueditems", null, false, true);
