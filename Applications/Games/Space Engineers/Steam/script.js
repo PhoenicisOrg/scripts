efbbac8a2af46951b3b56aeac7d2880c9ff4bc62
Zemogiter <zumi999@vp.pl>
2019-02-10 12:34:34 +0100

    Update script.js
    Removed the line about MoltenVK since it's now a part of DXVK verb, changed wine version. The game still shuts itself down.

--- Applications/Games/Space Engineers/Steam/script.js
+++ Applications/Games/Space Engineers/Steam/script.js
@@ -1,7 +1,7 @@
 include(["engines", "wine", "quick_script", "steam_script"]);
 include(["engines", "wine", "verbs", "dotnet472"]);
 include(["engines", "wine", "verbs", "vcrun2017"]);
-//include(["engines", "wine", "verbs", "dxvk"]);
+include(["engines", "wine", "verbs", "dxvk"]);
 //include(["engines", "wine", "verbs", "faudio"]);
 
 var installerImplementation = {
@@ -11,20 +11,13 @@ var installerImplementation = {
             .editor("Keen Software House")
             .author("Zemogiter")
             .appId(244850)
-            .wineVersion("4.0")
+            .wineVersion("4.1")
             .wineDistribution("upstream")
             .wineArchitecture("amd64")
             .preInstall(function (wine, wizard) {
-                var operatingSystemFetcher = Bean("operatingSystemFetcher");
-                print("Debug output: " + operatingSystemFetcher.fetchCurrentOperationSystem());
-                if (operatingSystemFetcher.fetchCurrentOperationSystem() != "Linux")
-                {
-                    throw "This game will not work under your OS due to MoltenVK not being implemented into Wine.".format("Space Engineers");
-                }
-                wizard.message(tr("Please ensure you have the latest drivers (418.30 minimum for NVIDIA and mesa 19 for AMD) or else this game will not work."));
                 wine.dotnet472();
                 wine.vcrun2017();
-                //wine.DXVK();
+                wine.DXVK();
                 //wine.faudio();
             })
             .gameOverlay(false)
