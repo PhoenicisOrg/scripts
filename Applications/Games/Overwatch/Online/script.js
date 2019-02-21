132377e351240943b5df33c4bfe6de38bdd29cdd
ImperatorS79 <fevrier.simon@gmail.com>
2019-02-21 08:02:47 +0100

    Update script.js

--- Applications/Games/Overwatch/Online/script.js
+++ Applications/Games/Overwatch/Online/script.js
@@ -1,12 +1,9 @@
 include(["engines", "wine", "quick_script", "online_installer_script"]);
-// include(["engines", "wine", "plugins", "csmt"]);
 include(["engines", "wine", "plugins", "windows_version"]);
 include(["engines", "wine", "plugins", "override_dll"]);
 include(["engines", "wine", "verbs", "vcrun2015"]);
 include(["engines", "wine", "verbs", "corefonts"]);
 include(["engines", "wine", "verbs", "dxvk"]);
-
-BUG: https://github.com/PhoenicisOrg/scripts/issues/792
 
 var installerImplementation = {
     run: function () {
@@ -16,19 +13,18 @@ var installerImplementation = {
             .applicationHomepage("http://www.playoverwatch.com/")
             .author("ImperatorS79, kreyren")
             .url("https://eu.battle.net/download/getInstaller?os=win&installer=Overwatch-Setup.exe")
-        //The checksum is different each time you download
+            //The checksum is different each time you download
             .wineVersion(LATEST_STAGING_VERSION)
             .wineDistribution("staging")
-            .wineArchitecture("amd64") // Multilib preffered? (without setting wine architecture)
+            .wineArchitecture("amd64")
             .category("Games")
-            .executable("Battle.net.exe") // affect ed by bug 
+            .executable("Battle.net.exe")
             .preInstall(function (wine/*, wizard*/) {
                 wine.windowsVersion("win7");
-                wine.vcrun2015(); // untested, should be mandatory
-                wine.corefonts(); // Untested, for fonts?
-                // wine.enableCSMT(); // Commented out since it's not tested
+                wine.vcrun2015();
+                wine.corefonts();
                 wine.overrideDLL().set("disabled", ["nvapi", "nvapi64"]).do();
-                wine.DXVK();  // Mandatory for graphics
+                wine.DXVK();
             })
             .go();
     }
