2542bb1ebfa2521a27a1fe698ab82f720944652b
test <fevrier.simon@gmail.com>
2019-05-15 17:28:27 +0200

    Plugin and Settings for font smoothing

--- Engines/Wine/Plugins/Font smoothing/script.js
+++ Engines/Wine/Plugins/Font smoothing/script.js
@@ -6,22 +6,22 @@ include("engines.wine.engine.object");
  * @returns {Wine} Wine object
  */
 Wine.prototype.FontSmoothing = function (mode) {
-	var FontSmoothingType;
-	var FontSmoothingOrientation;
-	if(mode == "RGB") {
-		FontSmoothingType = "2";
-		FontSmoothingOrientation = "1";
-	}
-	else if (mode == "BGR") {
+    var FontSmoothingType;
+    var FontSmoothingOrientation;
+    if (mode == "RGB") {
+        FontSmoothingType = "2";
+        FontSmoothingOrientation = "1";
+    }
+    else if (mode == "BGR") {
 	    FontSmoothingType = "2";
-		FontSmoothingOrientation = "0";
-	}
-	else if (mode == "Gray Scale"){
+        FontSmoothingOrientation = "0";
+    }
+    else if (mode == "Gray Scale"){
 	    FontSmoothingType = "1";
-		FontSmoothingOrientation = "1";
-	}
+        FontSmoothingOrientation = "1";
+    }
 
-	var regeditFileContent =
+    var regeditFileContent =
 	        "REGEDIT4\n"					+
 			"\n"						+
 			"[HKEY_CURRENT_USER\\Control Panel\\Desktop]\n"	+
@@ -29,7 +29,7 @@ Wine.prototype.FontSmoothing = function (mode) {
 			"\"FontSmoothingType\"=dword:0000000" + FontSmoothingType + "\n"	+
 			"\"FontSmoothingGamma\"=dword:00000578\n"	+
 			"\"FontSmoothingOrientation\"=dword:0000000" + FontSmoothingOrientation + "\n";
-			
-	this.regedit().patch(regeditFileContent);
+
+    this.regedit().patch(regeditFileContent);
     return this;
 };
