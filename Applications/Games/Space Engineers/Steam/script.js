ace30e199ef996d205c9bab1f9cc253d6786fef5
Zemogiter <zumi999@vp.pl>
2019-05-26 14:57:11 +0200

    Update script.js
    Added a "fix" to the intro cutscene issue (by removing it lol), commenting out the dll override since futher tests are needed to get this to work, using newer Wine version

--- Applications/Games/Space Engineers/Steam/script.js
+++ Applications/Games/Space Engineers/Steam/script.js
@@ -3,7 +3,8 @@ include("engines.wine.verbs.dotnet472");
 include("engines.wine.verbs.vcrun2017");
 include("engines.wine.verbs.dxvk");
 include("engines.wine.verbs.faudio");
-include("engines.wine.plugins.override_dll");
+//include("engines.wine.plugins.override_dll");
+include("utils.functions.filesystem.files");
 
 var installerImplementation = {
     run: function () {
@@ -12,7 +13,7 @@ var installerImplementation = {
             .editor("Keen Software House")
             .author("Zemogiter")
             .appId(244850)
-            .wineVersion("4.2")
+            .wineVersion("4.9")
             .wineDistribution("upstream")
             .wineArchitecture("amd64")
             .preInstall(function (wine) {
@@ -20,9 +21,12 @@ var installerImplementation = {
                 wine.vcrun2017();
                 wine.DXVK();
                 wine.faudio();
-                wine.overrideDLL()
+                /*wine.overrideDLL()
                     .set("native, builtin", ["xaudio2_0", "xaudio2_1", "xaudio2_2", "xaudio2_3", "xaudio2_4", "xaudio2_5", "xaudio2_6", "xaudio2_7", "xaudio2_8", "xaudio2_9"])
-                    .do();
+                    .do();*/
+            })
+            .postInstall(function (wine) {
+                remove(wine.prefixDirectory() + "drive_c/" + wine.programFiles() + "/Steam/steamapps/common/Space Engineers/Content/KSH.wmv");
             })
             .gameOverlay(false)
             .go();
