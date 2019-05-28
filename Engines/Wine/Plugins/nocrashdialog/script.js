beed4145fd93d2a6c63af1777982a8623b5d77a3
Zemogiter <zumi999@vp.pl>
2019-05-28 10:49:44 +0200

    Add nocrashdialog plugin(#954)

---
+++ Engines/Wine/Plugins/nocrashdialog/script.js
@@ -0,0 +1,15 @@
+include("engines.wine.engine.object");
+
+/**
+ * disables the crashdialog
+ * @returns {Wine} Wine object
+ */
+Wine.prototype.nocrashdialog = function () {
+    var regeditFileContent =
+        "REGEDIT4\n" +
+        "\n" +
+        "[HKEY_CURRENT_USER\\Software\\Wine\\WineDbg]\n" +
+        "\"ShowCrashDialog\"=\""00000000"\""
+    this.regedit().patch(regeditFileContent);
+    return this;
+};
