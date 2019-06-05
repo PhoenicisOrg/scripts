b81d0e8434016f7a1553f95fa21a684a144aac49
Jacob Hrbek <werifgx@gmail.com>
2019-06-05 12:50:54 -0500

    Add MinionMasters

---
+++ Applications/Games/MinionMasters/Steam_dxv9/script.js
@@ -0,0 +1,25 @@
+include("engines.wine.quick_script.steam_script");
+include("engines.wine.verbs.dxv9");
+include("engines.wine.plugins.usetakefocus")
+
+
+var installerImplementation = {
+    run: function () {
+        new SteamScript()
+            .name("Minion Masters")
+            .applicationHomepage("http://minionmastersthegame.com/") // https not supported [05.06.2019]
+            .appId(50130)
+            .author("KREYREN")
+            .category("Games")
+            .wineDistribution("staging")
+            .wineVersion(LATEST_STAGING_VERSION)
+            .preInstall(function (wine/*, wizard*/) {
+                wine.DXV9(); // Experimental: Hoping to improve performance
+                wine.UseTakeFocus("N"); // Mandatory to avoid focus issues
+            })
+            .go();
+    }
+};
+
+/* exported Installer */
+var Installer = Java.extend(org.phoenicis.scripts.Installer, installerImplementation);
