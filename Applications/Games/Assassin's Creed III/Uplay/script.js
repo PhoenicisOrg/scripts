8c3e46f202eb48392d691d4d0497060ea8e5837a
Jacob Hrbek <werifgx@gmail.com>
2019-06-06 06:40:18 -0500

    Added corefonts, vcrun2008 and switched on staging

--- Applications/Games/Assassin's Creed III/Uplay/script.js
+++ Applications/Games/Assassin's Creed III/Uplay/script.js
@@ -1,5 +1,7 @@
 include("engines.wine.quick_script.uplay_script");
 include("engines.wine.verbs.dxvk");
+include("engines.wine.verbs.corefonts");
+include("engines.wine.verbs.vcrun2008");
 
 var installerImplementation = {
     run: function () {
@@ -9,8 +11,12 @@ var installerImplementation = {
             .applicationHomepage("https://www.ubisoft.com/en-us/game/assassins-creed")
             .author("KREYREN")
             .appId(54)
+            .wineDistribution("staging")
+            .wineVersion(LATEST_STAGING_VERSION)
             .preInstall(function (wine/*, wizard*/) {
                 wine.DXVK();
+                wine.corefonts();
+                wine.vcrun2008();
             })
             .go();
     }
