58d8cb97aa60d60c680235aad4b2b8e3c4d01056
Jacob Hrbek <werifgx@gmail.com>
2019-06-06 08:28:52 -0500

    Added stub

--- Applications/Games/Assassin's Creed III/Steam/script.js
+++ Applications/Games/Assassin's Creed III/Steam/script.js
@@ -1,17 +1,23 @@
 include("engines.wine.quick_script.steam_script");
 include("engines.wine.verbs.dxvk");
+include("engines.wine.verbs.corefonts");
+include("engines.wine.verbs.vcrun2008");
+
+// Doesn't work! - https://github.com/PhoenicisOrg/scripts/pull/967#issuecomment-499492492
 
 var installerImplementation = {
     run: function () {
         new SteamScript()
             .name("Assassin’s Creed III")
             .editor("Ubisoft Montreal")
-            .author("Plata")
+            .author("Plata, KREYREN")
             .appId(208480)
             .wineVersion(LATEST_STAGING_VERSION)
             .wineDistribution("staging")
             .preInstall(function (wine/*, wizard*/) {
                 wine.DXVK();
+                wine.corefonts();
+                wine.vcrun2008();
             })
             .go();
     }
