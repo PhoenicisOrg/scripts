e6f60d2e10d597d7b86e332266445bdfff71dd15
Jacob Hrbek <werifgx@gmail.com>
2019-06-05 19:20:29 -0500

    Added DXVK

--- Applications/Games/Assassin's Creed/Steam/script.js
+++ Applications/Games/Assassin's Creed/Steam/script.js
@@ -1,4 +1,5 @@
 include("engines.wine.quick_script.steam_script");
+include("engines.wine.verbs.dxvk");
 
 var installerImplementation = {
     run: function () {
@@ -9,6 +10,9 @@ var installerImplementation = {
             .wineVersion(LATEST_STAGING_VERSION)
             .wineDistribution("staging")
             .appId(15100)
+            .preInstall(function (wine/*, wizard*/) {
+                wine.DXVK();
+            })
             .go();
     }
 };
