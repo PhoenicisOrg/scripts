a3daaceb8643a25e5786148ab286f40e014e0c75
ImperatorS79 <fevrier.simon@gmail.com>
2019-06-22 12:33:53 +0200

    Update script.js

--- Engines/Wine/Plugins/Font smoothing/script.js
+++ Engines/Wine/Plugins/Font smoothing/script.js
@@ -1,7 +1,7 @@
 include("engines.wine.engine.object");
 
 /**
- * force the Font smothing
+ * force the Font smoothing
  * @param {string} mode ("RGB", "BGR" or "Gray Scale")
  * @returns {Wine} Wine object
  */
