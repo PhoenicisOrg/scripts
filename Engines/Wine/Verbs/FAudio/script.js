6ac29c37cde2945e52b145bdea0c176a2bdf3099
Zemogiter <zumi999@vp.pl>
2019-06-25 11:01:52 +0200

    Update script.js
    Architecture check fixed thanks to @ImperatorS79

--- Engines/Wine/Verbs/FAudio/script.js
+++ Engines/Wine/Verbs/FAudio/script.js
@@ -11,8 +11,7 @@ include("utils.functions.filesystem.files");
 */
 Wine.prototype.faudio = function (faudioVersion) {
     if (this.architecture() != "amd64")
-        this.wizard().message(tr("FAudio does not support 32bit architecture."));
-        break;
+        throw "FAudio does not support 32bit architecture.";
     if (typeof faudioVersion !== 'string')
         faudioVersion = "19.06.07";
 
