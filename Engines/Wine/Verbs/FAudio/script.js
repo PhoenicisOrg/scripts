21e80bc1e6353a33d952883694b5b9cffee4dc5e
Marc Arndt <marc.arndt@masimi.de>
2019-06-28 22:58:27 +0200

    - change the new gallium9 script to the new format - some formatting

--- Engines/Wine/Verbs/FAudio/script.js
+++ Engines/Wine/Verbs/FAudio/script.js
@@ -6,23 +6,27 @@ include("utils.functions.filesystem.files");
 /**
  * Verb to install FAudio
  * see: https://github.com/Kron4ek/FAudio-Builds
- * 
+ *
  * @param {String} faudioVersion version of FAudio to downlaod
  * @returns {Wine} Wine object
  */
-Wine.prototype.faudio = function (faudioVersion) {
-    if (this.architecture() != "amd64")
-    {
+Wine.prototype.faudio = function(faudioVersion) {
+    if (this.architecture() != "amd64") {
         throw "FAudio does not support 32bit architecture.";
     }
-    if (typeof faudioVersion !== 'string')
-    {
+    if (typeof faudioVersion !== "string") {
         faudioVersion = "19.06.07";
     }
 
     var setupFile = new Resource()
         .wizard(this.wizard())
-        .url("https://github.com/Kron4ek/FAudio-Builds/releases/download/" + faudioVersion + "/faudio-" + faudioVersion + ".tar.xz")
+        .url(
+            "https://github.com/Kron4ek/FAudio-Builds/releases/download/" +
+                faudioVersion +
+                "/faudio-" +
+                faudioVersion +
+                ".tar.xz"
+        )
         .name("faudio-" + faudioVersion + ".tar.xz")
         .get();
 
@@ -37,7 +41,7 @@ Wine.prototype.faudio = function (faudioVersion) {
     var faudioDir = this.prefixDirectory() + "/FAudio/faudio-" + faudioVersion;
     var self = this;
 
-    forEach.call(ls(faudioDir + "/x64"), function (file) {
+    forEach.call(ls(faudioDir + "/x64"), function(file) {
         if (file.endsWith(".dll")) {
             cp(faudioDir + "/x64/" + file, sys64dir);
             self.overrideDLL()
@@ -47,7 +51,7 @@ Wine.prototype.faudio = function (faudioVersion) {
     });
 
     return this;
-}
+};
 
 /**
  * Verb to install FAudio
@@ -58,14 +62,17 @@ class FAudioVerb {
     }
 
     install(container) {
-        var wine = new Wine();
+        const wizard = SetupWizard(InstallationType.VERBS, "FAudio", java.util.Optional.empty());
+        const versions = ["19.06.07", "19.06", "19.05", "19.04", "19.03", "19.02", "19.01"];
+
+        const selectedVersion = wizard.menu(tr("Please select the version."), versions, "19.06.07");
+
+        const wine = new Wine();
         wine.prefix(container);
-        var wizard = SetupWizard(InstallationType.VERBS, "FAudio", java.util.Optional.empty());
-        var versions = ["19.06.07", "19.06", "19.05", "19.04", "19.03", "19.02", "19.01"];
-        var selectedVersion = wizard.menu(tr("Please select the version."), versions, "19.06.07");
         wine.wizard(wizard);
         // install selected version
         wine.faudio(selectedVersion.text);
+
         wizard.close();
     }
 }
