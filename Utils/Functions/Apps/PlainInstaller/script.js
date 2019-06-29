f86ee059378adc49c440768079e8b77e5ae32bd5
Marc Arndt <marc.arndt@masimi.de>
2019-06-29 18:06:22 +0200

    - fix codacy - add some comments

--- Utils/Functions/Apps/PlainInstaller/script.js
+++ Utils/Functions/Apps/PlainInstaller/script.js
@@ -1,8 +1,18 @@
+/**
+ * A "plain" script installer that is fully configurable.
+ */
+// eslint-disable-next-line no-unused-vars
 class PlainInstaller {
     constructor() {
         // do nothing
     }
 
+    /**
+     * Sets the installation script consisting of a lambda function
+     *
+     * @param {() => void} command The installation command
+     * @returns {PlainInstaller} The PlainInstaller object
+     */
     withScript(command) {
         this.command = command;
         return this;
