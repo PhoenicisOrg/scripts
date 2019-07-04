b1c1fc5592e4822f040862b9a8a2f45ec945d87d
Zemogiter <zumi999@vp.pl>
2019-07-04 20:04:19 +0200

    Update script.js
    Fixed spaces

--- Applications/Games/Space Engineers/Steam/script.js
+++ Applications/Games/Space Engineers/Steam/script.js
@@ -26,7 +26,7 @@ var installerImplementation = {
                 wizard.message(tr("You have to install libjpeg62 and libjpeg62-dev or else the thumbnails in New Game menu will be dispalyed as magenta rectangles."));
                 wizard.message(tr("Due to JIT compiler issues and the way this game uses multithreating, there are audio stutters. This script will attempt to minimize them but you might also have to enter the alsoft-conf command in terminal and set sample depth to 32bit float and peroid size to 2048."));
             })
-            .executable("Steam.exe", ["-silent", "-applaunch", 244850, "-no-ces-sandbox","-skipintro"]) 
+            .executable("Steam.exe", ["-silent", "-applaunch", 244850, "-no-ces-sandbox", "-skipintro"])
             .go();
     }
 };
