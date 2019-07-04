5c0d65551e81c4185d911ea4d63f2214f865162b
Zemogiter <zumi999@vp.pl>
2019-07-04 19:59:27 +0200

    Update script.js
    Added a message for user regarding audio stutter. Removed the "fix" for intro cutscene in favor of a proper one.

--- Applications/Games/Space Engineers/Steam/script.js
+++ Applications/Games/Space Engineers/Steam/script.js
@@ -4,7 +4,6 @@ include("engines.wine.verbs.vcrun2017");
 include("engines.wine.verbs.dxvk");
 include("engines.wine.verbs.faudio");
 include("engines.wine.plugins.override_dll");
-include("utils.functions.filesystem.files");
 
 var installerImplementation = {
     run: function () {
@@ -25,11 +24,9 @@ var installerImplementation = {
                     .set("native, builtin", ["msvcr120", "xaudio2_0", "xaudio2_1", "xaudio2_2", "xaudio2_3", "xaudio2_4", "xaudio2_5", "xaudio2_6", "xaudio2_7", "xaudio2_8", "xaudio2_9", "x3daudio1_3", "x3daudio1_4", "x3daudio1_5", "x3daudio1_6", "x3daudio1_7"])
                     .do();
                 wizard.message(tr("You have to install libjpeg62 and libjpeg62-dev or else the thumbnails in New Game menu will be dispalyed as magenta rectangles."));
+                wizard.message(tr("Due to JIT compiler issues and the way this game uses multithreating, there are audio stutters. This script will attempt to minimize them but you might also have to enter the alsoft-conf command in terminal and set sample depth to 32bit float and peroid size to 2048."));
             })
-            .postInstall(function (wine) {
-                remove(wine.prefixDirectory() + "drive_c/" + wine.programFiles() + "/Steam/steamapps/common/Space Engineers/Content/KSH.wmv");
-            })
-            .gameOverlay(false)
+            .executable("Steam.exe", ["-silent", "-applaunch", 244850, "-no-ces-sandbox","-skipintro"]) 
             .go();
     }
 };
