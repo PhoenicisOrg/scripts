c4747210739e205fc42f6668d21457ff1640dfaf
plata <plata@users.noreply.github.com>
2019-07-08 14:47:48 +0200

    Update font smoothing plugin (throw error if no mode specified)

--- Engines/Wine/Plugins/Font smoothing/script.js
+++ Engines/Wine/Plugins/Font smoothing/script.js
@@ -6,6 +6,10 @@ include("engines.wine.engine.object");
  * @returns {Wine} Wine object
  */
 Wine.prototype.fontSmoothing = function (mode) {
+    if (mode === undefined)
+    {
+        throw tr("No font smoothing mode specified!")
+    }
     let fontSmoothingType;
     let fontSmoothingOrientation;
     if (mode === "RGB") {
@@ -35,4 +39,4 @@ Wine.prototype.fontSmoothing = function (mode) {
 
     this.regedit().patch(regeditFileContent);
     return this;
-};
+}
