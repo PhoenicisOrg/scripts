2be93840ca053a4be09f2542b77c621d25b3e900
Zemogiter <zumi999@vp.pl>
2019-07-11 23:00:18 +0200

    Add files via upload
    Adding the header file with calls to `screenManager` Bean.

---
+++ Utils/Functions/System/virtual desktop/script.js
@@ -0,0 +1,19 @@
+var screenManager = Bean("screenManager");
+
+/**
+ * obtains the width of user's screen
+ * @returns {number} width of user's screen in pixels
+ */
+
+function getScreenWidth() {
+   return screenManager.getScreenWidth();
+}
+
+/**
+ * obtains the height of user's screen
+ * @returns {number} height of user's screen in pixels
+ */
+
+function getScreenHeight() {
+   return screenManager.getScreenHeight();
+}
