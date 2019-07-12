1169d54110e7c155d0df86951886f54a6f31a59c
Zemogiter <zumi999@vp.pl>
2019-07-12 08:55:24 +0200

    Update script.js

--- Engines/Wine/Plugins/virtual desktop/script.js
+++ Engines/Wine/Plugins/virtual desktop/script.js
@@ -24,7 +24,6 @@ Wine.prototype.setVirtualDesktop = function (width, height) {
         "\"Default\"=\"" + width + "x" + height + "\"\n" +
         "[HKEY_CURRENT_USER\\Software\\Wine\\Explorer]\n" +
         "\"Desktop\"=\"" + "Default" + "\"\n";
-    
     this.regedit().patch(regeditFileContent);
     return this;
 };
