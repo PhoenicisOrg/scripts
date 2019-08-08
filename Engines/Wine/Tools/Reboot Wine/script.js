4c71ff003dd433f4256c39c91d06789a4bc64621
Marc Arndt <marc.arndt@masimi.de>
2019-08-08 22:26:28 +0200

    - update tool includes

--- Engines/Wine/Tools/Reboot Wine/script.js
+++ Engines/Wine/Tools/Reboot Wine/script.js
@@ -1,5 +1,4 @@
 const Wine = include("engines.wine.engine.object");
-const {LATEST_STABLE_VERSION, LATEST_DEVELOPMENT_VERSION, LATEST_STAGING_VERSION, LATEST_DOS_SUPPORT_VERSION} = include("engines.wine.engine.versions");
 
 /**
  * Tool to reboot Wine
