9910f44913824bf8d814063f09abeaba4ec907d3
Zemogiter <zumi999@vp.pl>
2019-08-11 11:59:40 +0200

    Update script.js

--- Utils/Functions/System/virtual desktop/script.js
+++ Utils/Functions/System/virtual desktop/script.js
@@ -4,16 +4,10 @@ const screenManager = Bean("screenManager");
  * Obtains the width of user's screen
  * @returns {number} width of user's screen in pixels
  */
-// eslint-disable-next-line no-unused-vars
-function getScreenWidth() {
-    return screenManager.getScreenWidth();
-}
+module.getScreenWidth = getScreenWidth;
 
 /**
  * Obtains the height of user's screen
  * @returns {number} height of user's screen in pixels
  */
-// eslint-disable-next-line no-unused-vars
-function getScreenHeight() {
-    return screenManager.getScreenHeight();
-}
+module.getScreenHeight = getScreenHeight;
