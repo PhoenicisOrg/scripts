4329f07423e448eff3c24438726c77288f6ea2ba
Zemogiter <zumi999@vp.pl>
2019-08-12 20:14:53 +0200

    Update script.js
    Restored the definitions.

--- Utils/Functions/System/virtual desktop/script.js
+++ Utils/Functions/System/virtual desktop/script.js
@@ -4,10 +4,17 @@ const screenManager = Bean("screenManager");
  * Obtains the width of user's screen
  * @returns {number} width of user's screen in pixels
  */
-module.getScreenWidth = getScreenWidth;
+function getScreenWidth() {
+    return screenManager.getScreenWidth();
+}
 
 /**
  * Obtains the height of user's screen
  * @returns {number} height of user's screen in pixels
  */
+function getScreenHeight() {
+    return screenManager.getScreenHeight();
+}
+
+module.getScreenWidth = getScreenWidth;
 module.getScreenHeight = getScreenHeight;
