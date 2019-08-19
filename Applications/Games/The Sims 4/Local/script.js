2d5d942fcfc1dd67f04a69084a59ed98b8b492b4
Zemogiter <zumi999@vp.pl>
2019-08-19 20:29:05 +0200

    Update script.js
    Updated script structure

--- Applications/Games/The Sims 4/Local/script.js
+++ Applications/Games/The Sims 4/Local/script.js
@@ -1,4 +1,4 @@
-include("engines.wine.quick_script.local_installer_script");
+cinst LocalInstallerScript = include("engines.wine.quick_script.local_installer_script");
 include("engines.wine.verbs.vcrun6sp6");
 include("engines.wine.verbs.mfc42");
 include("engines.wine.verbs.dotnet20sp2");
@@ -8,31 +8,26 @@ include("engines.wine.verbs.d9vk");
 include("utils.functions.apps.resources");
 include("engines.wine.plugins.regedit");
 
-var installerImplementation = {
-    run: function () {
-        new LocalInstallerScript()
-            .name("The Sims 4")
-            .editor("Electronic Arts")
-            .applicationHomepage("https://www.ea.com/games/the-sims/the-sims-4/pc")
-            .author("ZemoScripter")
-            .category("Games")
-            .executable("TS4_x64.exe")
-            .wineArchitecture("amd64")
-            .wineVersion(4.7)
-            .wineDistribution("staging")
-            .preInstall(function (wine) {
-                wine.dotnet20sp2();
-                wine.vcrun6sp6();
-                wine.mfc42();
-                wine.vcrun2010();
-                wine.vcrun2013();
-                wine.D9VK();
-                var registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("registry.reg");
-                wine.regedit().patch(registrySettings);
-            })
-            .go();
-    }
-};
+new LocalInstallerScript()
+    .name("The Sims 4")
+    .editor("Electronic Arts")
+    .applicationHomepage("https://www.ea.com/games/the-sims/the-sims-4/pc")
+    .author("ZemoScripter")
+    .category("Games")
+    .executable("TS4_x64.exe")
+    .wineArchitecture("amd64")
+    .wineVersion(LATEST_STAGING_VERSION)
+    .wineDistribution("staging")
+    .preInstall(function (wine) {
+        wine.dotnet20sp2();
+        wine.vcrun6sp6();
+        wine.mfc42();
+        wine.vcrun2010();
+        wine.vcrun2013();
+        wine.D9VK();
+        var registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("registry.reg");
+        wine.regedit().patch(registrySettings);
+    })
 
 /* exported Installer */
 var Installer = Java.extend(org.phoenicis.scripts.Installer, installerImplementation);
