c13cf0d1a7570b2d6eb751724f477f207c0e278c
Zemogiter <zumi999@vp.pl>
2019-08-19 20:31:46 +0200

    Update script.js

--- Applications/Games/The Sims 4/Origin/script.js
+++ Applications/Games/The Sims 4/Origin/script.js
@@ -1,4 +1,4 @@
-include("engines.wine.quick_script.origin_script");
+const OriginScript = include("engines.wine.quick_script.origin_script");
 include("engines.wine.verbs.vcrun6sp6");
 include("engines.wine.verbs.mfc42");
 include("engines.wine.verbs.dotnet20sp2");
@@ -9,34 +9,26 @@ include("utils.functions.apps.resources");
 include("engines.wine.plugins.regedit");
 include("engines.wine.plugins.override_dll");
 
-var installerImplementation = {
-    run: function () {
-        new OriginScript()
-            .name("The Sims 4")
-            .editor("Electronic Arts")
-            .applicationHomepage("https://www.ea.com/games/the-sims/the-sims-4/pc")
-            .author("ZemoScripter")
-            .category("Games")
-            .wineVersion("4.3")
-            .wineDistribution("staging")
-            .wineArchitecture("amd64")
-            .appId("1011164,1015875,1015876,1015793,1015794,1015795,1015806,1015807,1015808,1018025,1018023,1015236,1015235,1015224")
-            .preInstall(function (wine) {
-                wine.dotnet20sp2();
-                wine.vcrun6sp6();
-                wine.mfc42();
-                wine.vcrun2010();
-                wine.vcrun2013();
-                wine.D9VK();
-                var registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("registry.reg");
-                wine.regedit().patch(registrySettings);
-                this.overrideDLL()
-                    .set("disabled", ["nvapi","nvapi64"])
-                    .do();
-            })
-            .go();
-    }
-};
-
-/* exported Installer */
-var Installer = Java.extend(org.phoenicis.scripts.Installer, installerImplementation);
+new OriginScript()
+    .name("The Sims 4")
+    .editor("Electronic Arts")
+    .applicationHomepage("https://www.ea.com/games/the-sims/the-sims-4/pc")
+    .author("ZemoScripter")
+    .category("Games")
+    .wineVersion("4.3")
+    .wineDistribution("staging")
+    .wineArchitecture("amd64")
+    .appId("1011164,1015875,1015876,1015793,1015794,1015795,1015806,1015807,1015808,1018025,1018023,1015236,1015235,1015224")
+    .preInstall(function (wine) {
+        wine.dotnet20sp2();
+        wine.vcrun6sp6();
+        wine.mfc42();
+        wine.vcrun2010();
+        wine.vcrun2013();
+        wine.D9VK();
+        var registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("registry.reg");
+        wine.regedit().patch(registrySettings);
+        this.overrideDLL()
+            .set("disabled", ["nvapi","nvapi64"])
+            .do();
+    })
