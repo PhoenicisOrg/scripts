7665d5bb6024e72e0aaaf34895821edb84ff51da
Zemogiter <zumi999@vp.pl>
2019-08-24 17:15:21 +0200

    Update script.js

--- Engines/Wine/Verbs/D9VK/script.js
+++ Engines/Wine/Verbs/D9VK/script.js
@@ -22,21 +22,20 @@ Wine.prototype.D9VK = function (d9vkVersion) {
         const answer = uiQuestionFactory.create(
             tr("D9VK is currently unsupported on non-Linux operating systems due to MoltenVK implementation being incomplete. Select how do you want to approach this situation."),
             ["YES, continue with D9VK installation regardless", "NO, quit script alltogether", "Exit D9VK Installer, but continue with the script"]
-         );
-         
-         if (!answer || answer == "Exit D9VK Installer, but continue with the script") {
+        );
+        if (!answer || answer == "Exit D9VK Installer, but continue with the script") {
             return this;
-         }
-         if (answer == "NO, quit script alltogether") {
-             throw "User aborted the script.";
-         }
+        }
+        if (answer == "NO, quit script alltogether") {
+            throw "User aborted the script.";
+        }
     }
-    else 
+    else
     {
         this.wizard().message(tr("Please ensure you have the latest drivers (418.30 minimum for NVIDIA and mesa 19 for AMD) or else D9VK might not work correctly."));
     }
 
-    if (typeof d9vkVersion !== 'string') 
+    if (typeof d9vkVersion !== 'string')
     {
         d9vkVersion = "0.13f";
     }
