cfdc573082f5e24318bba3d0236ffed9302427f4
ImperatorS79 <fevrier.simon@gmail.com>
2019-08-25 12:06:04 +0200

    Update script.js

--- Applications/Games/League of Legends/Online/script.js
+++ Applications/Games/League of Legends/Online/script.js
@@ -27,7 +27,7 @@ new CustomInstallerScript()
         ];
         const selectedRegion = wizard.menu(tr("Select your region:"), regions);
         const baseUrl = "https://riotgamespatcher-a.akamaihd.net/releases/live/installer/deploy/League%20of%20Legends%20installer%20";
-        let url, regionID;       
+        let url, regionID;
         switch (selectedRegion.text){
             case "EU West":
                 regionID = "EUW";
