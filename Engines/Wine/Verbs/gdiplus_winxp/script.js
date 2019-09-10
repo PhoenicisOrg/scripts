a4a79b0ca491f48dccb4d3dcb84fdde59717124d
ImperatorS79 <fevrier.simon@gmail.com>
2019-09-10 20:31:01 +0200

    Update script.js

--- Engines/Wine/Verbs/gdiplus_winxp/script.js
+++ Engines/Wine/Verbs/gdiplus_winxp/script.js
@@ -11,7 +11,7 @@ include("engines.wine.plugins.override_dll");
  * @returns {Wine} Wine object
  */
 Wine.prototype.gdiplusWinXP = function () {
-    var setupFile = new Resource()
+    const setupFile = new Resource()
         .wizard(this.wizard())
         .url("https://download.microsoft.com/download/1/4/6/1467c2ba-4d1f-43ad-8d9b-3e8bc1c6ac3d/NDP1.0sp2-KB830348-X86-Enu.exe")
         .checksum("6113cd89d77525958295ccbd73b5fb8b89abd0aa")
@@ -23,7 +23,7 @@ Wine.prototype.gdiplusWinXP = function () {
         .to(this.prefixDirectory() + "/drive_c/gdiplus/")
         .extract(["-F", "FL_gdiplus_dll_____X86.3643236F_FC70_11D3_A536_0090278A1BB8"]);
 		
-    var content = cat(this.prefixDirectory() + "/drive_c/gdiplus/FL_gdiplus_dll_____X86.3643236F_FC70_11D3_A536_0090278A1BB8");
+    const content = cat(this.prefixDirectory() + "/drive_c/gdiplus/FL_gdiplus_dll_____X86.3643236F_FC70_11D3_A536_0090278A1BB8");
     writeToFile(this.system32directory() + "/gdiplus.dll", content);
 
     remove(this.prefixDirectory() + "/drive_c/gdiplus/");
@@ -45,9 +45,9 @@ module.default = class GdiplusWinXPVerb {
     }
 
     install(container) {
-        var wine = new Wine();
+        const wine = new Wine();
         wine.prefix(container);
-        var wizard = SetupWizard(InstallationType.VERBS, "gdiplus (windows xp)", java.util.Optional.empty());
+        const wizard = SetupWizard(InstallationType.VERBS, "gdiplus (windows xp)", java.util.Optional.empty());
         wine.wizard(wizard);
         wine.gdiplus_winxp();
         wizard.close();
