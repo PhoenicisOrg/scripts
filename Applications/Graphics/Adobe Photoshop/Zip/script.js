8f0243e860e8bcccc75424fd92ff09673bc7a8c4
ImperatorS79 <fevrier.simon@gmail.com>
2019-09-10 23:22:57 +0200

    Update script.js

--- Applications/Graphics/Adobe Photoshop/Zip/script.js
+++ Applications/Graphics/Adobe Photoshop/Zip/script.js
@@ -1,13 +1,13 @@
 const ZipScript = include("engines.wine.quick_script.zip_script");
 const {LATEST_STAGING_VERSION} = include("engines.wine.engine.versions");
 include("engines.wine.plugins.font_smoothing");
-include("engines.wine.verbs.corefonts");
-include("engines.wine.verbs.gdiplus");
-include("engines.wine.verbs.vcrun2015");
-include("engines.wine.verbs.msxml3");
-include("engines.wine.verbs.msxml6");
-include("engines.wine.verbs.atmlib");
-include("engines.wine.verbs.adobeair");
+const CoreFonts = include("engines.wine.verbs.corefonts");
+const GDIPlus = include("engines.wine.verbs.gdiplus");
+const Vcrun2015 = include("engines.wine.verbs.vcrun2015");
+const Msxml3 = include("engines.wine.verbs.msxml3");
+const Msxml6 = include("engines.wine.verbs.msxml6");
+const Atmlib = include("engines.wine.verbs.atmlib");
+const AdobeAir = include("engines.wine.verbs.adobeair");
 
 
 new ZipScript()
@@ -21,12 +21,12 @@ new ZipScript()
     .setupPathInsidePrefix("Adobe Photoshop/Set-up.exe")
     .url("http://prdl-download.adobe.com/Photoshop/55E8FC8663C847F08BFBCD8DFE336AE8/1546595903133/AdobePhotoshop20-mul_x64.zip")
     .preInstall(function (wine /*, wizard*/) {
-        wine.corefonts();
-        wine.gdiplus();
-        wine.vcrun2015();
-        wine.atmlib();
-        wine.msxml3();
-        wine.msxml6();
-        wine.adobeair();
+        new Corefonts(wine).go();
+        new GDIPlus(wine).go();
+        new Vcrun2015(wine).go();
+        new Atmlib(wine).go();
+        new Msxml3(wine).go();
+        new Msxml6(wine).go();
+        new AdobeAir(wine).go();
         wine.fontSmoothing("RGB");
     });
