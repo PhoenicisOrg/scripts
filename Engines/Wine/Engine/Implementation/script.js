9e50acf699d2affafdb4b3e0b6fe1710b838a9b1
plata <plata@users.noreply.github.com>
2019-09-11 18:31:50 +0200

    Avoid that runtime is installed multiple times during one installation (#1123)
    fixes #1055

--- Engines/Wine/Engine/Implementation/script.js
+++ Engines/Wine/Engine/Implementation/script.js
@@ -26,6 +26,7 @@ module.default = class WineEngine {
         this._wineWebServiceUrl = propertyReader.getProperty("webservice.wine.url");
         this._wizard = null;
         this._workingContainer = "";
+        this._fetchedRuntimeJson = false;
     }
 
     getLocalDirectory(subCategory, version) {
@@ -109,6 +110,11 @@ module.default = class WineEngine {
         }
     }
     _installRuntime(setupWizard) {
+        // avoid that runtime is installed multiple times during one installation
+        if (this._fetchedRuntimeJson) {
+            return;
+        }
+
         const runtimeJsonPath = this._wineEnginesDirectory + "/runtime.json";
         let runtimeJson;
         let runtimeJsonFile;
@@ -261,6 +267,8 @@ module.default = class WineEngine {
 
             remove(this._wineEnginesDirectory + "/TMP");
         }
+
+        this._fetchedRuntimeJson = true;
     }
 
     _installGecko(setupWizard, winePackage, localDirectory) {
