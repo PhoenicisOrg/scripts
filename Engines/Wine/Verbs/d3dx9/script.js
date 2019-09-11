79a7331c65d958e1bf31628beea029f3a072442b
ImperatorS79 <fevrier.simon@gmail.com>
2019-09-11 21:05:47 +0200

    Update script.js

--- Engines/Wine/Verbs/d3dx9/script.js
+++ Engines/Wine/Verbs/d3dx9/script.js
@@ -47,6 +47,7 @@ class D3DX9 {
         const wizard = this.wine.wizard();
         const prefixDirectory = this.wine.prefixDirectory();
         const system32directory = this.wine.system32directory();
+        const architecture = this.wine.architecture();
 
         const setupFile = new Resource()
             .wizard(wizard)
@@ -92,7 +93,7 @@ class D3DX9 {
 
         this.extractDirectXToSystemDirectory(progressBar, filesToExtractx86, system32directory, "d3dx9*.dll");
 
-        if (this.architecture() == "amd64") {
+        if (architecture == "amd64") {
             const system64directory = this.wine.system64directory();
 
             new CabExtract()
