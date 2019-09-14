71d9c47e3b7112e16041bc1d4c9d73f7ae06d38b
Zemogiter <zumi999@vp.pl>
2019-09-14 12:25:14 +0200

    Update script.js

--- Applications/Games/The Sims 4/Origin/script.js
+++ Applications/Games/The Sims 4/Origin/script.js
@@ -1,14 +1,14 @@
 const OriginScript = include("engines.wine.quick_script.origin_script");
 const {LATEST_STAGING_VERSION} = include("engines.wine.engine.versions");
 const AppResource = include("utils.functions.apps.resources");
-include("engines.wine.verbs.vcrun6sp6");
-include("engines.wine.verbs.mfc42");
-include("engines.wine.verbs.dotnet20sp2");
-include("engines.wine.verbs.vcrun2010");
-include("engines.wine.verbs.vcrun2013");
-include("engines.wine.verbs.d9vk");
-include("engines.wine.plugins.regedit");
-include("engines.wine.plugins.override_dll");
+const vcrun6sp6 = include("engines.wine.verbs.vcrun6sp6");
+const MFC42 = include("engines.wine.verbs.mfc42");
+const dotnet20sp2 = include("engines.wine.verbs.dotnet20sp2");
+const vcrun2010 = include("engines.wine.verbs.vcrun2010");
+const vcrun2013 = include("engines.wine.verbs.vcrun2013");
+const D9VK = include("engines.wine.verbs.d9vk");
+const regedit = include("engines.wine.plugins.regedit");
+const overrideDLL = include("engines.wine.plugins.override_dll");
 
 new OriginScript()
     .name("The Sims 4")
@@ -21,12 +21,12 @@ new OriginScript()
     .wineArchitecture("amd64")
     .appId("1011164,1015875,1015876,1015793,1015794,1015795,1015806,1015807,1015808,1018025,1018023,1015236,1015235,1015224")
     .preInstall(function (wine) {
-        wine.dotnet20sp2();
-        wine.vcrun6sp6();
-        wine.mfc42();
-        wine.vcrun2010();
-        wine.vcrun2013();
-        wine.D9VK();
+        new dotnet20sp2(wine).go();
+        new vcrun6sp6(wine).go();
+        new MFC(wine).go();
+        new vcrun2010(wine).go();
+        new vcrun2013(wine).go();
+        new D9VK(wine).go();
         const registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("registry.reg");
         wine.regedit().patch(registrySettings);
         wine.overrideDLL()
