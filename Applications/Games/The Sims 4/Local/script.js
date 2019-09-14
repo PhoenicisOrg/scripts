371b32a2d6577b15b3ce6a6a551f8d47c468c3fb
Zemogiter <zumi999@vp.pl>
2019-09-14 12:26:16 +0200

    Update script.js

--- Applications/Games/The Sims 4/Local/script.js
+++ Applications/Games/The Sims 4/Local/script.js
@@ -1,13 +1,14 @@
 const LocalInstallerScript = include("engines.wine.quick_script.local_installer_script");
 const {LATEST_STAGING_VERSION} = include("engines.wine.engine.versions");
 const AppResource = include("utils.functions.apps.resources");
-include("engines.wine.verbs.vcrun6sp6");
-include("engines.wine.verbs.mfc42");
-include("engines.wine.verbs.dotnet20sp2");
-include("engines.wine.verbs.vcrun2010");
-include("engines.wine.verbs.vcrun2013");
-include("engines.wine.verbs.d9vk");
-include("engines.wine.plugins.regedit");
+const vcrun6sp6 = include("engines.wine.verbs.vcrun6sp6");
+const MFC42 = include("engines.wine.verbs.mfc42");
+const dotnet20sp2 = include("engines.wine.verbs.dotnet20sp2");
+const vcrun2010 = include("engines.wine.verbs.vcrun2010");
+const vcrun2013 = include("engines.wine.verbs.vcrun2013");
+const D9VK = include("engines.wine.verbs.d9vk");
+const regedit = include("engines.wine.plugins.regedit");
+const overrideDLL = include("engines.wine.plugins.override_dll");
 
 new LocalInstallerScript()
     .name("The Sims 4")
@@ -20,12 +21,12 @@ new LocalInstallerScript()
     .wineVersion(LATEST_STAGING_VERSION)
     .wineDistribution("staging")
     .preInstall(function (wine) {
-        wine.dotnet20sp2();
-        wine.vcrun6sp6();
-        wine.mfc42();
-        wine.vcrun2010();
-        wine.vcrun2013();
-        wine.D9VK();
+        new dotnet20sp2(wine).go();
+        new vcrun6sp6(wine).go();
+        new MFC(wine).go();
+        new vcrun2010(wine).go();
+        new vcrun2013(wine).go();
+        new D9VK(wine).go();
         const registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("registry.reg");
         wine.regedit().patch(registrySettings);
         wine.overrideDLL()
