7d9eae66a4f5d0665eab5739126e6442a82a6560
Zemogiter <zumi999@vp.pl>
2019-09-14 10:16:50 +0200

    Update script.js

--- Applications/Games/The Sims 2/Local/script.js
+++ Applications/Games/The Sims 2/Local/script.js
@@ -1,10 +1,11 @@
 const LocalScript = include("engines.wine.quick_script.local_installer_script");
-include("engines.wine.verbs.vcrun2010");
-include("engines.wine.verbs.vcrun2013");
+const AppResource = include("utils.functions.apps.resources");
+const vcrun2010 = include("engines.wine.verbs.vcrun2010");
+const vcrun2013 = include("engines.wine.verbs.vcrun2013");
+
 include("utils.functions.net.resource");
 include("utils.functions.filesystem.files");
 include("utils.functions.filesystem.extract");
-include("utils.functions.apps.resources");
 
 new LocalInstallerScript()
     .name("The Sims 2")
@@ -16,8 +17,8 @@ new LocalInstallerScript()
     .wineVersion("4.5")
     .wineDistribution("staging")
     .preInstall(function (wine) {
-        wine.vcrun2010();
-        wine.vcrun2013();
+        new vcrun2010(wine).go();
+        new vcrun2013(wine).go();
         var registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("registry.reg");
         wine.regedit().patch(registrySettings);
      })
@@ -33,7 +34,7 @@ new LocalInstallerScript()
             .archive(fixes)
             .to(wine.prefixDirectory() + "/drive_c/users/" + username + "My Documents/EA Games/The Sims\u2122 2 Ultimate Collection/Downloads")
             .extract();
-        var configFile = wine.prefixDirectory() + "drive_c/users/" + username + "My Documents/EA Games/The Sims\u2122 2 Ultimate Collection/Config/userstartup.cheat";
+        const configFile = wine.prefixDirectory() + "drive_c/users/" + username + "My Documents/EA Games/The Sims\u2122 2 Ultimate Collection/Config/userstartup.cheat";
         touch(configFile);
         writeToFile(configFile, "boolprop useshaders true\nboolProp   createNVidiaWorkaroundTexture false\nboolProp   bumpMapping false");
     })
