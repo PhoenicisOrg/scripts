50b98e4b296bd6bb5800f89c2c6fc44245618897
Zemogiter <zumi999@vp.pl>
2019-09-20 18:08:21 +0200

    Update script.js
    Using more standard const names and redid the faudio message to fit more scenarios.

--- Applications/Games/Space Engineers/Steam/script.js
+++ Applications/Games/Space Engineers/Steam/script.js
@@ -1,6 +1,6 @@
 const SteamScript = include("engines.wine.quick_script.steam_script");
-const DotNET472 = include("engines.wine.verbs.dotnet472");
-const Vcrun2017 = include("engines.wine.verbs.vcrun2017");
+const dotnet472 = include("engines.wine.verbs.dotnet472");
+const vcrun2017 = include("engines.wine.verbs.vcrun2017");
 const DXVK = include("engines.wine.verbs.dxvk");
 
 include("engines.wine.plugins.override_dll");
@@ -14,13 +14,13 @@ new SteamScript()
     .wineDistribution("upstream")
     .wineArchitecture("amd64")
     .preInstall(function (wine, wizard) {
-        new DotNET472(wine).go();
-        new Vcrun2017(wine).go();
+        new dotnet472(wine).go();
+        new vcrun2017(wine).go();
         new DXVK(wine).go();
         wine.overrideDLL()
             .set("native, builtin", ["msvcr120"])
             .do();
         wizard.message(tr("You have to install libjpeg62 package or else the thumbnails in New Game menu will be dispalyed as magenta rectangles."));
-        wizard.message(tr("Due to JIT compiler issues and the way this game uses multithreating, there are audio stutters. To minimize those (assuming you are on a Debian-based Linux distribution), download the libFAudio package from this PPA:\nhttps://launchpad.net/~cybermax-dexter/+archive/ubuntu/sdl2-backport"));
+        wizard.message(tr("Due to JIT compiler issues and the way this game uses multithreating, there are audio stutters. If you are on a Debian-based distribution you can try the libFAudio package from this PPA:\nhttps://launchpad.net/~cybermax-dexter/+archive/ubuntu/sdl2-backport"));
     })
     .executable("Steam.exe", ["-silent", "-applaunch", "244850", "-no-cef-sandbox", "-skipintro"]);
