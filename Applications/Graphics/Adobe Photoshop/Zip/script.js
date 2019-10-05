dfd253a417a802969e8f07cc44bb342d0267af95
ImperatorS79 <fevrier.simon@gmail.com>
2019-10-05 11:06:35 +0200

    Update script.js

--- Applications/Graphics/Adobe Photoshop/Zip/script.js
+++ Applications/Graphics/Adobe Photoshop/Zip/script.js
@@ -1,6 +1,6 @@
 const ZipScript = include("engines.wine.quick_script.zip_script");
 const {LATEST_STAGING_VERSION} = include("engines.wine.engine.versions");
-include("engines.wine.plugins.font_smoothing");
+const FontSmoothing = include("engines.wine.plugins.font_smoothing");
 const Corefonts = include("engines.wine.verbs.corefonts");
 const GDIPlus = include("engines.wine.verbs.gdiplus");
 const Vcrun2015 = include("engines.wine.verbs.vcrun2015");
@@ -20,7 +20,7 @@ new ZipScript()
     .wineDistribution("staging")
     .setupPathInsidePrefix("Adobe Photoshop/Set-up.exe")
     .url("http://prdl-download.adobe.com/Photoshop/55E8FC8663C847F08BFBCD8DFE336AE8/1546595903133/AdobePhotoshop20-mul_x64.zip")
-    .preInstall(function (wine /*, wizard*/) {
+    .preInstall(function (wine) {
         new Corefonts(wine).go();
         new GDIPlus(wine).go();
         new Vcrun2015(wine).go();
@@ -28,5 +28,5 @@ new ZipScript()
         new Msxml3(wine).go();
         new Msxml6(wine).go();
         new AdobeAir(wine).go();
-        wine.fontSmoothing("RGB");
+        new FontSmoothing(wine).withMode("RGB").go();
     });
