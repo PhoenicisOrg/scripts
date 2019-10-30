53dbb87057d60a78517583e3f2250b45890e03e2
Zemogiter <zumi999@vp.pl>
2019-10-30 19:37:23 +0100

    Update script.js
    Using different link for fixes constant, moved the username constant up, added an env value.

--- Applications/Games/The Sims 2/Origin/script.js
+++ Applications/Games/The Sims 2/Origin/script.js
@@ -15,17 +15,19 @@ new OriginScript()
     .category("Games")
     .wineVersion("4.5")
     .wineDistribution("staging")
+    .wineArchitecture("amd64")
     .appId("1014457,sims2_apt_life,sims2_bestofbusiness_dd,sims2_bonvoyage_na,sims2dd_remaster,sims2_freetime,sims2_funwpets_dd,sims2_stuffpackglamour_na,sims2_holiday2_na,sims2-holiday,sims2_inseason_na,sims2_collegepack_dd")
     .preInstall(function (wine) {
         new vcrun2010(wine).go();
         new vcrun2013(wine).go();
-        const registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("registry.reg");
-        wine.regedit().patch(registrySettings);
+        //const registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("registry.reg");
+        //wine.regedit().patch(registrySettings);
     })
     .postInstall(function (wine) {
+        const username = System.getProperty("user.name");
         const fixes = new Resource()
             .wizard(wine.wizard())
-            .url("https://raw.githubusercontent.com/tannisroot/installer-fixes/master/sims2_fixes.tar.xz")
+            .url("https://github.com/tannisroot/installer-fixes/raw/master/sims2_fixes.tar.xz")
             .name("sims2_fixes.tar.xz")
             .get();
     
@@ -35,8 +37,8 @@ new OriginScript()
             .to(wine.prefixDirectory() + "/drive_c/users/" + username + "My Documents/EA Games/The Sims\u2122 2 Ultimate Collection/Downloads")
             .extract();
     
-        const username = System.getProperty("user.name");
         const configFile = wine.prefixDirectory() + "drive_c/users/" + username + "My Documents/EA Games/The Sims\u2122 2 Ultimate Collection/Config/userstartup.cheat";
         touch(configFile);
         writeToFile(configFile, "boolprop useshaders true\nboolProp   createNVidiaWorkaroundTexture false\nboolProp   bumpMapping false");
     })
+    .environment('{ "LARGE_ADDRESS_AWARE": "1"}')
