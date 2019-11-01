01282f758a7f86a2b01927ae55d7a4e6c50dc35c
Zemogiter <zumi999@vp.pl>
2019-11-01 12:17:06 +0100

    Update script.js
    Added a DXVK config file to disable NVAPI workarround for extra performance with mods.

--- Applications/Games/Space Engineers/Steam/script.js
+++ Applications/Games/Space Engineers/Steam/script.js
@@ -3,6 +3,7 @@ const dotnet472 = include("engines.wine.verbs.dotnet472");
 const vcrun2017 = include("engines.wine.verbs.vcrun2017");
 const DXVK = include("engines.wine.verbs.dxvk");
 const OverrideDLL = include("engines.wine.plugins.override_dll");
+const { touch, writeToFile } = include("utils.functions.filesystem.files");
 
 new SteamScript()
     .name("Space Engineers")
@@ -20,7 +21,11 @@ new SteamScript()
         new OverrideDLL(wine)
             .withMode("native, builtin", ["msvcr120"])
             .go();
+        const dxvkConfigFile = wine.prefixDirectory() + "/drive_c/dxvk.conf";
+        touch(dxvkConfigFile);
+        writeToFile(dxvkConfigFile, "dxgi.nvapiHack = False");
         wizard.message(tr("You have to install libjpeg62 package or else the thumbnails in New Game menu will be dispalyed as magenta rectangles."));
         wizard.message(tr("Due to JIT compiler issues and the way this game uses multithreating, there are audio stutters. If you are on a Debian-based distribution you can try the libFAudio package from this PPA:\nhttps://launchpad.net/~cybermax-dexter/+archive/ubuntu/sdl2-backport"));
     })
     .executable("Steam.exe", ["-silent", "-applaunch", "244850", "-no-cef-sandbox", "-skipintro"]);
+    .environment('{"DXVK_CONFIG_FILE": "configFile"}')
