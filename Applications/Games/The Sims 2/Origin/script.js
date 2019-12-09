bed2d92d0630e07c3fa8a2050c3c9e557d662d98
Zemogiter <zumi999@vp.pl>
2019-12-09 11:11:32 +0100

    Update script.js
    Made the script more similiar to Lutris one.

--- Applications/Games/The Sims 2/Origin/script.js
+++ Applications/Games/The Sims 2/Origin/script.js
@@ -1,11 +1,12 @@
 const OriginScript = include("engines.wine.quick_script.origin_script");
 const vcrun2010 = include("engines.wine.verbs.vcrun2010");
 const vcrun2013 = include("engines.wine.verbs.vcrun2013");
-//const AppResource = include("utils.functions.apps.resources");
 const System = Java.type("java.lang.System");
 const Extractor = include("utils.functions.filesystem.extract");
 const Resource = include("utils.functions.net.resource");
 const {touch, writeToFile} = include("utils.functions.filesystem.files");
+const D9VK = include("engines.wine.verbs.d9vk");
+
 
 new OriginScript()
     .name("The Sims 2")
@@ -13,15 +14,20 @@ new OriginScript()
     .applicationHomepage("http://thesims2.ea.com")
     .author("ZemoScripter")
     .category("Games")
-    .wineVersion("4.5")
-    .wineDistribution("staging")
+    .wineVersion("4.20")
+    .wineDistribution("upstream")
     .wineArchitecture("amd64")
     .appId("1014457,sims2_apt_life,sims2_bestofbusiness_dd,sims2_bonvoyage_na,sims2dd_remaster,sims2_freetime,sims2_funwpets_dd,sims2_stuffpackglamour_na,sims2_holiday2_na,sims2-holiday,sims2_inseason_na,sims2_collegepack_dd")
     .preInstall(function (wine) {
         new vcrun2010(wine).go();
         new vcrun2013(wine).go();
-        //const registrySettings = new AppResource().application([TYPE_ID, CATEGORY_ID, APPLICATION_ID]).get("registry.reg");
-        //wine.regedit().patch(registrySettings);
+        new D9VK(wine).go();
+        const dxvkConfigFile = wine.prefixDirectory() + "/drive_c/dxvk.conf";
+        touch(dxvkConfigFile);
+        writeToFile(dxvkConfigFile, "dxgi.nvapiHack = False");
+        new OverrideDLL(wine)
+        .withMode("disabled", ["nvapi, nvapi64"])
+        .go();
     })
     .postInstall(function (wine) {
         const username = System.getProperty("user.name");
@@ -39,6 +45,6 @@ new OriginScript()
 
         const configFile = wine.prefixDirectory() + "drive_c/users/" + username + "My Documents/EA Games/The Sims\u2122 2 Ultimate Collection/Config/userstartup.cheat";
         touch(configFile);
-        writeToFile(configFile, "boolprop useshaders true\nboolProp   createNVidiaWorkaroundTexture false\nboolProp   bumpMapping false");
+        writeToFile(configFile, "boolprop useshaders true");
     })
-    .environment('{ "LARGE_ADDRESS_AWARE": "1"}')
+    .environment('{ "LARGE_ADDRESS_AWARE": "1", "DXVK_CONFIG_FILE": "configFile"}')
