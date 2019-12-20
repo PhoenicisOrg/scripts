e43bbbc164d342b83d539f628106c8c7d128b476
Zemogiter <zumi999@vp.pl>
2019-12-20 12:24:56 +0100

    Update script.js

--- Applications/Games/The Sims 4/Origin/script.js
+++ Applications/Games/The Sims 4/Origin/script.js
@@ -1,6 +1,7 @@
 const OriginScript = include("engines.wine.quick_script.origin_script");
 const vcrun2010 = include("engines.wine.verbs.vcrun2010");
 const vcrun2013 = include("engines.wine.verbs.vcrun2013");
+const vcrun2015 = include("engines.wine.verbs.vcrun2015");
 const D9VK = include("engines.wine.verbs.d9vk");
 const { touch, writeToFile } = include("utils.functions.filesystem.files");
 const OverrideDLL = include("engines.wine.plugins.override_dll");
@@ -11,13 +12,14 @@ new OriginScript()
     .applicationHomepage("https://www.ea.com/games/the-sims/the-sims-4/pc")
     .author("ZemoScripter")
     .category("Games")
-    .wineVersion("4.20")
+    .wineVersion("4.21")
     .wineDistribution("upstream")
     .wineArchitecture("amd64")
     .appId("1011164,1015875,1015876,1015793,1015794,1015795,1015806,1015807,1015808,1018025,1018023,1015236,1015235,1015224")
     .preInstall(function (wine) {
         new vcrun2010(wine).go();
         new vcrun2013(wine).go();
+        new vcrun2015(wine).go();
         new D9VK(wine).go();
         var configFile = wine.prefixDirectory() + "/drive_c/dxvk.conf";
         touch(configFile);
