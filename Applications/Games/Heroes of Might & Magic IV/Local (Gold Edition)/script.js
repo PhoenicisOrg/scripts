84af7b0b21db977af96d0f7cb9b872e963a65792
Zemogiter <zumi999@vp.pl>
2020-01-02 16:10:48 +0100

    Update script.js
    Corrected wizard usage, using new wine version calling

--- Applications/Games/Heroes of Might & Magic IV/Local (Gold Edition)/script.js
+++ Applications/Games/Heroes of Might & Magic IV/Local (Gold Edition)/script.js
@@ -1,22 +1,21 @@
 const LocalInstallerScript = include("engines.wine.quick_script.local_installer_script");
 const Resource = include("utils.functions.net.resource");
-const {LATEST_DEVELOPMENT_VERSION} = include("engines.wine.engine.versions");
+const { getLatestDevelopmentVersion } = include("engines.wine.engine.versions");
 const Downloader = include("utils.functions.net.download");
 
 new LocalInstallerScript()
     .name("Heroes of Might & Magic IV")
     .editor("New World Computing, Inc.")
-    .applicationHomepage("")
     .author("Zemogiter")
     .category("Games")
     .wineDistribution("upstream")
-    .wineVersion(LATEST_DEVELOPMENT_VERSION)
+    .wineVersion(getLatestDevelopmentVersion)
     .executable("H4mod.exe")
     .postInstall(function (wine) {
         const dataDirectory = wine.prefixDirectory() + "drive_c/" + wine.programFiles() + "Ubisoft/Heroes of Might & Magic IV - Złota Edycja/Data";
 
         const patchForGameExecutable = new Resource()
-            .wizard(this._wizard)
+            .wizard(wine.wizard)
             .url("https://equilibris.celestialheavens.com/downloads/heroes4_sfx.exe")
             .checksum("373df5403ada0d2bb9285862efeca00415cb0915")
             .name("heroes4_sfx.exe")
@@ -24,7 +23,7 @@ new LocalInstallerScript()
         wine.run(patchForGameExecutable);
 
         const patchForCampaignEditor = new Resource()
-            .wizard(this._wizard)
+            .wizard(wine.wizard)
             .url("https://equilibris.celestialheavens.com/downloads/camp_ed_sfx.exe")
             .checksum("9d34c409a0358057b0c2d00b7d4c96ccd67c7214")
             .name("camp_ed_sfx.exe")
@@ -32,14 +31,14 @@ new LocalInstallerScript()
         wine.run(patchForCampaignEditor);
 
         new Downloader()
-            .wizard(this._wizard)
+            .wizard(wine.wizard)
             .url("https://equilibris.celestialheavens.com/downloads/equi.aop")
             .checksum("3a188a45017a1fd7cb38d6883428f6abc9b6e160")
             .to(dataDirectory)
             .get();
 
         const mod = new Resource()
-            .wizard(this._wizard)
+            .wizard(wine.wizard)
             .url("https://equilibris.celestialheavens.com/downloads/install_equilibris_v.3.51.exe")
             .checksum("3a188a45017a1fd7cb38d6883428f6abc9b6e160")
             .name("install_equilibris_v.3.51.exe")
