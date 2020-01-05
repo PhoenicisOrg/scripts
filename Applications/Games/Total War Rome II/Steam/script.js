08a2e1f475629599ff765834e25104ffd4895b68
Zemogiter <zumi999@vp.pl>
2020-01-05 14:17:55 +0100

    Update script.js

--- Applications/Games/Total War Rome II/Steam/script.js
+++ Applications/Games/Total War Rome II/Steam/script.js
@@ -22,7 +22,6 @@ new SteamScript()
         wizard.message(
             tr(
                 "If you are experiencing issues with game (e.g. it crashes at start or rendering is broken), you can try to enable de OpenGL renderer, by modifying :\n\n gfx_device_type to 2\n\n in the {0}/drive_c/users/USERNAME/Application Data/The Creative Assembly/Rome2/scripts/preferences_script.txt",
-                wine.prefixDirectory
             )
         );
         //N.B. : maybe need "LD_PRELOAD="libpthread.so.0 libGL.so.1" __GL_THREADED_OPTIMIZATIONS=1" for terrain glitches (OpenGL Mode)
