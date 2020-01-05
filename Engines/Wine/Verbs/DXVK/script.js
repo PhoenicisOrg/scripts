f0978039459775413c60c30889ea094a9f14b254
plata <plata@users.noreply.github.com>
2020-01-05 14:08:05 +0100

    Fix ESLint (include issues) (#1182)

--- Engines/Wine/Verbs/DXVK/script.js
+++ Engines/Wine/Verbs/DXVK/script.js
@@ -1,6 +1,6 @@
 const Wine = include("engines.wine.engine.object");
 const { Extractor } = include("utils.functions.filesystem.extract");
-const { ls, cp, cat, remove } = include("utils.functions.filesystem.files");
+const { ls, cp, remove } = include("utils.functions.filesystem.files");
 const operatingSystemFetcher = Bean("operatingSystemFetcher");
 const Optional = Java.type("java.util.Optional");
 const OverrideDLL = include("engines.wine.plugins.override_dll");
