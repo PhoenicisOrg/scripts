ce523ad42bfe47c605839feee1c2442d1d5a74ce
Starcommander <starcommander@gmx.at>
2020-06-10 00:56:55 +0200

    Add new install-scripts for game Dreamscapes2.

--- Applications/Games/Dreamscapes2/Online/script.js
+++ Applications/Games/Dreamscapes2/Online/script.js
@@ -4,14 +4,14 @@ const Vcrun2015 = include("engines.wine.verbs.vcrun2015");
 const Corefonts = include("engines.wine.verbs.corefonts");
 
 new OnlineInstallerScript()
-    .name("Dreamscapes")
+    .name("Dreamscapes2")
     .editor("GameTop-ShamanGames")
     .applicationHomepage("https://gametop.com")
     .author("Starcommander")
-    .url("https://cdn.gametop.com/free-games-download/Dreamscapes-The-Sandman.exe")
+    .url("https://cdn.gametop.com/free-games-download/Dreamscapes-2.exe")
     // Todo: checksum()
     .category("Games")
-    .executable("Dreamscapes_TheSandman.exe")
+    .executable("game.exe")
     .preInstall((wine /*, wizard*/) => {
         new Vcrun2015(wine).go();
         new Corefonts(wine).go();
