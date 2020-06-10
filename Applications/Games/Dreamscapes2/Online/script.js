2f7fa840cb5f9682f0188aa99c4402922e6731a6
Starcommander <starcommander@gmx.at>
2020-06-10 12:31:46 +0200

    Tested on macos, skip corefonts as it crashes and is not necessary. Error on mac with corefonts: could not run cabextract

--- Applications/Games/Dreamscapes2/Online/script.js
+++ Applications/Games/Dreamscapes2/Online/script.js
@@ -1,7 +1,6 @@
 const OnlineInstallerScript = include("engines.wine.quick_script.online_installer_script");
 
 const Vcrun2015 = include("engines.wine.verbs.vcrun2015");
-const Corefonts = include("engines.wine.verbs.corefonts");
 
 new OnlineInstallerScript()
     .name("Dreamscapes2")
@@ -14,5 +13,4 @@ new OnlineInstallerScript()
     .executable("game.exe")
     .preInstall((wine /*, wizard*/) => {
         new Vcrun2015(wine).go();
-        new Corefonts(wine).go();
     });
