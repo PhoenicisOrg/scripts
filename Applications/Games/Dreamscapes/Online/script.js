b5c2c523c815653afe3f5629fba9134e9bc86619
Starcommander <soundmodul@gmx.at>
2020-06-20 16:46:56 +0200

    Add Dreamscapes and Dreamscapes 2 (#1211)

---
+++ Applications/Games/Dreamscapes/Online/script.js
@@ -0,0 +1,16 @@
+const OnlineInstallerScript = include("engines.wine.quick_script.online_installer_script");
+
+const Vcrun2015 = include("engines.wine.verbs.vcrun2015");
+
+new OnlineInstallerScript()
+    .name("Dreamscapes")
+    .editor("GameTop-ShamanGames")
+    .applicationHomepage("https://gametop.com")
+    .author("Starcommander")
+    .url("https://cdn.gametop.com/free-games-download/Dreamscapes-The-Sandman.exe")
+    .checksum("ec3b9e2e8175ceca946f1f285542643a2a686c50")
+    .category("Games")
+    .executable("Dreamscapes_TheSandman.exe")
+    .preInstall((wine /*, wizard*/) => {
+        new Vcrun2015(wine).go();
+    });
