9795eaadf6a947467fef13ea0e8738444e82c1e0
Starcommander <starcommander@gmx.at>
2020-06-21 20:36:02 +0200

    Delete not needed DirectX9 for Namariel Legends: Iron Lord Also Vcrun2015.

--- Applications/Games/Namariel Legends: Iron Lord/Online/script.js
+++ Applications/Games/Namariel Legends: Iron Lord/Online/script.js
@@ -1,8 +1,5 @@
 const OnlineInstallerScript = include("engines.wine.quick_script.online_installer_script");
 
-const D3DX9 = include("engines.wine.verbs.d3dx9");
-const Vcrun2015 = include("engines.wine.verbs.vcrun2015");
-
 new OnlineInstallerScript()
     .name("Namariel Legends: Iron Lord")
     .editor("GameTop-ShamanGames")
@@ -11,8 +8,4 @@ new OnlineInstallerScript()
     .url("https://cdn.gametop.com/free-games-download/Namariel-Legends-Iron-Lord.exe")
     .checksum("c2c07aa8c8169b9da7f20e355bba460b6e7e356f")
     .category("Games")
-    .executable("game.exe")
-    .preInstall((wine /*, wizard*/) => {
-        new Vcrun2015(wine).go();
-        new D3DX9(wine).go();
-    });
+    .executable("game.exe");
