323b30dbb031b652c8b671d6a4a76a88072099ff
Starcommander <starcommander@gmx.at>
2020-06-22 20:39:05 +0200

    Add Fall Of The New Age

---
+++ Applications/Games/Fall Of The New Age/Online/script.js
@@ -0,0 +1,11 @@
+const OnlineInstallerScript = include("engines.wine.quick_script.online_installer_script");
+
+new OnlineInstallerScript()
+    .name("Fall Of The New Age")
+    .editor("GameTop")
+    .applicationHomepage("https://gametop.com")
+    .author("Starcommander")
+    .url("https://cdn.gametop.com/free-games-download/Fall-of-the-New-Age.exe")
+    .checksum("7cfcddde5e7a21dc1badce54ec29201c23012143")
+    .category("Games")
+    .executable("game.exe");
