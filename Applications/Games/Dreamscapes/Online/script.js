fd9d33fed851ede14e689a1bcf9af1ee1a0f3b3b
Dadu042 <56343138+Dadu042@users.noreply.github.com>
2022-06-10 08:53:50 +0200

    Fix checksum (my fault: MD5 -> SHA1).

--- Applications/Games/Dreamscapes/Online/script.js
+++ Applications/Games/Dreamscapes/Online/script.js
@@ -8,7 +8,7 @@ new OnlineInstallerScript()
     .applicationHomepage("https://gametop.com")
     .author("Starcommander")
     .url("https://cdn.gametop.com/free-games-download/Dreamscapes-The-Sandman.exe")
-    .checksum("8f5f5c4529793a22d16de3ae866bcc6d")
+    .checksum("a8f202256998b1dfd259f7caaf87d4f7b3e12279")
     .category("Games")
     .executable("Dreamscapes_TheSandman.exe")
     .preInstall((wine /*, wizard*/) => {
