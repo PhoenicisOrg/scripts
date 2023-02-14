85f7a6319196796be7339bbeafc3aae3d9a7ffd6
Walter Duque de Estrada <wbduque@mac.com>
2023-02-14 16:24:58 -0600

    ESLint

--- Engines/Wine/Engine/Versions/script.js
+++ Engines/Wine/Engine/Versions/script.js
@@ -1,4 +1,4 @@
-const {cat, touch} = include("utils.functions.filesystem.files");
+const { cat, touch } = include("utils.functions.filesystem.files");
 const Downloader = include("utils.functions.net.download");
 const propertyReader = Bean("propertyReader");
 
@@ -9,63 +9,63 @@ const propertyReader = Bean("propertyReader");
  */
 function sortVersions(versions) {
     versions.sort((a, b) => {
-            // check version format
-            const versionRegExp = /^(\d+\.\d+(\.\d+)?)(.*)?$/;
-            if (!versionRegExp.test(a.version)) {
-                throw new Error(`invalid Wine version "${a.version}`);
-            }
-            if (!versionRegExp.test(b.version)) {
-                throw new Error(`invalid Wine version "${a.version}`);
-            }
+        // check version format
+        const versionRegExp = /^(\d+\.\d+(\.\d+)?)(.*)?$/;
+        if (!versionRegExp.test(a.version)) {
+            throw new Error(`invalid Wine version "${a.version}`);
+        }
+        if (!versionRegExp.test(b.version)) {
+            throw new Error(`invalid Wine version "${a.version}`);
+        }
 
-            // sort
-            const aVersionParts = a.version.match(versionRegExp);
-            const bVersionParts = b.version.match(versionRegExp);
+        // sort
+        const aVersionParts = a.version.match(versionRegExp);
+        const bVersionParts = b.version.match(versionRegExp);
 
-            const aVersionNumbers = Array.from(aVersionParts[1].split('.')).map(item => Number(item));
-            const bVersionNumbers = Array.from(bVersionParts[1].split('.')).map(item => Number(item));
+        const aVersionNumbers = Array.from(aVersionParts[1].split('.')).map(item => Number(item));
+        const bVersionNumbers = Array.from(bVersionParts[1].split('.')).map(item => Number(item));
 
-            // ensure that cases where not all version numbers (major, minor, patch) are set are handled correctly
-            const maxVersionIdx = 2;
-            for (let i = 0; i <= maxVersionIdx; i++) {
-                if (typeof aVersionNumbers[i] === 'undefined') {
-                    aVersionNumbers[i] = 0;
-                }
-                if (typeof bVersionNumbers[i] === 'undefined') {
-                    bVersionNumbers[i] = 0;
-                }
+        // ensure that cases where not all version numbers (major, minor, patch) are set are handled correctly
+        const maxVersionIdx = 2;
+        for (let i = 0; i <= maxVersionIdx; i++) {
+            if (typeof aVersionNumbers[i] === 'undefined') {
+                aVersionNumbers[i] = 0;
             }
-
-            // ensure that cases where the description is not set are handled correctly
-            let aVersionDescription = aVersionParts[3];
-            if (typeof aVersionDescription === 'undefined') {
-                aVersionDescription = "";
-            }
-
-            let bVersionDescription = bVersionParts[3];
-            if (typeof bVersionDescription === 'undefined') {
-                bVersionDescription = "";
-            }
-
-            // major
-            if (aVersionNumbers[0] != bVersionNumbers[0]) {
-                return aVersionNumbers[0] - bVersionNumbers[0];
-            }
-            // minor
-            if (aVersionNumbers[1] != bVersionNumbers[1]) {
-                return aVersionNumbers[1] - bVersionNumbers[1];
-            }
-            // patch
-            if (aVersionNumbers[2] != bVersionNumbers[2]) {
-                return aVersionNumbers[2] - bVersionNumbers[2];
-            }
-            // description
-            if (aVersionDescription < bVersionDescription) {
-                return -1;
-            } else {
-                return 1;
+            if (typeof bVersionNumbers[i] === 'undefined') {
+                bVersionNumbers[i] = 0;
             }
         }
+
+        // ensure that cases where the description is not set are handled correctly
+        let aVersionDescription = aVersionParts[3];
+        if (typeof aVersionDescription === 'undefined') {
+            aVersionDescription = "";
+        }
+
+        let bVersionDescription = bVersionParts[3];
+        if (typeof bVersionDescription === 'undefined') {
+            bVersionDescription = "";
+        }
+
+        // major
+        if (aVersionNumbers[0] != bVersionNumbers[0]) {
+            return aVersionNumbers[0] - bVersionNumbers[0];
+        }
+        // minor
+        if (aVersionNumbers[1] != bVersionNumbers[1]) {
+            return aVersionNumbers[1] - bVersionNumbers[1];
+        }
+        // patch
+        if (aVersionNumbers[2] != bVersionNumbers[2]) {
+            return aVersionNumbers[2] - bVersionNumbers[2];
+        }
+        // description
+        if (aVersionDescription < bVersionDescription) {
+            return -1;
+        } else {
+            return 1;
+        }
+    }
     );
 }
 
@@ -85,7 +85,7 @@ function getLatestVersion(wizard, category, regex) {
 
     const regExp = new RegExp(regex);
     const versions = packages
-        .filter(({version}) => regExp.test(version))
+        .filter(({ version }) => regExp.test(version))
         .map(packageData => packageData.version);
 
     return versions[versions.length - 1];
@@ -120,7 +120,7 @@ module.getAvailableVersions = function (wizard) {
 }
 
 module.getLatestStableVersion = function (wizard, architecture) {
-    return getLatestStableVersion(wizard, this._wineDistribution, this._winePackage, architecture);
+    return getLatestVersion(wizard, `${this._wineDistribution}-${this._winePackage}-${architecture}`, /^\d+\.0(\.\d+)?$/);
 }
 
 module.getLatestStableVersion = function (wizard, distribution, winePackage, architecture) {
