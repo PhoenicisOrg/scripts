21183f8faf5e43abfeedc6158ec20cbc8b31bc8f
ahmedmoselhi <ahmedmoselhi55@gmail.com>
2023-03-10 14:31:25 +0200

    update notepad++ to v8.5

--- Applications/Development/Notepad++/Online/script.js
+++ Applications/Development/Notepad++/Online/script.js
@@ -5,7 +5,7 @@ new OnlineInstallerScript()
     .editor("Notepad++")
     .applicationHomepage("https://notepad-plus-plus.org/")
     .author("Quentin PÂRIS")
-    .url("https://notepad-plus-plus.org/repository/7.x/7.7.1/npp.7.7.1.Installer.exe")
+    .url("https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.5/npp.8.5.Installer.x64.exe")
     .checksum("071a81782d88810b0084bd2162c67cf0ff3ad13f")
     .category("Development")
     .executable("Notepad++.exe");
