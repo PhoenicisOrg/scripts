5a8f2e75843207e1798ba0de1cf385b89d3f6441
ahmedmoselhi <ahmedmoselhi55@gmail.com>
2023-03-10 15:14:20 +0200

    Update Notepad++ to 8.5 and ImgBurn checksum (#1265)

--- Applications/Accessories/ImgBurn/Online/script.js
+++ Applications/Accessories/ImgBurn/Online/script.js
@@ -6,6 +6,6 @@ new OnlineInstallerScript()
     .applicationHomepage("http://www.imgburn.com/")
     .author("ImperatorS79")
     .url("http://download.imgburn.com/SetupImgBurn_2.5.8.0.exe")
-    .checksum("6fc013ed5944b13efc54648699ea80f304e37ad0")
+    .checksum("6a3d20796e1fcd4169d5d339af6e491dcea3367c")
     .category("Accessories")
     .executable("ImgBurn.exe");
