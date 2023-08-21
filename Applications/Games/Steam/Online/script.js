1b934f3e43245d2f0296b686ca1fd408e9e7423d
Ripan Choudhary <bajwaripanpajal@gmail.com>
2023-08-21 21:11:39 +0530

    Update Steam checksum and fix formatting

--- Applications/Games/Steam/Online/script.js
+++ Applications/Games/Steam/Online/script.js
@@ -1,21 +1,19 @@
-const OnlineInstallerScript = include(
-  "engines.wine.quick_script.online_installer_script"
-);
+const OnlineInstallerScript = include("engines.wine.quick_script.online_installer_script");
 const { getLatestStagingVersion } = include("engines.wine.engine.versions");
 
 const Corefonts = include("engines.wine.verbs.corefonts");
 
 new OnlineInstallerScript()
-  .name("Steam")
-  .editor("Valve")
-  .applicationHomepage("https://www.steampowered.com")
-  .author("Quentin PÂRIS")
-  .url("https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe")
-  .checksum("1e5598f2de49fed2e81f3dd8630c7346a2b89487")
-  .category("Games")
-  .wineVersion(getLatestStagingVersion)
-  .wineDistribution("staging")
-  .preInstall((wine) => {
-    new Corefonts(wine).go();
-  })
-  .executable("steam.exe", ["-no-cef-sandbox"]);
+    .name("Steam")
+    .editor("Valve")
+    .applicationHomepage("https://www.steampowered.com")
+    .author("Quentin PÂRIS")
+    .url("https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe")
+    .checksum("1e5598f2de49fed2e81f3dd8630c7346a2b89487")
+    .category("Games")
+    .wineVersion(getLatestStagingVersion)
+    .wineDistribution("staging")
+    .preInstall((wine) => {
+        new Corefonts(wine).go();
+    })
+    .executable("steam.exe", ["-no-cef-sandbox"]);
